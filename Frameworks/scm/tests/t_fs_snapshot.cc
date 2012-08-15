cb010dd97308cc9b87836a04e8d7bbdced460220
Allan Odgaard <git@abetarda.com>
2012-08-15 02:04:51 +0200

    Fix test failure for builds in source tree
    The fs::snapsot_t class creates a finger print of a folder. We used the source tree for testing, but if the source tree hosts the build directory, then the fingerprint will (likely) change, since we run simultaneous build jobs.
    
    It now uses the Frameworks folder in the source tree, as it’s unlikely that someone would place their build directory in this location.

--- Frameworks/scm/tests/t_fs_snapshot.cc
+++ Frameworks/scm/tests/t_fs_snapshot.cc
@@ -6,7 +6,7 @@ class FsTreeTests : public CxxTest::TestSuite
 public:
 	void test_fs_tree ()
 	{
-		std::string path = path::join(__FILE__, "../../../..");
+		std::string path = path::join(__FILE__, "../../..");
 		TS_ASSERT_EQUALS(fs::snapshot_t(path), fs::snapshot_t(path));
 
 		test::jail_t jail;
