9940e4b32106033ea5d621bc7c41580006b1e666
Allan Odgaard <git@abetarda.com>
2012-08-30 23:05:08 +0200

    Remove unused boost headers

--- Shared/PCH/prelude.cc
+++ Shared/PCH/prelude.cc
@@ -17,11 +17,6 @@
 #include <memory>
 #include <numeric>
 #include <thread>
-#include <boost/optional.hpp>
 #include <boost/variant.hpp>
-#include <boost/bimap/bimap.hpp>
-#include <boost/bimap/multiset_of.hpp>
-#include <boost/tuple/tuple.hpp>
-#include <boost/tuple/tuple_comparison.hpp>
 
 #endif /* end of include guard: PRELUDE_CC_PCH_U5CKEP2N */
