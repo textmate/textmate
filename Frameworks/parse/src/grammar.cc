884989900704d3fa013b811f09c6652861ce1242
Allan Odgaard <git@abetarda.com>
2012-09-05 15:23:11 +0200

    Fix crash for missing grammars
    Incase our index is out-of-date and we try to load and parse a grammar that does not exist on disk, we would get a grammar_t with a “null” root rule, this would later crash when trying to use the grammar.
    
    As a simple fix we now ensure a dummy rule always exist.

--- Frameworks/parse/src/grammar.cc
+++ Frameworks/parse/src/grammar.cc
@@ -102,6 +102,13 @@ namespace parse
 	{
 		bundles::add_callback(&_bundles_callback);
 		_rule = parse_rule(_item->plist());
+
+		if(!_rule)
+		{
+			fprintf(stderr, "*** grammar missing for ‘%s’\n", _item->name().c_str());
+			_rule.reset(new rule_t);
+		}
+
 		_old_plist = _item->plist();
 	}
 
@@ -118,7 +125,7 @@ namespace parse
 
 	stack_ptr grammar_t::seed () const
 	{
-		return stack_ptr(new stack_t(_rule, _rule->scope_string));
+		return stack_ptr(new stack_t(_rule, _rule ? _rule->scope_string : ""));
 	}
 
 	void grammar_t::bundles_did_change ()
@@ -146,6 +153,9 @@ namespace parse
 
 	void grammar_t::resolve_includes (rule_ptr rule, std::vector<rule_ptr>& stack)
 	{
+		if(!rule)
+			return;
+
 		std::string const& inc = rule->include_string;
 		if(inc != NULL_STR && inc != "$base")
 		{
