c154751045268eb67b68319c40d22f19a16a14bd
Allan Odgaard <git@abetarda.com>
2012-09-08 01:00:41 +0200

    Allow cf::color_t to be uninitialized

--- Frameworks/cf/src/color.h
+++ Frameworks/cf/src/color.h
@@ -7,13 +7,15 @@ namespace cf
 {
 	struct PUBLIC color_t
 	{
-		color_t (std::string const& str = NULL_STR);
+		color_t () : _empty(true) { }
+		color_t (std::string const& str);
 		color_t (CGFloat r, CGFloat g, CGFloat b, CGFloat a) : _red(r), _green(g), _blue(b), _alpha(a) { }
 
 		bool operator== (color_t const& rhs) const;
 		bool operator!= (color_t const& rhs) const;
 
 		operator CGColorRef () const;
+		explicit operator bool () const { return !_empty; }
 
 		CGFloat red () const   { return _red;   }
 		CGFloat green () const { return _green; }
@@ -23,6 +25,7 @@ namespace cf
 	private:
 		CGFloat _red, _green, _blue, _alpha;
 		mutable std::shared_ptr<struct CGColor> cachedValue;
+		bool _empty = false;
 
 		friend std::string to_s (color_t const& c);
 		friend bool color_is_dark (color_t const& color);
