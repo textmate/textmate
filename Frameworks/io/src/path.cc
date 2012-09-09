textmate/textmate

commit 68ed829313b90cfe4fe79eb79095c8b031a951b1

Show alert if readlink fails
I am seeing crash reports from resolve_links and I can’t think of any other reason for this crash, than readlink() returning a value which is neither -1 nor in the range [0..len].

Frameworks/io/src/path.cc
@@ -266,12 +266,18 @@ namespace path
 			{
 				char buf[PATH_MAX];
 				ssize_t len = readlink(path.c_str(), buf, sizeof(buf));
-				if(len == -1)
+				if(0 < len && len < PATH_MAX)
 				{
-					fprintf(stderr, "*** error reading link ‘%s’\n", path.c_str());
-					return NULL_STR;
+					path = resolve_links(join(resolvedParent, std::string(buf, buf + len)), resolveParent, seen);
 				}
-				path = resolve_links(join(resolvedParent, std::string(buf, buf + len)), resolveParent, seen);
+				else
+				{
+					std::string errStr = len == -1 ? strerror(errno) : text::format("Result outside allowed range %zd", len);
+					std::string message = text::format("Failure calling readlink(‘%s’):\n\n\u2003%s\n\nPlease submit a bug report quoting the above:\nhttps://github.com/textmate/textmate/issues", path.c_str(), errStr.c_str());
+
+					CFOptionFlags responseFlags;
+					CFUserNotificationDisplayAlert(0 /* timeout */, kCFUserNotificationStopAlertLevel, NULL /* iconURL */, NULL /* soundURL */, NULL /* localizationURL */, CFSTR("Read Link Failed"), cf::wrap(message), CFSTR("Continue"), NULL /* alternateButtonTitle */, NULL /* otherButtonTitle */, &responseFlags);
+				}
 			}
 			else if(S_ISREG(buf.st_mode))
 			{

commit aed4c1c27ff7f2133a2c465bc4a6daee733723f7

No longer show dialog for readlink() failure
Only one readlink() failure reported (issue #389) which wouldn’t explain the crashes which the dialog (introduced in 68ed8293) was trying to track down.

Frameworks/io/src/path.cc
@@ -273,10 +273,7 @@ namespace path
 				else
 				{
 					std::string errStr = len == -1 ? strerror(errno) : text::format("Result outside allowed range %zd", len);
-					std::string message = text::format("Failure calling readlink(‘%s’):\n\n\u2003%s\n\nPlease submit a bug report quoting the above:\nhttps://github.com/textmate/textmate/issues", path.c_str(), errStr.c_str());
-
-					CFOptionFlags responseFlags;
-					CFUserNotificationDisplayAlert(0 /* timeout */, kCFUserNotificationStopAlertLevel, NULL /* iconURL */, NULL /* soundURL */, NULL /* localizationURL */, CFSTR("Read Link Failed"), cf::wrap(message), CFSTR("Continue"), NULL /* alternateButtonTitle */, NULL /* otherButtonTitle */, &responseFlags);
+					fprintf(stderr, "*** readlink(‘%s’) failed: %s\n", path.c_str(), errStr.c_str());
 				}
 			}
 			else if(S_ISREG(buf.st_mode))