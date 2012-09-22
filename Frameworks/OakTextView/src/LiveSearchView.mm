80f0e34566f49a38b28ab9eada8d434f7dae9c8f
Mads Hartmann Jensen <mads379@gmail.com>
2012-09-22 19:16:38 +0200

    Improved gradient a bit

--- Frameworks/OakTextView/src/LiveSearchView.mm
+++ Frameworks/OakTextView/src/LiveSearchView.mm
@@ -4,17 +4,17 @@
 
 - (void)drawRect:(NSRect)dirtyRect {
 		
-	NSColor *startingColor = [NSColor colorWithDeviceWhite:239.0/255.0 alpha:1];
+	NSColor *cornerColor = [NSColor colorWithDeviceWhite:239.0/255.0 alpha:1];
 	NSColor *middleColor = [NSColor colorWithDeviceWhite:223/255.0 alpha:1];
-	NSColor *endingColor = [NSColor colorWithDeviceWhite:209.0/255.0 alpha:1];
 	int angle = 270;
 
 	NSGradient* aGradient = [[NSGradient alloc] initWithColorsAndLocations:
-		startingColor,0.0f,
-		middleColor,0.1f,
-	   middleColor,0.9f,
-	   endingColor,1.0f, nil];
-	[aGradient drawInRect:[self bounds] angle:angle];
+		cornerColor,0.0f,
+	   middleColor,0.5f,
+	   cornerColor,1.0f, nil];
+	
+	NSRect bounds = NSMakeRect(self.bounds.origin.x, self.bounds.origin.y+1, self.bounds.size.width, self.bounds.size.height-1);
+	[aGradient drawInRect:bounds angle:angle];
 	
 	[[NSColor colorWithDeviceWhite:169.0/255.0 alpha:1] setFill];
 	NSRectFill(NSMakeRect(0,0,self.bounds.size.width,1));
