78bb0e57204754ad324aa9da37b780efa7c3f605
Allan Odgaard <git@abetarda.com>
2012-09-24 20:04:05 +0200

    Move code out of header

--- Frameworks/OakAppKit/src/OakStatusBar.mm
+++ Frameworks/OakAppKit/src/OakStatusBar.mm
@@ -5,6 +5,55 @@
 #import <OakFoundation/NSString Additions.h>
 #import <text/format.h>
 
+namespace sb
+{
+	cell_t::cell_t (cell::type type) : type(type), text(), toolTip(), image(), disabledImage(), action(), menuAction(), tag(0), min(0), max(0), skipTrailingSeparator(NO), state(NSOnState), padding(0)
+	{
+	}
+
+	cell_t& cell_t::size (CGFloat minSize, CGFloat maxSize)
+	{
+		min = minSize;
+		max = maxSize ?: minSize;
+		return *this;
+	}
+
+	cell_t& cell_t::set_tag (NSInteger newTag)         { tag = newTag;                             return *this; }
+	cell_t& cell_t::set_image (NSImage* img)           { image = img;                              return *this; }
+	cell_t& cell_t::set_view (NSView* aView)           { view = aView;                             return *this; }
+	cell_t& cell_t::pressed_image (NSImage* img)       { pressedImage = img;                       return *this; }
+	cell_t& cell_t::disabled_image (NSImage* img)      { disabledImage = img;                      return *this; }
+	cell_t& cell_t::tool_tip (std::string const& str)  { toolTip = str;                            return *this; }
+	cell_t& cell_t::enabled (BOOL enabled)             { state = enabled ? NSOnState : NSOffState; return *this; }
+	cell_t& cell_t::no_padding ()                      { padding = -1;                             return *this; }
+	cell_t& cell_t::no_separator ()                    { skipTrailingSeparator = YES;              return *this; }
+	cell_t& cell_t::set_menu_action (SEL anAction)     { menuAction = anAction;                    return *this; }
+
+	void cell_t::set_content (template_image const& tpl)
+	{
+		NSImage* templateImage = [NSImage imageNamed:tpl.name];
+		ASSERT(templateImage);
+		NSImage* standardImage = [[[NSImage alloc] initWithSize:templateImage.size] autorelease];
+		[standardImage lockFocus];
+		[templateImage drawAtPoint:NSZeroPoint fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:0.5];
+		[standardImage unlockFocus];
+		set_image(standardImage);
+		pressed_image(templateImage);
+	}
+
+	void cell_t::set_content (NSView* aView)          { view = aView; }
+	void cell_t::set_content (NSString* str)          { text = [str UTF8String]; }
+	void cell_t::set_content (std::string const& str) { text = str; }
+
+	void cell_t::set_content (NSImage* img)
+	{
+		image = img;
+		if(type == cell::button)
+			no_padding();
+	}
+
+} /* sb */
+
 @interface OakStatusBar ()
 - (void)updateLayout;
 @end
