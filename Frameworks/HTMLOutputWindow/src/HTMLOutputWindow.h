962bf5149bf213d247d127b955e9d2e94b2034bd
Allan Odgaard <git@abetarda.com>
2012-09-24 18:32:33 +0200

    ARC: Update HTMLOutputWindow framework

--- Frameworks/HTMLOutputWindow/src/HTMLOutputWindow.h
+++ Frameworks/HTMLOutputWindow/src/HTMLOutputWindow.h
@@ -1,14 +1,6 @@
 #import <HTMLOutput/HTMLOutput.h>
-#import <command/runner.h>
-#import <oak/debug.h>
+#import <oak/misc.h>
 
 PUBLIC @interface HTMLOutputWindowController : NSObject <NSWindowDelegate>
-{
-	OBJC_WATCH_LEAKS(HTMLOutputWindowController);
-
-	NSWindow* window;
-	OakHTMLOutputView* htmlOutputView;
-	command::runner_ptr runner;
-}
 + (HTMLOutputWindowController*)HTMLOutputWindowWithRunner:(command::runner_ptr const&)aRunner;
 @end
