cba6b3275b97e56deab9af2219cfbe314d309633
Allan Odgaard <git@abetarda.com>
2012-09-30 13:36:11 +0200

    Send crash reports in low-priority dispatch queue

--- Frameworks/CrashReporter/src/CrashReporter.mm
+++ Frameworks/CrashReporter/src/CrashReporter.mm
@@ -4,15 +4,13 @@
 #import <network/post.h>
 #import <plist/date.h>
 #import <io/path.h>
-#import <regexp/regexp.h>
 #import <text/format.h>
-#import <oak/server.h>
-#import <cf/cf.h>
 #import <ns/ns.h>
 #import <oak/oak.h>
 
 NSString* const kUserDefaultsDisableCrashReportingKey   = @"DisableCrashReports";
 NSString* const kUserDefaultsCrashReportsContactInfoKey = @"CrashReportsContactInfo";
+NSString* const kUserDefaultsCrashReportsSent           = @"CrashReportsSent";
 
 static std::string hardware_info (int field, bool integer = false)
 {
@@ -47,86 +45,51 @@ static std::string create_gzipped_file (std::string const& path)
 	return res;
 }
 
-namespace
+static void submit_crash_reports (std::string const& url, std::string const& processName)
 {
-	static NSString* const kUserDefaultsCrashReportsSent = @"CrashReportsSent";
-
-	struct post_reports_in_background_t
+	// could send: all reports from the last week
+	std::set<std::string> couldSend;
+	oak::date_t const cutOffDate = oak::date_t::now() - 7*24*60*60;
+	citerate(pair, find_reports(processName))
 	{
-		struct request_t { std::string url; std::vector<std::string> reports; };
+		if(cutOffDate < oak::date_t(pair->first))
+			couldSend.insert(pair->second);
+	}
 
-		post_reports_in_background_t (std::string const& url, std::string const& processName)
-		{
-			_client_key = _server.register_client(this);
+	// has sent: reports we already posted
+	std::set<std::string> hasSent;
+	for(NSString* path in [[NSUserDefaults standardUserDefaults] arrayForKey:kUserDefaultsCrashReportsSent])
+		hasSent.insert([path fileSystemRepresentation]);
 
-			// could send: all reports from the last week
-			oak::date_t const cutOffDate = oak::date_t::now() - 7*24*60*60;
-			citerate(pair, find_reports(processName))
-			{
-				if(cutOffDate < oak::date_t(pair->first))
-					_could_send.insert(pair->second);
-			}
+	// should send: “could send - has sent”
+	std::vector<std::string> shouldSend;
+	std::set_difference(couldSend.begin(), couldSend.end(), hasSent.begin(), hasSent.end(), back_inserter(shouldSend));
 
-			// has sent: reports we already posted
-			for(NSString* path in [[NSUserDefaults standardUserDefaults] arrayForKey:kUserDefaultsCrashReportsSent])
-				_has_sent.insert([path fileSystemRepresentation]);
-
-			// should send: “could send - has sent”
-			std::vector<std::string> shouldSend;
-			std::set_difference(_could_send.begin(), _could_send.end(), _has_sent.begin(), _has_sent.end(), back_inserter(shouldSend));
-			if(!shouldSend.empty())
-				return _server.send_request(_client_key, (request_t){ url, shouldSend });
-
-			delete this;
-		}
-
-		~post_reports_in_background_t ()
-		{
-			_server.unregister_client(_client_key);
-		}
-
-		static std::set<std::string> handle_request (request_t const& request)
+	std::set<std::string> didSend;
+	for(auto report : shouldSend)
+	{
+		std::string gzippedReport = create_gzipped_file(report);
+		if(gzippedReport != NULL_STR)
 		{
 			std::map<std::string, std::string> payload;
 			payload["hardware"] = hardware_info(HW_MODEL) + "/" + hardware_info(HW_MACHINE) + "/" + hardware_info(HW_NCPU, true);
 			payload["contact"]  = to_s([[NSUserDefaults standardUserDefaults] stringForKey:kUserDefaultsCrashReportsContactInfoKey]);
-
-			std::set<std::string> res;
-			for(std::string const report : request.reports)
-			{
-				std::string gzippedReport = create_gzipped_file(report);
-				if(gzippedReport != NULL_STR)
-				{
-					payload["report"] = "@" + gzippedReport;
-					long rc = post_to_server(request.url, payload);
-					if(200 <= rc && rc < 300 || 400 <= rc && rc < 500) // we don’t resend reports on a 4xx failure.
-						res.insert(report);
-					unlink(gzippedReport.c_str());
-				}
-			}
-			return res;
+			payload["report"]   = "@" + gzippedReport;
+			long rc = post_to_server(url, payload);
+			if(200 <= rc && rc < 300 || 400 <= rc && rc < 500) // we don’t resend reports on a 4xx failure.
+				didSend.insert(report);
+			unlink(gzippedReport.c_str());
 		}
+	}
 
-		void handle_reply (std::set<std::string> const& reportsSent)
-		{
-			_has_sent.insert(reportsSent.begin(), reportsSent.end());
-			std::vector<std::string> updatedHasSent;
-			std::set_intersection(_could_send.begin(), _could_send.end(), _has_sent.begin(), _has_sent.end(), back_inserter(updatedHasSent));
+	hasSent.insert(didSend.begin(), didSend.end());
+	std::vector<std::string> updatedHasSent;
+	std::set_intersection(couldSend.begin(), couldSend.end(), hasSent.begin(), hasSent.end(), back_inserter(updatedHasSent));
 
-			NSMutableArray* array = [NSMutableArray array];
-			for(auto path : updatedHasSent)
-				[array addObject:[NSString stringWithCxxString:path]];
-			[[NSUserDefaults standardUserDefaults] setObject:array forKey:kUserDefaultsCrashReportsSent];
-
-			delete this;
-		}
-
-	private:
-		std::set<std::string> _could_send, _has_sent;
-
-		size_t _client_key;
-		oak::server_t< post_reports_in_background_t, request_t > _server;
-	};
+	NSMutableArray* array = [NSMutableArray array];
+	for(auto path : updatedHasSent)
+		[array addObject:[NSString stringWithCxxString:path]];
+	[[NSUserDefaults standardUserDefaults] setObject:array forKey:kUserDefaultsCrashReportsSent];
 }
 
 static NSString* contact_info ()
@@ -140,8 +103,10 @@ static NSString* contact_info ()
 void OakSubmitNewCrashReportsInBackground (NSString* url, NSString* processName)
 {
 	[[NSUserDefaults standardUserDefaults] registerDefaults:@{ kUserDefaultsCrashReportsContactInfoKey : contact_info() }];
-	if([[NSUserDefaults standardUserDefaults] boolForKey:kUserDefaultsDisableCrashReportingKey])
-		return;
-
-	new post_reports_in_background_t(to_s(url), to_s(processName ?: [[NSProcessInfo processInfo] processName]));
+	if(![[NSUserDefaults standardUserDefaults] boolForKey:kUserDefaultsDisableCrashReportingKey])
+	{
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+			submit_crash_reports(to_s(url), to_s(processName ?: [[NSProcessInfo processInfo] processName]));
+		});
+	}
 }
