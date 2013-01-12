d861ae238ebcec5845e3018622ec5bee7c57cf73
Allan Odgaard <git@abetarda.com>
2013-01-13 00:54:25 +0100

    Let the pretty_plist CLI tool accept file name
    Previously it only supported stdin meaning it couldn’t be used with find, xargs, and similar.

--- Applications/pretty_plist/src/pretty_plist.cc
+++ Applications/pretty_plist/src/pretty_plist.cc
@@ -1,20 +1,21 @@
 #include <plist/ascii.h>
 
-static double const AppVersion  = 2.0;
+static double const AppVersion  = 2.1;
 static size_t const AppRevision = APP_REVISION;
 
-void version ()
+static void version ()
 {
 	fprintf(stdout, "%1$s %2$.1f (" COMPILE_DATE " revision %3$zu)\n", getprogname(), AppVersion, AppRevision);
 }
 
-void usage (FILE* io)
+static void usage (FILE* io)
 {
 	fprintf(io,
 		"%1$s %2$.1f (" COMPILE_DATE " revision %3$zu)\n"
-		"Usage: %1$s [-axhv]\n"
+		"Usage: %1$s [-axhv] file\n"
 		"Description:\n"
-		" Reads a property list from standard input and outputs ASCII version to stdout.\n"
+		" Reads a property list and outputs ASCII version to stdout.\n"
+		" If no file is specified, stdin is used.\n"
 		"Options:\n"
 		" -a, --ascii     Parse TextMate’s ASCII property list variant.\n"
 		" -x, --extended  Output TextMate’s ASCII property list variant.\n"
@@ -22,6 +23,46 @@ void usage (FILE* io)
 		" -v, --version   Print version information.\n"
 		"\n", getprogname(), AppVersion, AppRevision
 	);
+}
+
+static void parse_plist (FILE* fp, bool ascii, bool extended)
+{
+	std::string data;
+
+	char buf[1024];
+	while(size_t len = fread(buf, 1, sizeof(buf), fp))
+		data.insert(data.end(), buf, buf + len);
+
+	if(data.empty())
+	{
+		fprintf(stderr, "empty property list\n");
+		abort();
+	}
+
+	if(ascii)
+	{
+		if(CFPropertyListRef cfPlist = plist::create_cf_property_list(plist::parse_ascii(data)))
+		{
+			if(CFURLRef url = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, CFSTR("/dev/stdout"), kCFURLPOSIXPathStyle, false))
+			{
+				if(CFWriteStreamRef writeStream = CFWriteStreamCreateWithFile(kCFAllocatorDefault, url))
+				{
+					CFStringRef error = NULL;
+					CFWriteStreamOpen(writeStream);
+					CFPropertyListWriteToStream(cfPlist, writeStream, kCFPropertyListXMLFormat_v1_0, &error);
+					CFWriteStreamClose(writeStream);
+					CFRelease(writeStream);
+				}
+				CFRelease(url);
+			}
+			CFRelease(cfPlist);
+		}
+	}
+	else
+	{
+		std::string const& str = to_s(plist::parse(data), extended ? plist::kPreferSingleQuotedStrings : 0) + "\n";
+		write(STDOUT_FILENO, str.data(), str.size());
+	}
 }
 
 int main (int argc, char* const* argv)
@@ -55,35 +96,21 @@ int main (int argc, char* const* argv)
 	argc -= optind;
 	argv += optind;
 
-	std::string data;
+	if(argc == 0)
+		parse_plist(stdin, ascii, extended);
 
-	char buf[1024];
-	while(size_t len = fread(buf, 1, sizeof(buf), stdin))
-		data.insert(data.end(), buf, buf + len);
-
-	if(ascii)
+	for(int i = 0; i < argc; ++i)
 	{
-		if(CFPropertyListRef cfPlist = plist::create_cf_property_list(plist::parse_ascii(data)))
+		if(FILE* fp = fopen(argv[i], "r"))
 		{
-			if(CFURLRef url = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, CFSTR("/dev/stdout"), kCFURLPOSIXPathStyle, false))
-			{
-				if(CFWriteStreamRef writeStream = CFWriteStreamCreateWithFile(kCFAllocatorDefault, url))
-				{
-					CFStringRef error = NULL;
-					CFWriteStreamOpen(writeStream);
-					CFPropertyListWriteToStream(cfPlist, writeStream, kCFPropertyListXMLFormat_v1_0, &error);
-					CFWriteStreamClose(writeStream);
-					CFRelease(writeStream);
-				}
-				CFRelease(url);
-			}
-			CFRelease(cfPlist);
+			parse_plist(fp, ascii, extended);
+			fclose(fp);
 		}
-	}
-	else
-	{
-		std::string const& str = to_s(plist::parse(data), extended ? plist::kPreferSingleQuotedStrings : 0) + "\n";
-		write(STDOUT_FILENO, str.data(), str.size());
+		else
+		{
+			perror("fopen");
+			abort();
+		}
 	}
 
 	return 0;
