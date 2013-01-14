69904bef754c5a07b925b4e63789e8f8e2bd8246
Allan Odgaard <git@abetarda.com>
2013-01-14 04:39:51 +0100

    Improve look of navigation bar

--- Frameworks/OakFileBrowser/src/ui/OFBHeaderView.mm
+++ Frameworks/OakFileBrowser/src/ui/OFBHeaderView.mm
@@ -1,4 +1,5 @@
 #import "OFBHeaderView.h"
+#import <OakAppKit/OakAppKit.h>
 
 static NSButton* ImageButton (NSString* imageName)
 {
@@ -25,6 +26,10 @@ static NSPopUpButton* PopUpButton ()
 	return res;
 }
 
+@interface OFBHeaderView ()
+@property (nonatomic) BOOL renderEnabled;
+@end
+
 @implementation OFBHeaderView
 - (id)initWithFrame:(NSRect)aRect
 {
@@ -38,6 +43,8 @@ static NSPopUpButton* PopUpButton ()
 
 		NSDictionary* views = @{
 			@"folder"   : self.folderPopUpButton,
+			@"shadow"   : OakCreateViewWithColor([NSColor grayColor]),
+			@"divider"  : OakCreateViewWithColor([NSColor lightGrayColor]),
 			@"back"     : self.goBackButton,
 			@"forward"  : self.goForwardButton,
 		};
@@ -48,11 +55,19 @@ static NSPopUpButton* PopUpButton ()
 			[self addSubview:view];
 		}
 
-		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(5)-[folder(>=75)]-[back(==21)][forward(==back)]-(5)-|" options:NSLayoutFormatAlignAllCenterY metrics:nil views:views]];
-		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[folder(==26)]"                                     options:0 metrics:nil views:views]];
-		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(1)-[back(==forward,==23)]-(1)-|"                 options:0 metrics:nil views:views]];
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(1)-[folder(>=75)]-(3)-[shadow(==1)][divider(==1)]-(9)-[back(==11)]-(9)-[forward(==back)]-(9)-|" options:0 metrics:nil views:views]];
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(2)-[folder]"           options:0 metrics:nil views:views]];
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[shadow]|"               options:0 metrics:nil views:views]];
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[divider]|"              options:0 metrics:nil views:views]];
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(0)-[back(==22)]-(2)-|" options:0 metrics:nil views:views]];
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(0)-[forward(==back)]"  options:0 metrics:nil views:views]];
 	}
 	return self;
+}
+
+- (void)dealloc
+{
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (BOOL)isOpaque
@@ -60,8 +75,44 @@ static NSPopUpButton* PopUpButton ()
 	return YES;
 }
 
+- (void)viewWillMoveToWindow:(NSWindow*)newWindow
+{
+	if(self.window)
+	{
+		[[NSNotificationCenter defaultCenter] removeObserver:self name:NSWindowDidBecomeMainNotification object:self.window];
+		[[NSNotificationCenter defaultCenter] removeObserver:self name:NSWindowDidResignMainNotification object:self.window];
+	}
+
+	if(newWindow)
+	{
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowDidChangeMain:) name:NSWindowDidBecomeMainNotification object:newWindow];
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowDidChangeMain:) name:NSWindowDidResignMainNotification object:newWindow];
+	}
+}
+
+- (void)viewDidMoveToWindow
+{
+	self.renderEnabled = [self.window isMainWindow];
+}
+
+- (void)windowDidChangeMain:(NSNotification*)aNotification
+{
+	self.renderEnabled = [self.window isMainWindow];
+}
+
+- (void)setRenderEnabled:(BOOL)flag
+{
+	if(_renderEnabled != flag)
+	{
+		_renderEnabled = flag;
+		[self setNeedsDisplay:YES];
+	}
+}
+
 - (void)drawRect:(NSRect)aRect
 {
-	[[[NSGradient alloc] initWithStartingColor:[NSColor grayColor] endingColor:[NSColor lightGrayColor]] drawInRect:self.bounds angle:90];
+	NSColor* topColor    = self.renderEnabled ? [NSColor lightGrayColor] : [NSColor lightGrayColor];
+	NSColor* bottomColor = self.renderEnabled ? [NSColor grayColor]      : [NSColor lightGrayColor];
+	[[[NSGradient alloc] initWithStartingColor:bottomColor endingColor:topColor] drawInRect:self.bounds angle:90];
 }
 @end
