textmate/textmate

commit 645f8d91e14cfc9b9b45aab3701378949e142e65

Let OS handle keys for Go to Tab submenu
My memory is a little vague here, but I believe the point of handling the menu keys was to workaround a bug in NSMenu.

The bug was that NSMenu would look at (the target of) cached menu items, probably for UI validation, so potentially sending methods to unretained objects, which could lead to a crash. Several workarounds were attempted (like clearing the target property after the menu had been displayed) but the only effective one was overloading key handling.

I’m quite sure though that this bug is no longer relevant.

Frameworks/OakAppKit/src/OakSubmenuController.mm
@@ -6,28 +6,7 @@
 
 OAK_DEBUG_VAR(OakSubmenuController);
 
-static OakSubmenuController* SharedInstance = nil;
-
-@interface OakSubmenuController ()
-@property (nonatomic, retain) id representedObject;
-@end
-
 @implementation OakSubmenuController
-@synthesize representedObject;
-
-+ (OakSubmenuController*)sharedInstance
-{
-	return SharedInstance ?: [[OakSubmenuController new] autorelease];
-}
-
-- (id)init
-{
-	if(SharedInstance)
-			[self release];
-	else	self = SharedInstance = [[super init] retain];
-	return SharedInstance;
-}
-
 - (void)awakeFromNib
 {
 	[goToMenu setDelegate:self];
@@ -46,48 +25,5 @@ static OakSubmenuController* SharedInstance = nil;
 - (void)menuNeedsUpdate:(NSMenu*)aMenu
 {
 	[self updateMenu:aMenu withSelector:aMenu == goToMenu ? @selector(updateGoToMenu:) : @selector(updateBookmarksMenu:)];
-}
-
-- (BOOL)menuHasKeyEquivalent:(NSMenu*)aMenu forEvent:(NSEvent*)anEvent target:(id*)anId action:(SEL*)aSEL
-{
-	D(DBF_OakSubmenuController, bug("%s %s\n", to_s(anEvent).c_str(), [[aMenu description] UTF8String]););
-
-	if(aMenu != goToMenu)
-		return NO;
-
-	self.representedObject = nil;
-	std::string const eventString = to_s(anEvent);
-
-	NSMenu* dummy = [[NSMenu new] autorelease];
-	[self updateMenu:dummy withSelector:@selector(updateGoToMenu:)];
-	for(NSMenuItem* item in [dummy itemArray])
-	{
-		if(eventString == ns::create_event_string(item.keyEquivalent, item.keyEquivalentModifierMask))
-		{
-			D(DBF_OakSubmenuController, bug("%s%ld\n", sel_getName(item.action), item.tag););
-			*anId                  = item.target;
-			*aSEL                  = item.action;
-			tag                    = item.tag;
-			self.representedObject = item.representedObject;
-			return YES;
-		}
-	}
-	return NO;
-}
-
-- (NSInteger)tagForSender:(id)aSender
-{
-	if([aSender respondsToSelector:@selector(tag)])
-		return [aSender tag];
-	ASSERT([aSender isKindOfClass:[NSMenu class]]);
-	return tag;
-}
-
-- (id)representedObjectForSender:(id)aSender
-{
-	if([aSender respondsToSelector:@selector(representedObject)])
-		return [aSender representedObject];
-	ASSERT([aSender isKindOfClass:[NSMenu class]]);
-	return representedObject;
 }
 @end

Applications/TextMate/src/AboutWindowController.mm
@@ -1,5 +1,4 @@
 #import "AboutWindowController.h"
-#import <OakAppKit/OakSubmenuController.h>
 #import <OakFoundation/NSString Additions.h>
 #import <updater/updater.h>
 #import <license/license.h>
@@ -147,8 +146,6 @@ static NSData* Digest (NSString* someString)
 		identifier = [sender itemIdentifier];
 	else if([sender respondsToSelector:@selector(representedObject)])
 		identifier = [sender representedObject];
-	else if([sender isKindOfClass:[NSMenu class]])
-		identifier = [[OakSubmenuController sharedInstance] representedObjectForSender:sender];
 	else if([sender respondsToSelector:@selector(description)])
 		identifier = [sender description];
 

Frameworks/DocumentWindow/src/DocumentController.mm
@@ -9,7 +9,6 @@
 #import <OakAppKit/OakFileIconImage.h>
 #import <OakAppKit/OakPasteboard.h>
 #import <OakAppKit/OakSavePanel.h>
-#import <OakAppKit/OakSubmenuController.h>
 #import <OakAppKit/OakTabBarView.h>
 #import <OakAppKit/OakWindowFrameHelper.h>
 #import <OakFoundation/NSArray Additions.h>
@@ -1182,7 +1181,7 @@ namespace
 
 - (IBAction)selectNextTab:(id)sender            { self.selectedTabIndex = (_selectedTabIndex + 1) % _documents.size();                     [self openAndSelectDocument:_documents[_selectedTabIndex]]; }
 - (IBAction)selectPreviousTab:(id)sender        { self.selectedTabIndex = (_selectedTabIndex + _documents.size() - 1) % _documents.size(); [self openAndSelectDocument:_documents[_selectedTabIndex]]; }
-- (IBAction)takeSelectedTabIndexFrom:(id)sender { self.selectedTabIndex = [[OakSubmenuController sharedInstance] tagForSender:sender];     [self openAndSelectDocument:_documents[_selectedTabIndex]]; }
+- (IBAction)takeSelectedTabIndexFrom:(id)sender { self.selectedTabIndex = [sender tag];                                                    [self openAndSelectDocument:_documents[_selectedTabIndex]]; }
 
 // ==================
 // = OakFileBrowser =

Frameworks/Find/src/FFWindowController.mm
@@ -7,7 +7,6 @@
 #import <OakAppKit/NSImage Additions.h>
 #import <OakAppKit/NSMenu Additions.h>
 #import <OakAppKit/NSMenuItem Additions.h>
-#import <OakAppKit/OakSubmenuController.h>
 #import <OakFoundation/NSString Additions.h>
 #import <document/collection.h>
 #import <io/path.h>
@@ -305,7 +304,7 @@ struct operation_t
 
 - (IBAction)selectMatchingPath:(id)sender
 {
-	if(NSString* path = [[OakSubmenuController sharedInstance] representedObjectForSender:sender])
+	if(NSString* path = [sender representedObject])
 	{
 		for(FFMatch* match in self.searcher.allDocumentsWithMatches)
 		{

Frameworks/OakAppKit/src/OakSubmenuController.h
@@ -1,14 +1,8 @@
 #import <oak/misc.h>
 
-PUBLIC @interface OakSubmenuController : NSObject <NSMenuDelegate>
+@interface OakSubmenuController : NSObject <NSMenuDelegate>
 {
 	IBOutlet NSMenu* goToMenu;
 	IBOutlet NSMenu* marksMenu;
-
-	NSInteger tag;
-	id representedObject;
 }
-+ (OakSubmenuController*)sharedInstance;
-- (NSInteger)tagForSender:(id)aSender;
-- (id)representedObjectForSender:(id)aSender;
 @end

Frameworks/OakFilterList/src/datasources/BundleItemChooser.mm
@@ -6,7 +6,6 @@
 #import <scope/scope.h>
 #import <OakFoundation/NSString Additions.h>
 #import <OakAppKit/NSMenu Additions.h>
-#import <OakAppKit/OakSubmenuController.h>
 #import <text/case.h>
 #import <text/ctype.h>
 #import <text/ranker.h>
@@ -269,8 +268,8 @@ static NSString* const AllScopes    = @"AllScopes";
 
 - (void)takeSelectedItemFrom:(NSMenuItem*)item
 {
-	NSString* identifier  = [[OakSubmenuController sharedInstance] representedObjectForSender:item];
-	NSUInteger groupIndex = [[OakSubmenuController sharedInstance] tagForSender:item];
+	NSString* identifier  = [item representedObject];
+	NSUInteger groupIndex = [item tag];
 	[scopeBar setSelected:YES forItem:identifier inGroup:groupIndex];
 }
 

Frameworks/OakFilterList/src/datasources/OakFileChooser.mm
@@ -2,7 +2,6 @@
 #import "SymbolList.h"
 #import "../OakAbbreviations.h"
 #import "../highlight_ranges.h"
-#import <OakAppKit/OakSubmenuController.h>
 #import <OakFoundation/NSString Additions.h>
 #import <OakFoundation/OakHistoryList.h>
 #import <text/case.h>
@@ -466,7 +465,7 @@ void file_chooser_t::wait () const
 
 - (IBAction)takeSourceIndexFrom:(id)sender
 {
-	sourceSelector.selectedSegment = [sender respondsToSelector:@selector(selectedSegment)] ? [sender selectedSegment] : [[OakSubmenuController sharedInstance] tagForSender:sender];
+	sourceSelector.selectedSegment = [sender respondsToSelector:@selector(selectedSegment)] ? [sender selectedSegment] : [sender tag];
 	fileChooser.sourceIndex        = sourceSelector.selectedSegment;
 	[self updateTitles];
 }

Frameworks/Preferences/src/Preferences.mm
@@ -7,7 +7,6 @@
 #import "TerminalPreferences.h"
 #import "Keys.h"
 #import <MASPreferences/MASPreferencesWindowController.h>
-#import <OakAppKit/OakSubmenuController.h>
 #import <oak/debug.h>
 
 OAK_DEBUG_VAR(Preferences);
@@ -50,7 +49,7 @@ OAK_DEBUG_VAR(Preferences);
 
 - (void)takeSelectedViewControllerIndexFrom:(id)sender
 {
-	NSUInteger index = [[OakSubmenuController sharedInstance] tagForSender:sender];
+	NSUInteger index = [sender tag];
 	[_windowController selectControllerAtIndex:index];
 }
 

commit 67ebb34f23a3893eefc66171a9d0ce0f76e7afde

Fix unreliable tab switching keys
This was broken by 645f8d91e14cfc9b9b45aab3701378949e142e65.

The issue is that if the user press a key not in our managed menu, then the menu delegate will no longer be asked to update the menu for keys not in this (cached) menu.

Example: If there are items for ⌘1-3 in the menu and the user press ⌥⌘N a few times (to create more tabs), then the delegate is asked to update the menu on the first press, but as ⌥⌘N is not in the menu, successive events will not cause the menu to be updated. If the user then press ⌘5 (to go to the 5’th tab), nothing will happen, because our menu only holds 3 items, and the delegate is not asked to update the menu.

Frameworks/OakAppKit/src/OakSubmenuController.mm
@@ -6,6 +6,22 @@
 
 OAK_DEBUG_VAR(OakSubmenuController);
 
+@interface OakProxyMenuItem : NSMenuItem
+@end
+
+@implementation OakProxyMenuItem
+- (void)tmSendAction:(id)sender
+{
+	[NSApp sendAction:self.action to:self.target from:self];
+	self.target = nil;
+	self.representedObject = nil;
+}
+@end
+
+@interface OakSubmenuController ()
+@property (nonatomic, retain) OakProxyMenuItem* proxyMenuItem;
+@end
+
 @implementation OakSubmenuController
 - (void)awakeFromNib
 {
@@ -25,5 +41,38 @@ OAK_DEBUG_VAR(OakSubmenuController);
 - (void)menuNeedsUpdate:(NSMenu*)aMenu
 {
 	[self updateMenu:aMenu withSelector:aMenu == goToMenu ? @selector(updateGoToMenu:) : @selector(updateBookmarksMenu:)];
+}
+
+- (BOOL)menuHasKeyEquivalent:(NSMenu*)aMenu forEvent:(NSEvent*)anEvent target:(id*)anId action:(SEL*)aSEL
+{
+	D(DBF_OakSubmenuController, bug("%s %s\n", to_s(anEvent).c_str(), [[aMenu description] UTF8String]););
+
+	if(aMenu != goToMenu)
+		return NO;
+
+	std::string const eventString = to_s(anEvent);
+
+	NSMenu* dummy = [[NSMenu new] autorelease];
+	[self updateMenu:dummy withSelector:@selector(updateGoToMenu:)];
+	for(NSMenuItem* item in [dummy itemArray])
+	{
+		if(eventString == ns::create_event_string(item.keyEquivalent, item.keyEquivalentModifierMask))
+		{
+			D(DBF_OakSubmenuController, bug("%s%ld\n", sel_getName(item.action), item.tag););
+			if(!self.proxyMenuItem)
+				self.proxyMenuItem = [[OakProxyMenuItem new] autorelease];
+
+			self.proxyMenuItem.action            = item.action;
+			self.proxyMenuItem.target            = item.target;
+			self.proxyMenuItem.tag               = item.tag;
+			self.proxyMenuItem.representedObject = item.representedObject;
+
+			*anId = self.proxyMenuItem;
+			*aSEL = @selector(tmSendAction:);
+
+			return YES;
+		}
+	}
+	return NO;
 }
 @end