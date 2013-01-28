1475f131edbf6d386a3d93b13c3be5f7aca44b32
Steven Clukey <saclukey@live.com>
2013-01-28 18:26:58 -0500

    SymbolChooser references OakDocumentView instead document.
    Since a document cannot send notifications, this will allow the
    SymbolChooser to monitor and respond to notifications from the
    DocumentView.

--- Frameworks/DocumentWindow/src/DocumentController.mm
+++ Frameworks/DocumentWindow/src/DocumentController.mm
@@ -1453,7 +1453,7 @@ namespace
 - (IBAction)showSymbolChooser:(id)sender
 {
 	self.filterWindowController                         = [OakFilterWindowController new];
-	self.filterWindowController.dataSource              = [SymbolChooser symbolChooserForDocument:[self selectedDocument]];
+	self.filterWindowController.dataSource              = [SymbolChooser symbolChooserForDocumentView:[self documentView]];
 	self.filterWindowController.action                  = @selector(symbolChooserDidSelectItems:);
 	self.filterWindowController.sendActionOnSingleClick = YES;
 	[self.filterWindowController showWindowRelativeToWindow:self.window];
