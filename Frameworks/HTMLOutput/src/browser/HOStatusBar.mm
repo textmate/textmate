c46070f9ffab1e00fb38638ed76cc4846b01708b
Allan Odgaard <git@abetarda.com>
2013-02-12 15:42:14 +0100

    Omit background fill if we have a border

--- Frameworks/HTMLOutput/src/browser/HOStatusBar.mm
+++ Frameworks/HTMLOutput/src/browser/HOStatusBar.mm
@@ -147,9 +147,12 @@ static NSImageView* OakCreateImageView (NSImage* image)
 
 - (void)drawRect:(NSRect)aRect
 {
-	[[NSColor windowBackgroundColor] set]; NSRectFill(aRect);
-	
-	[super drawRect:aRect];
+	if([self.window contentBorderThicknessForEdge:NSMinYEdge] < NSMaxY(self.frame))
+	{
+		[[NSColor windowBackgroundColor] set];
+		NSRectFill(aRect);
+		[super drawRect:aRect];
+	}
 }
 
 - (void)setIndeterminateProgress:(BOOL)newIndeterminateProgress
