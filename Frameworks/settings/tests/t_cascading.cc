a3ec25891212c2ff23f3684af1d82d6bd8ff3faa
Allan Odgaard <git@abetarda.com>
2013-02-25 15:25:17 +0100

    Update settings tests to new system

--- Frameworks/settings/tests/t_cascading.cc
+++ Frameworks/settings/tests/t_cascading.cc
@@ -1,30 +1,26 @@
 #include <settings/settings.h>
 #include <test/jail.h>
 
-class CascadingTests : public CxxTest::TestSuite
+void test_scope_selector_ranking ()
 {
-public:
-	void test_scope_selector_ranking ()
-	{
-		test::jail_t jail;
+	test::jail_t jail;
 
-		jail.set_content(".tm_properties",         "mySetting = 1\n[ text ]\nmySetting = 2\n[ text.markup ]\nmySetting = 3\n[ text.markup.html ]\nmySetting = 4\n");
-		jail.set_content("foo/.tm_properties",     "mySetting = 5\n[ text.markup ]\nmySetting = 6\n");
-		jail.set_content("foo/bar/.tm_properties", "[ text ]\nmySetting = 7\n[ text.markup.html ]\nmySetting = 8\n");
+	jail.set_content(".tm_properties",         "mySetting = 1\n[ text ]\nmySetting = 2\n[ text.markup ]\nmySetting = 3\n[ text.markup.html ]\nmySetting = 4\n");
+	jail.set_content("foo/.tm_properties",     "mySetting = 5\n[ text.markup ]\nmySetting = 6\n");
+	jail.set_content("foo/bar/.tm_properties", "[ text ]\nmySetting = 7\n[ text.markup.html ]\nmySetting = 8\n");
 
-		TS_ASSERT_EQUALS(settings_for_path(jail.path("file")                            ).get("mySetting"), "1");
-		TS_ASSERT_EQUALS(settings_for_path(jail.path("file"),                     "text").get("mySetting"), "2");
-		TS_ASSERT_EQUALS(settings_for_path(jail.path("file"),              "text.markup").get("mySetting"), "3");
-		TS_ASSERT_EQUALS(settings_for_path(jail.path("file"),         "text.markup.html").get("mySetting"), "4");
+	OAK_ASSERT_EQ(settings_for_path(jail.path("file")                            ).get("mySetting"), "1");
+	OAK_ASSERT_EQ(settings_for_path(jail.path("file"),                     "text").get("mySetting"), "2");
+	OAK_ASSERT_EQ(settings_for_path(jail.path("file"),              "text.markup").get("mySetting"), "3");
+	OAK_ASSERT_EQ(settings_for_path(jail.path("file"),         "text.markup.html").get("mySetting"), "4");
 
-		TS_ASSERT_EQUALS(settings_for_path(jail.path("foo/file")                        ).get("mySetting"), "5");
-		TS_ASSERT_EQUALS(settings_for_path(jail.path("foo/file"),                 "text").get("mySetting"), "2");
-		TS_ASSERT_EQUALS(settings_for_path(jail.path("foo/file"),          "text.markup").get("mySetting"), "6");
-		TS_ASSERT_EQUALS(settings_for_path(jail.path("foo/file"),     "text.markup.html").get("mySetting"), "4");
+	OAK_ASSERT_EQ(settings_for_path(jail.path("foo/file")                        ).get("mySetting"), "5");
+	OAK_ASSERT_EQ(settings_for_path(jail.path("foo/file"),                 "text").get("mySetting"), "2");
+	OAK_ASSERT_EQ(settings_for_path(jail.path("foo/file"),          "text.markup").get("mySetting"), "6");
+	OAK_ASSERT_EQ(settings_for_path(jail.path("foo/file"),     "text.markup.html").get("mySetting"), "4");
 
-		TS_ASSERT_EQUALS(settings_for_path(jail.path("foo/bar/file")                    ).get("mySetting"), "5");
-		TS_ASSERT_EQUALS(settings_for_path(jail.path("foo/bar/file"),             "text").get("mySetting"), "7");
-		TS_ASSERT_EQUALS(settings_for_path(jail.path("foo/bar/file"),      "text.markup").get("mySetting"), "6");
-		TS_ASSERT_EQUALS(settings_for_path(jail.path("foo/bar/file"), "text.markup.html").get("mySetting"), "8");
-	}
-};
+	OAK_ASSERT_EQ(settings_for_path(jail.path("foo/bar/file")                    ).get("mySetting"), "5");
+	OAK_ASSERT_EQ(settings_for_path(jail.path("foo/bar/file"),             "text").get("mySetting"), "7");
+	OAK_ASSERT_EQ(settings_for_path(jail.path("foo/bar/file"),      "text.markup").get("mySetting"), "6");
+	OAK_ASSERT_EQ(settings_for_path(jail.path("foo/bar/file"), "text.markup.html").get("mySetting"), "8");
+}
