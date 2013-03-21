fbec3539c6ba3fbd2750a858f3d75582f01a2a71
Allan Odgaard <git@abetarda.com>
2013-03-21 14:17:43 +0100

    Use io::spawn for launching tar

--- Frameworks/network/src/tbz.cc
+++ Frameworks/network/src/tbz.cc
@@ -3,7 +3,7 @@
 #include <text/trim.h>
 #include <oak/datatypes.h>
 #include <oak/compat.h>
-#include <io/environment.h>
+#include <io/exec.h>
 
 #define OAK_CHECK(expr) do { if((expr) != 0) { perror(#expr); return -1; } } while(false)
 
@@ -11,51 +11,28 @@ namespace network
 {
 	pid_t launch_tbz (std::string const& dest, int& input, int& output, std::string& error)
 	{
-		signal(SIGPIPE, SIG_IGN);
-
-		char const* const argv[] = { "/usr/bin/tar", "-jxmkC", dest.c_str(), "--strip-components", "1", NULL };
-		oak::c_array env(oak::basic_environment());
-
-		int in[2], out[2];
-		posix_spawn_file_actions_t fileActions;
-		posix_spawnattr_t flags;
-		pid_t pid = -1;
-
-		short closeOnExecFlag = (oak::os_major() == 10 && oak::os_minor() == 7) ? 0 : POSIX_SPAWN_CLOEXEC_DEFAULT;
-
-		OAK_CHECK(pipe(&in[0]) );
-		OAK_CHECK(pipe(&out[0]));
-		OAK_CHECK(fcntl(in[1],  F_SETFD, FD_CLOEXEC));
-		OAK_CHECK(fcntl(out[0], F_SETFD, FD_CLOEXEC));
-		OAK_CHECK(posix_spawn_file_actions_init(&fileActions));
-		OAK_CHECK(posix_spawn_file_actions_adddup2(&fileActions, in[0], 0));
-		OAK_CHECK(posix_spawn_file_actions_adddup2(&fileActions, out[1], 1));
-		OAK_CHECK(posix_spawn_file_actions_adddup2(&fileActions, out[1], 2));
-		OAK_CHECK(posix_spawn_file_actions_addclose(&fileActions, in[0]));
-		OAK_CHECK(posix_spawn_file_actions_addclose(&fileActions, out[1]));
-		OAK_CHECK(posix_spawnattr_init(&flags));
-		OAK_CHECK(posix_spawnattr_setflags(&flags, POSIX_SPAWN_SETSIGDEF|closeOnExecFlag));
-
-		int rc = posix_spawn(&pid, "/usr/bin/tar", &fileActions, &flags, (char* const*)argv, env);
-		if(rc != 0)
-			perror(text::format("posix_spawn(\"/usr/bin/tar\")").c_str());
-
-		OAK_CHECK(posix_spawnattr_destroy(&flags));
-		OAK_CHECK(posix_spawn_file_actions_destroy(&fileActions));
-		OAK_CHECK(close(in[0]));
-		OAK_CHECK(close(out[1]));
-
-		if(rc != 0)
+		if(io::process_t process = io::spawn(std::vector<std::string>{ "/usr/bin/tar", "-jxmkC", dest.c_str(), "--strip-components", "1" }))
 		{
-			close(in[1]);
-			close(out[0]);
-			return -1;
+			signal(SIGPIPE, SIG_IGN);
+
+			input  = process.in;
+			output = process.out;
+
+			dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+				ssize_t len;
+				char bytes[512];
+				std::string str;
+				while((len = read(process.err, bytes, sizeof(bytes))) > 0)
+					str.insert(str.end(), bytes, bytes + len);
+				close(process.err);
+
+				if(!str.empty())
+					fprintf(stderr, "%s\n", str.c_str());
+			});
+
+			return process.pid;
 		}
-
-		input  = in[1];
-		output = out[0];
-
-		return pid;
+		return -1;
 	}
 
 	bool finish_tbz (pid_t pid, int& input, int& output, std::string& error)
