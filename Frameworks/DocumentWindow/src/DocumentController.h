e80e9c517280c44fcab52e2008166ed8681dbb75
Allan Odgaard <git@abetarda.com>
2013-05-15 21:23:15 +0700

    Remove private methods from header

--- Frameworks/DocumentWindow/src/DocumentController.h
+++ Frameworks/DocumentWindow/src/DocumentController.h
@@ -82,14 +82,7 @@ PUBLIC @interface DocumentController : NSObject
 - (IBAction)goToSCMDataSource:(id)sender;
 - (IBAction)orderFrontGoToFolder:(id)sender;
 
-// ==============
-// = Legacy API =
-// ==============
-
+// Used by DocumentCommand.mm
 + (instancetype)controllerForDocument:(document::document_ptr const&)aDocument;
 - (void)bundleItemPreExec:(pre_exec::type)preExec completionHandler:(void(^)(BOOL success))callback;
-
-// Private (used by DocumentCommand.mm)
-@property (nonatomic, readonly) NSString* scopeAttributes;
-- (NSString*)untitledSavePath;
 @end
