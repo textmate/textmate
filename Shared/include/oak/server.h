2ad42430c4275fabbe1b0f6fea83604a41f84d31
Allan Odgaard <git@abetarda.com>
2013-08-05 11:17:04 +0200

    Make client API for oak::server_t thread safe

--- Shared/include/oak/server.h
+++ Shared/include/oak/server.h
@@ -38,6 +38,7 @@ namespace oak
 		cf::callback_ptr run_loop_source;
 		std::vector< std::pair<size_t, ARG> > requests;
 		std::vector< std::pair<size_t, RESULT>  > results;
+		std::recursive_mutex client_lock;
 		pthread_mutex_t requests_mutex;
 		pthread_mutex_t results_mutex;
 		pthread_cond_t cond;
@@ -90,6 +91,7 @@ namespace oak
 	template <typename T, typename ARG, typename RESULT>
 	size_t server_t<T, ARG, RESULT>::register_client (T* callback)
 	{
+		std::lock_guard<std::recursive_mutex> lock(client_lock);
 		client_to_callback.insert(std::make_pair(next_client_key, callback));
 		return next_client_key++;
 	}
@@ -97,6 +99,7 @@ namespace oak
 	template <typename T, typename ARG, typename RESULT>
 	void server_t<T, ARG, RESULT>::unregister_client (size_t clientKey)
 	{
+		std::lock_guard<std::recursive_mutex> lock(client_lock);
 		client_to_callback.erase(client_to_callback.find(clientKey));
 		remove_requests(clientKey);
 	}
@@ -156,6 +159,7 @@ namespace oak
 		results.swap(offload);
 		pthread_mutex_unlock(&results_mutex);
 
+		std::lock_guard<std::recursive_mutex> lock(client_lock);
 		iterate(it, offload)
 		{
 			typename std::map<size_t, T*>::iterator client = client_to_callback.find(it->first);
