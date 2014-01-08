textmate/textmate

commit 1de5d90475788143655a60fdbff654139fec3cb4

Set hugging priority for horizontal dividers
In the HTML output window’s status bar, the divider was taking up too much space (on 10.9).

Frameworks/OakAppKit/src/OakUIConstructionFunctions.mm
@@ -176,5 +176,6 @@ NSImageView* OakCreateDividerImageView ()
 {
 	NSImageView* res = [[NSImageView alloc] initWithFrame:NSZeroRect];
 	[res setImage:[NSImage imageNamed:@"Divider" inSameBundleAsClass:[OakDividerLineView class]]];
+	[res setContentHuggingPriority:NSLayoutPriorityRequired forOrientation:NSLayoutConstraintOrientationHorizontal];
 	return res;
 }

Frameworks/OakFileBrowser/src/ui/OFBHeaderView.mm
@@ -49,12 +49,9 @@ static NSPopUpButton* OakCreateFolderPopUpButton ()
 		[self.goBackButton.cell accessibilitySetOverrideValue:self.goBackButton.toolTip forAttribute:NSAccessibilityDescriptionAttribute];
 		[self.goForwardButton.cell accessibilitySetOverrideValue:self.goForwardButton.toolTip forAttribute:NSAccessibilityDescriptionAttribute];
 
-		NSImageView* divider = OakCreateDividerImageView();
-		[divider setContentCompressionResistancePriority:NSLayoutPriorityDefaultLow forOrientation:NSLayoutConstraintOrientationVertical];
-
 		NSDictionary* views = @{
 			@"folder"   : self.folderPopUpButton,
-			@"divider"  : divider,
+			@"divider"  : OakCreateDividerImageView(),
 			@"back"     : self.goBackButton,
 			@"forward"  : self.goForwardButton,
 		};

commit a87c190c6f839dba36cf8c86267d90e865fcd2a2

Fix file browser height for tabs above document
This fixes regression caused by 1de5d90475788143655a60fdbff654139fec3cb4 that
divider height and default compression priority disallowed expected height.

Frameworks/OakAppKit/src/OakUIConstructionFunctions.mm
@@ -177,5 +177,6 @@ NSImageView* OakCreateDividerImageView ()
 	NSImageView* res = [[NSImageView alloc] initWithFrame:NSZeroRect];
 	[res setImage:[NSImage imageNamed:@"Divider" inSameBundleAsClass:[OakDividerLineView class]]];
 	[res setContentHuggingPriority:NSLayoutPriorityRequired forOrientation:NSLayoutConstraintOrientationHorizontal];
+	[res setContentCompressionResistancePriority:NSLayoutPriorityDefaultLow forOrientation:NSLayoutConstraintOrientationVertical];
 	return res;
 }