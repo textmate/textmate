ff94480298e5b344dfe655877bf5dc4cd56b904d
Allan Odgaard <git@abetarda.com>
2014-04-10 13:07:46 +0700

    Check that CoreFoundation functions succeed
    Several crash reports indicate failure to create a CFString (in ct::line_t’s constructor). As we have an explicit UTF-8 check, I am not sure why it would fail, but at least now TextMate should not crash when it happens.

--- Frameworks/layout/src/ct.cc
+++ Frameworks/layout/src/ct.cc
@@ -99,58 +99,68 @@ namespace ct
 
 	line_t::line_t (std::string const& text, std::map<size_t, scope::scope_t> const& scopes, theme_ptr const& theme, CGColorRef textColor) : _text(text)
 	{
-		crash_reporter_info_t info(text::format("text size: %zu, is valid utf-8: %s, %zu scope(s): %zu-%zu", text.size(), BSTR(utf8::is_valid(text.begin(), text.end())), scopes.size(), scopes.empty() ? 0 : scopes.begin()->first, scopes.empty() ? 0 : (--scopes.end())->first));
 		ASSERT(utf8::is_valid(text.begin(), text.end()));
 		ASSERT(scopes.empty() || (--scopes.end())->first <= text.size());
 
-		CFMutableAttributedStringRef toDraw = CFAttributedStringCreateMutable(kCFAllocatorDefault, 0);
-		for(auto pair = scopes.begin(); pair != scopes.end(); )
+		if(CFMutableAttributedStringRef toDraw = CFAttributedStringCreateMutable(kCFAllocatorDefault, 0))
 		{
-			styles_t const& styles = theme->styles_for_scope(pair->second);
-			size_t i = pair->first;
-			size_t j = ++pair != scopes.end() ? pair->first : text.size();
-
-			if(j < i)
+			for(auto pair = scopes.begin(); pair != scopes.end(); )
 			{
-				info << text::format("bad range: %zu-%zu (at end: %s)", i, j, BSTR(pair == scopes.end()));
-				abort();
-			}
+				styles_t const& styles = theme->styles_for_scope(pair->second);
+				size_t i = pair->first;
+				size_t j = ++pair != scopes.end() ? pair->first : text.size();
 
-			std::string const cStr = text.substr(i, j - i);
-			if(!utf8::is_valid(cStr.begin(), cStr.end()))
-			{
-				info << text::format("range %zu-%zu is not UTF-8:\n%s", i, j, text::to_hex(cStr.begin(), cStr.end()).c_str());
-				abort();
-			}
+				if(j < i)
+				{
+					crash_reporter_info_t info(text::format("bad range: %zu-%zu (at end: %s)", i, j, BSTR(pair == scopes.end())));
+					abort();
+				}
 
-			CFMutableAttributedStringRef str = CFAttributedStringCreateMutable(kCFAllocatorDefault, 0);
-			CFAttributedStringReplaceString(str, CFRangeMake(0, 0), cf::wrap(cStr));
-			CFAttributedStringSetAttribute(str, CFRangeMake(0, CFAttributedStringGetLength(str)), kCTFontAttributeName, styles.font());
-			CFAttributedStringSetAttribute(str, CFRangeMake(0, CFAttributedStringGetLength(str)), kCTForegroundColorAttributeName, textColor ?: styles.foreground());
-			CFAttributedStringSetAttribute(str, CFRangeMake(0, CFAttributedStringGetLength(str)), kCTLigatureAttributeName, cf::wrap(0));
-			if(styles.underlined())
-				_underlines.push_back(std::make_pair(CFRangeMake(CFAttributedStringGetLength(toDraw), CFAttributedStringGetLength(str)), CGColorPtr(CGColorRetain(styles.foreground()), CGColorRelease)));
-			_backgrounds.push_back(std::make_pair(CFRangeMake(CFAttributedStringGetLength(toDraw), CFAttributedStringGetLength(str)), CGColorPtr(CGColorRetain(styles.background()), CGColorRelease)));
-			CFAttributedStringReplaceAttributedString(toDraw, CFRangeMake(CFAttributedStringGetLength(toDraw), 0), str);
-			CFRelease(str);
+				if(!utf8::is_valid(text.begin() + i, text.begin() + j))
+				{
+					crash_reporter_info_t info(text::format("text size: %zu, line is valid utf-8: %s, %zu scope(s): %zu-%zu", text.size(), BSTR(utf8::is_valid(text.begin(), text.end())), scopes.size(), scopes.empty() ? 0 : scopes.begin()->first, scopes.empty() ? 0 : (--scopes.end())->first));
+					info << text::format("range %zu-%zu is not UTF-8:\n%s", i, j, text::to_hex(text.begin() + i, text.begin() + j).c_str());
+					abort();
+				}
+
+				if(CFStringRef cfStr = CFStringCreateWithBytes(kCFAllocatorDefault, (UInt8*)text.data() + i, j - i, kCFStringEncodingUTF8, false))
+				{
+					if(CFMutableAttributedStringRef str = CFAttributedStringCreateMutable(kCFAllocatorDefault, 0))
+					{
+						CFAttributedStringReplaceString(str, CFRangeMake(0, 0), cfStr);
+						CFAttributedStringSetAttribute(str, CFRangeMake(0, CFAttributedStringGetLength(str)), kCTFontAttributeName, styles.font());
+						CFAttributedStringSetAttribute(str, CFRangeMake(0, CFAttributedStringGetLength(str)), kCTForegroundColorAttributeName, textColor ?: styles.foreground());
+						CFAttributedStringSetAttribute(str, CFRangeMake(0, CFAttributedStringGetLength(str)), kCTLigatureAttributeName, cf::wrap(0));
+						if(styles.underlined())
+							_underlines.push_back(std::make_pair(CFRangeMake(CFAttributedStringGetLength(toDraw), CFAttributedStringGetLength(str)), CGColorPtr(CGColorRetain(styles.foreground()), CGColorRelease)));
+						_backgrounds.push_back(std::make_pair(CFRangeMake(CFAttributedStringGetLength(toDraw), CFAttributedStringGetLength(str)), CGColorPtr(CGColorRetain(styles.background()), CGColorRelease)));
+						CFAttributedStringReplaceAttributedString(toDraw, CFRangeMake(CFAttributedStringGetLength(toDraw), 0), str);
+						CFRelease(str);
+					}
+					CFRelease(cfStr);
+				}
+				else
+				{
+					fprintf(stderr, "%s: failed to create CFString for ‘%.*s’\n", getprogname(), int(j - i), text.data() + i);
+				}
+			}
+			_line.reset(CTLineCreateWithAttributedString(toDraw), CFRelease);
 		}
-
-		_line.reset(CTLineCreateWithAttributedString(toDraw), CFRelease);
 	}
 
 	CGFloat line_t::width (CGFloat* ascent, CGFloat* descent, CGFloat* leading) const
 	{
-		return CTLineGetTypographicBounds(_line.get(), ascent, descent, leading);
+		return _line ? CTLineGetTypographicBounds(_line.get(), ascent, descent, leading) : 0;
 	}
 
 	size_t line_t::index_for_offset (CGFloat offset) const
 	{
-		return utf16::advance(_text.data(), CTLineGetStringIndexForPosition(_line.get(), CGPointMake(offset, 0)), _text.data() + _text.size()) - _text.data();
+		return _line ? utf16::advance(_text.data(), CTLineGetStringIndexForPosition(_line.get(), CGPointMake(offset, 0)), _text.data() + _text.size()) - _text.data() : 0;
 	}
 
 	CGFloat line_t::offset_for_index (size_t index) const
 	{
-		return CTLineGetOffsetForStringIndex(_line.get(), utf16::distance(_text.begin(), _text.begin() + index), NULL);
+		return _line ? CTLineGetOffsetForStringIndex(_line.get(), utf16::distance(_text.begin(), _text.begin() + index), NULL) : 0;
 	}
 
 	static void draw_spelling_dot (ng::context_t const& context, CGRect const& rect, bool isFlipped)
@@ -168,6 +178,9 @@ namespace ct
 
 	void line_t::draw_foreground (CGPoint pos, ng::context_t const& context, bool isFlipped, std::vector< std::pair<size_t, size_t> > const& misspelled) const
 	{
+		if(!_line)
+			return;
+
 		for(auto const& pair : _underlines) // Draw our own underline since CoreText does an awful job <rdar://5845224>
 		{
 			CGFloat x1 = round(pos.x + CTLineGetOffsetForStringIndex(_line.get(), pair.first.location, NULL));
@@ -194,6 +207,9 @@ namespace ct
 
 	void line_t::draw_background (CGPoint pos, CGFloat height, ng::context_t const& context, bool isFlipped, CGColorRef currentBackground) const
 	{
+		if(!_line)
+			return;
+
 		for(auto const& pair : _backgrounds)
 		{
 			if(CFEqual(currentBackground, pair.second.get()))
