0ad0b12a8223657d42b3243eb32c412d3ac798ae
Allan Odgaard <git@abetarda.com>
2014-04-25 12:41:17 +0700

    Improve “Open With” submenu
    - Path to application is shown in the menu item’s tool tip.
    - When menu is shown for multiple file types with different default applications, we show the default applications first.
    - Use the potentially localized bundle name rather than file system display name.
    - Use bundle identifier to group applications rather than display name: Grouping is done to decide if version number should be shown for the item.

--- Frameworks/OakAppKit/src/OakOpenWithMenu.mm
+++ Frameworks/OakAppKit/src/OakOpenWithMenu.mm
@@ -2,63 +2,72 @@
 #import "NSMenu Additions.h"
 #import <OakFoundation/NSString Additions.h>
 #import <oak/oak.h>
-#import <text/ctype.h>
-#import <io/path.h>
 
-static std::vector< std::pair<NSString*, NSURL*> > ApplicationURLsForPaths (NSSet* paths)
+static NSURL* CanonicalURL (NSURL* url, BOOL isDirectoryFlag = YES)
 {
-	std::vector< std::pair<NSString*, NSURL*> > res;
-	NSMutableSet* defaultApplicationURLs = [NSMutableSet set];
-	NSMutableSet* allApplicationURLs = [NSMutableSet set];
+	return [NSURL fileURLWithPath:[[url filePathURL] path] isDirectory:isDirectoryFlag];
+}
 
+static NSArray* ApplicationURLsForPaths (NSSet* paths)
+{
+	NSMutableSet* allAppURLs     = [NSMutableSet set];
+	NSMutableSet* defaultAppURLs = [NSMutableSet set];
+
+	bool first = true;
 	for(NSString* path in paths)
 	{
-		CFURLRef fileURL = (CFURLRef)CFBridgingRetain([NSURL fileURLWithPath:path]);
-		NSArray* applicationURLs = (NSArray*)CFBridgingRelease(LSCopyApplicationURLsForURL(fileURL, kLSRolesAll));
-		CFURLRef defaultApplicationURL = nil;
-		if(noErr == LSGetApplicationForURL(fileURL, kLSRolesAll, NULL, &defaultApplicationURL))
+		NSURL* url = [NSURL fileURLWithPath:path];
+
+		NSMutableSet* appUrls = [NSMutableSet set];
+		NSArray* appUrlsArray = (NSArray*)CFBridgingRelease(LSCopyApplicationURLsForURL((__bridge CFURLRef)url, kLSRolesAll));
+		for(NSURL* url in appUrlsArray)
+			[appUrls addObject:CanonicalURL(url)];
+
+		CFURLRef defaultAppURL = nil;
+		if(noErr == LSGetApplicationForURL((__bridge CFURLRef)url, kLSRolesAll, NULL, &defaultAppURL))
 		{
-			NSURL* defaultAppURL = (NSURL*)CFBridgingRelease(defaultApplicationURL);
-			if(![applicationURLs containsObject:defaultAppURL])
-				applicationURLs = [applicationURLs arrayByAddingObject:defaultAppURL];
-			[defaultApplicationURLs addObject:defaultAppURL];
+			NSURL* tmp = CanonicalURL((__bridge NSURL*)defaultAppURL);
+			[appUrls addObject:tmp];
+			[defaultAppURLs addObject:tmp];
+
+			CFRelease(defaultAppURL);
 		}
-		if(allApplicationURLs.count == 0)
-				[allApplicationURLs setSet:[NSSet setWithArray:applicationURLs]];
-		else	[allApplicationURLs intersectSet:[NSSet setWithArray:applicationURLs]];
-		CFRelease(fileURL);
+
+		if(std::exchange(first, false))
+				[allAppURLs setSet:appUrls];
+		else	[allAppURLs intersectSet:appUrls];
 	}
 
-	if(allApplicationURLs.count == 0)
-		return res;
+	NSMutableArray* apps        = [NSMutableArray array];
+	NSMutableDictionary* counts = [NSMutableDictionary dictionary];
 
-	std::map<std::string, std::map<std::string, NSURL*>, text::less_t> apps;
-	for(NSURL* appURL in allApplicationURLs)
+	for(NSURL* url in allAppURLs)
 	{
-		std::string const& appName = path::display_name(appURL.path.UTF8String);
-		NSBundle* appBundle        = [NSBundle bundleWithPath:appURL.path];
-		NSString* appVersion       = [appBundle objectForInfoDictionaryKey:@"CFBundleShortVersionString"] ?: ([appBundle objectForInfoDictionaryKey:@"CFBundleVersion"] ?: @"???");
-		apps[appName][appVersion.UTF8String] = appURL;
-	}
-
-	NSURL* defaultApplicationURL = defaultApplicationURLs.count == 1 ? defaultApplicationURLs.anyObject : nil;
-	for(auto const& appIter : apps)
-	{
-		riterate(versIter, appIter.second)
+		if(NSBundle* bundle = [NSBundle bundleWithURL:url])
 		{
-			NSString* appName = [NSString stringWithCxxString:appIter.first];
-			NSURL* appURL = versIter->second;
-
-			if([defaultApplicationURL isEqual:appURL])
-				appName = [NSString stringWithFormat:@"%@ (default)", appName];
-
-			if(appIter.second.size() > 1) // we have more than one version
-				appName = [NSString stringWithFormat:@"%@ (%@)", appName, [NSString stringWithCxxString:versIter->first]];
-
-			res.insert(([defaultApplicationURL isEqual:appURL] ? res.begin() : res.end()), std::make_pair(appName, appURL));
+			NSString* identifier = [bundle bundleIdentifier];
+			counts[identifier] = @([counts[identifier] intValue] + 1);
+			[apps addObject:@{
+				@"identifier" : identifier,
+				@"name"       : [bundle objectForInfoDictionaryKey:@"CFBundleName"],
+				@"version"    : [bundle objectForInfoDictionaryKey:@"CFBundleShortVersionString"] ?: ([bundle objectForInfoDictionaryKey:@"CFBundleVersion"] ?: @"???"),
+				@"url"        : url,
+				@"isDefault"  : @([defaultAppURLs containsObject:url]),
+			}];
 		}
 	}
-	return res;
+
+	NSMutableArray* res = [NSMutableArray array];
+	for(NSDictionary* app in [apps sortedArrayUsingDescriptors:@[ [NSSortDescriptor sortDescriptorWithKey:@"isDefault" ascending:NO], [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES selector:@selector(localizedCompare:)], [NSSortDescriptor sortDescriptorWithKey:@"version" ascending:NO] ]])
+	{
+		NSString* name = app[@"name"];
+		if([counts[app[@"identifier"]] intValue] > 1)
+			name = [NSString stringWithFormat:@"%@ (%@)", name, app[@"version"]];
+		if([app[@"isDefault"] boolValue] && [defaultAppURLs count] == 1)
+			name = [NSString stringWithFormat:@"%@ (default)", name];
+		[res addObject:@{ @"name" : name, @"url" : app[@"url"], @"isDefault" : app[@"isDefault"] }];
+	}
+	return [res count] == 0 ? nil : res;
 }
 
 @implementation OakOpenWithMenu
@@ -83,31 +92,33 @@ static std::vector< std::pair<NSString*, NSURL*> > ApplicationURLsForPaths (NSSe
 	if(menu.numberOfItems > 0 || !superItem)
 		return;
 
-	std::vector< std::pair<NSString*, NSURL*> > appURLs = ApplicationURLsForPaths(superItem.representedObject);
-
-	if(appURLs.empty())
+	NSArray* apps = ApplicationURLsForPaths(superItem.representedObject);
+	if(!apps)
 	{
 		[menu addItemWithTitle:@"No Suitable Applications Found" action:@selector(nop:) keyEquivalent:@""];
 		return;
 	}
 
-	for(auto const& app : appURLs)
+	BOOL didInsertDefaultItems = NO;
+	for(NSDictionary* app : apps)
 	{
-		NSMenuItem* menuItem = [menu addItemWithTitle:app.first action:@selector(openWith:) keyEquivalent:@""];
+		if(didInsertDefaultItems && ![app[@"isDefault"] boolValue])
+			[menu addItem:[NSMenuItem separatorItem]];
+		didInsertDefaultItems = [app[@"isDefault"] boolValue];
+
+		NSMenuItem* menuItem = [menu addItemWithTitle:app[@"name"] action:@selector(openWith:) keyEquivalent:@""];
 		[menuItem setTarget:self];
-		[menuItem setRepresentedObject:app.second];
+		[menuItem setRepresentedObject:app[@"url"]];
+		[menuItem setToolTip:[[[app[@"url"] filePathURL] path] stringByAbbreviatingWithTildeInPath]];
 
 		NSImage* image = nil;
-		if([app.second getResourceValue:&image forKey:NSURLEffectiveIconKey error:NULL] || (image = [[NSWorkspace sharedWorkspace] iconForFile:[app.second path]]))
+		if([app[@"url"] getResourceValue:&image forKey:NSURLEffectiveIconKey error:NULL] || (image = [[NSWorkspace sharedWorkspace] iconForFile:[app[@"url"] path]]))
 		{
 			image = [image copy];
 			image.size = NSMakeSize(16, 16);
 			[menuItem setImage:image];
 		}
 	}
-
-	if(appURLs.size() > 1)
-		[menu insertItem:[NSMenuItem separatorItem] atIndex:1];
 }
 
 - (void)openWith:(id)sender
