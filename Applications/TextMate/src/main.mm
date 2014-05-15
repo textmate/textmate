c832c6a13112de1cb5263f6bb91eaf4a71390e6f
Allan Odgaard <git@abetarda.com>
2014-05-15 11:59:59 +0700

    Release dispatch sources
    This is to avoid them showing up in leak reports.

--- Applications/TextMate/src/main.mm
+++ Applications/TextMate/src/main.mm
@@ -61,7 +61,11 @@ int main (int argc, char const* argv[])
 	}
 
 	try {
-		return NSApplicationMain(argc, argv);
+		int res = NSApplicationMain(argc, argv);
+		dispatch_release(sigUsr1Src);
+		dispatch_release(sigIntSrc);
+		dispatch_release(sigTermSrc);
+		return res;
 	}
 	catch(std::exception const& e) {
 		crash_reporter_info_t info(text::format("C++ Exception: %s", e.what()));
