a6be5b48631fba345b66a509dcf43686918555c9
Allan Odgaard <git@abetarda.com>
2014-08-02 12:25:57 +0200

    Update some ruby shebangs

--- bin/gen_credits.rb
+++ bin/gen_credits.rb
@@ -1,4 +1,4 @@
-#!/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby -wKU
+#!/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/ruby -w
 # == Synopsis
 #
 # Module to assist in building the Contributors page using git commit history.
