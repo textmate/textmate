f88d6f54250c2aaa17ec0c1e2a5a22b47ae995dc
Allan Odgaard <git@abetarda.com>
2014-08-14 17:31:49 +0200

    Don’t search for crash reports generated on 10.6 or lower

--- Frameworks/CrashReporter/src/find_reports.cc
+++ Frameworks/CrashReporter/src/find_reports.cc
@@ -22,20 +22,14 @@ static CFAbsoluteTime parse_date (std::string const& dateStr, std::string const&
 
 std::map<CFAbsoluteTime, std::string> find_reports (std::string const& process)
 {
-	static std::string const locations[] = {
-		path::join(path::home(), "Library/Logs/DiagnosticReports"),
-		path::join(path::home(), "Library/Logs/CrashReporter") // before Snow Leopard
-	};
+	std::string const location = path::join(path::home(), "Library/Logs/DiagnosticReports");
 
 	std::map<CFAbsoluteTime, std::string> res;
-	for(auto const& location : locations)
+	for(auto const& entry : path::entries(location))
 	{
-		for(auto const& entry : path::entries(location))
-		{
-			std::string file(entry->d_name);
-			if(entry->d_type == DT_REG && file.find(process) == 0)
-				res.emplace(parse_date(file, process + "_%F-\x25H\x25M%S"), path::join(location, entry->d_name));
-		}
+		std::string file(entry->d_name);
+		if(entry->d_type == DT_REG && file.find(process) == 0)
+			res.emplace(parse_date(file, process + "_%F-\x25H\x25M%S"), path::join(location, entry->d_name));
 	}
 	return res;
 }
