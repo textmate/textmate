a4f47a7bfcb24eded4c9aeb4ad3ad4973d147379
Allan Odgaard <git@abetarda.com>
2014-09-10 13:34:11 +0200

    Use Core Animation for the open document animation (zooming icon)

--- Frameworks/OakAppKit/src/OakZoomingIcon.mm
+++ Frameworks/OakAppKit/src/OakZoomingIcon.mm
@@ -4,10 +4,6 @@
 {
 	OBJC_WATCH_LEAKS(OakZoomingIcon);
 }
-@property (nonatomic) NSRect         startFrame;
-@property (nonatomic) NSDate*        startTime;
-@property (nonatomic) NSTimeInterval duration;
-@property (nonatomic) NSTimer*       animationTimer;
 @end
 
 @implementation OakZoomingIcon
@@ -15,11 +11,6 @@
 {
 	if(self = [super initWithContentRect:aRect styleMask:NSBorderlessWindowMask backing:NSBackingStoreBuffered defer:NO])
 	{
-		self.startFrame     = aRect;
-		self.startTime      = [NSDate new];
-		self.duration       = ([[NSApp currentEvent] modifierFlags] & NSShiftKeyMask) ? 4.0 : 0.4;
-		self.animationTimer = [NSTimer scheduledTimerWithTimeInterval:1.0/60.0 target:self selector:@selector(tick:) userInfo:nil repeats:YES];
-
 		self.releasedWhenClosed = NO;
 		self.ignoresMouseEvents = YES;
 		self.backgroundColor    = [NSColor clearColor];
@@ -35,6 +26,22 @@
 		self.contentView = imageView;
 
 		[self orderFront:self];
+
+		[NSAnimationContext beginGrouping];
+
+		[NSAnimationContext currentContext].duration = ([[NSApp currentEvent] modifierFlags] & NSShiftKeyMask) ? 2.5 : 0.25;
+		[NSAnimationContext currentContext].completionHandler = ^{
+			[self orderOut:self];
+		};
+
+		CABasicAnimation* anim = [CABasicAnimation animation];
+		anim.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+
+		self.animations = @{ @"alphaValue" : anim, @"frame" : anim };
+		[self.animator setAlphaValue:0];
+		[self.animator setFrame:NSInsetRect(aRect, -56, -56) display:YES animate:YES];
+
+		[NSAnimationContext endGrouping];
 	}
 	return self;
 }
@@ -42,30 +49,5 @@
 + (OakZoomingIcon*)zoomIcon:(NSImage*)icon fromRect:(NSRect)aRect
 {
 	return [[OakZoomingIcon alloc] initWithIcon:icon rect:aRect];
-}
-
-- (void)tick:(NSTimer*)aTimer
-{
-	NSTimeInterval delta = [[NSDate date] timeIntervalSinceDate:self.startTime];
-	CGFloat t = std::min(1.0, delta / self.duration);
-
-	t = log2(5.0*(t) + 1.0) / log2(6.0);
-	[self setAlphaValue:1.0 - t/1.2];
-
-	CGFloat size = 16 + t * (128-16);
-	NSRect r = NSMakeRect(NSMidX(self.startFrame) - 0.5 * size, NSMidY(self.startFrame) - 0.5 * size, size, size);
-	CGFloat offset = round(0.5 * size);
-	r = NSMakeRect(
-		round(NSMidX(self.startFrame) - offset),
-		round(NSMidY(self.startFrame) - offset),
-		(2 * offset), (2 * offset));
-	[self setFrame:r display:YES];
-
-	if(delta > self.duration)
-	{
-		[self close];
-		[self.animationTimer invalidate];
-		self.animationTimer = nil;
-	}
 }
 @end
