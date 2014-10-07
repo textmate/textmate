23a66a40b9a2de3ca3931139c8a0fa3940b50ce1
Allan Odgaard <git@abetarda.com>
2014-10-07 22:27:59 +0200

    Move FFResultNode to its own file

---
+++ Frameworks/Find/src/FFResultNode.h
@@ -0,0 +1,35 @@
+#import <document/document.h>
+
+@class FFMatch;
+
+@interface FFResultNode : NSObject
+@property (nonatomic, weak) FFResultNode* parent;
+@property (nonatomic, weak) FFResultNode* next;
+@property (nonatomic, weak) FFResultNode* previous;
+@property (nonatomic) NSUInteger countOfLeafs;
+@property (nonatomic) NSUInteger countOfExcluded;
+
++ (FFResultNode*)resultNodeWithMatch:(FFMatch*)aMatch baseDirectory:(NSString*)base;
++ (FFResultNode*)resultNodeWithMatch:(FFMatch*)aMatch;
+
+- (void)addResultNode:(FFResultNode*)aMatch;
+- (void)removeFromParent;
+
+- (FFResultNode*)firstResultNode;
+- (FFResultNode*)lastResultNode;
+
+- (NSUInteger)lineSpan;
+- (NSAttributedString*)excerptWithReplacement:(NSString*)replacementString;
+- (NSImage*)icon;
+
+@property (nonatomic) FFMatch* match;
+@property (nonatomic) NSArray* children;
+@property (nonatomic) BOOL excluded;
+@property (nonatomic) BOOL replacementDone;
+@property (nonatomic) NSImage* icon;
+
+@property (nonatomic, readonly) document::document_ptr document;
+@property (nonatomic, readonly) NSString* path;
+@property (nonatomic, readonly) NSString* identifier;
+@property (nonatomic) NSAttributedString* displayPath;
+@end
