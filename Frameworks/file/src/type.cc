58f98cad23b4bda05d19a409e8516848c9a0ff13
Allan Odgaard <git@abetarda.com>
2014-10-13 22:11:17 +0200

    Ignore unknown file types when finding document’s file type
    The problem is if the user sets a certain file type to open with grammar X and then uninstalls X, there will still be a setting binding the file type to X which means we skip trying to find another suitable grammar.

--- Frameworks/file/src/type.cc
+++ Frameworks/file/src/type.cc
@@ -79,6 +79,11 @@ static std::string file_type_from_bytes (io::bytes_ptr const& bytes)
 	return ordering.empty() ? NULL_STR : file_type_from_grammars(std::vector<bundles::item_ptr>(1, ordering.begin()->second));
 }
 
+static bool unknown_file_type (std::string const& fileType)
+{
+	return fileType == NULL_STR || bundles::query(bundles::kFieldGrammarScope, fileType, scope::wildcard, bundles::kItemTypeGrammar).empty();
+}
+
 static std::string find_file_type (std::string const& path, io::bytes_ptr const& bytes, std::string const& virtualPath = NULL_STR)
 {
 	// FIXME we need to use “current” folder when getting (fileType) settings
@@ -89,27 +94,27 @@ static std::string find_file_type (std::string const& path, io::bytes_ptr const&
 	std::string res = NULL_STR;
 
 	// check if user has an explicit binding for this path (e.g. *.md → Markdown)
-	if(res == NULL_STR && effectivePath != NULL_STR)
+	if(effectivePath != NULL_STR)
 		res = settings_for_path(effectivePath, pathAttributes, settingsDirectory).get(kSettingsFileTypeKey, NULL_STR);
 
 	// check if a grammar recognize the content (e.g. #!/usr/bin/ruby → Ruby)
-	if(res == NULL_STR && bytes)
+	if(unknown_file_type(res) && bytes)
 		res = file_type_from_bytes(bytes);
 
 	// check if a grammar recognize the path extension (.git/config → Git Config)
-	if(res == NULL_STR && effectivePath != NULL_STR)
+	if(unknown_file_type(res) && effectivePath != NULL_STR)
 		res = file_type_from_path(effectivePath);
 
 	// check if there is a setting for untitled files (pathAttributes include ‘attr.untitled’)
-	if(res == NULL_STR && effectivePath == NULL_STR)
+	if(unknown_file_type(res) && effectivePath == NULL_STR)
 		res = settings_for_path(effectivePath, pathAttributes, settingsDirectory).get(kSettingsFileTypeKey, NULL_STR);
 
 	// check if user has a fallback for unrecognized files (to override the “What is the file type of XXX?” prompt)
-	if(res == NULL_STR && effectivePath != NULL_STR)
+	if(unknown_file_type(res) && effectivePath != NULL_STR)
 		res = settings_for_path(effectivePath, "attr.file.unknown-type " + pathAttributes, settingsDirectory).get(kSettingsFileTypeKey, NULL_STR);
 
 	// check if file has no extension, if so, treat it as plain text
-	if(res == NULL_STR && path::extension(effectivePath).empty())
+	if(unknown_file_type(res) && path::extension(effectivePath).empty())
 		res = "text.plain";
 
 	return res;
