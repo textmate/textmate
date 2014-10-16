02d7e287d09b3dfd72e6688e0e1dd8e309d1dc3f
Allan Odgaard <git@abetarda.com>
2014-10-16 23:10:58 +0200

    Remove unused code

--- Frameworks/OakAppKit/src/OakWindowFrameHelper.h
+++
@@ -1,9 +0,0 @@
-#import <oak/debug.h>
-
-@protocol OakWindowFrameHelperDelegate <NSObject>
-- (NSRect)savableWindowFrame;
-@end
-
-PUBLIC @interface OakWindowFrameHelper : NSObject
-+ (OakWindowFrameHelper*)windowFrameHelperWithWindow:(NSWindow*)aWindow;
-@end
