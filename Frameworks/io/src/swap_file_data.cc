textmate/textmate

commit 9b3dd8c0f43eb7bcbe47355bd4ca27e20e6c62b2

Don’t use COPYFILE_METADATA with files on network drives
According to a user report this is causing TextMate to freeze. Reported to Apple as <rdar://17480649>.

Frameworks/io/src/swap_file_data.cc
@@ -37,8 +37,18 @@ namespace path
 		{
 			if(errno == ENOTSUP && access(dst.c_str(), F_OK) == 0)
 			{
-				copyfile(dst.c_str(), src.c_str(), NULL, COPYFILE_METADATA);
-				utimes(src.c_str(), NULL);
+				// Skip COPYFILE_METADATA for network drives <rdar://17480649>
+				if(path::is_local(src))
+				{
+					copyfile(dst.c_str(), src.c_str(), NULL, COPYFILE_METADATA);
+					utimes(src.c_str(), NULL);
+				}
+				else
+				{
+					struct stat sbuf;
+					if(stat(src.c_str(), &sbuf) == 0)
+						chmod(dst.c_str(), sbuf.st_mode & (S_IRWXU|S_IRWXG|S_IRWXO));
+				}
 			}
 
 			if(::rename(src.c_str(), dst.c_str()) == 0)

commit d59029b70c293bf12b55e0996541b47cc60d5136

File flags were lost when saving to a non-local file system
This broke in 9b3dd8c.

Frameworks/io/src/swap_file_data.cc
@@ -46,8 +46,8 @@ namespace path
 				else
 				{
 					struct stat sbuf;
-					if(stat(src.c_str(), &sbuf) == 0)
-						chmod(dst.c_str(), sbuf.st_mode & (S_IRWXU|S_IRWXG|S_IRWXO));
+					if(stat(dst.c_str(), &sbuf) == 0)
+						chmod(src.c_str(), sbuf.st_mode & (S_IRWXU|S_IRWXG|S_IRWXO));
 				}
 			}
 