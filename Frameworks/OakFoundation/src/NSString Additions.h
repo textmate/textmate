67184816be234560119cbab06a8852f79436eac6
Ronald Wampler <rdwampler@gmail.com>
2014-11-10 14:31:02 -0500

    fixup! Remove NSString `existAtPath` category method

--- Frameworks/OakFoundation/src/NSString Additions.h
+++ Frameworks/OakFoundation/src/NSString Additions.h
@@ -1,4 +1,4 @@
-@interface NSString (Addition)
+@interface NSString (Additions)
 + (NSString*)stringWithUTF8String:(char const*)aString length:(unsigned)aLength;
 + (NSString*)stringWithCxxString:(std::string const&)aString;
 @end
