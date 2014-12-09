88dc86882a479210324f682876cf301133c966a0
Ronald Wampler <rdwampler@gmail.com>
2014-12-08 23:03:39 -0500

    Make `scm_enabled_for_path` API public

--- Frameworks/scm/src/scm.h
+++ Frameworks/scm/src/scm.h
@@ -45,6 +45,7 @@ namespace scm
 
 	PUBLIC void disable ();
 	PUBLIC void enable ();
+	PUBLIC bool scm_enabled_for_path (std::string const& path);
 	PUBLIC std::string root_for_path (std::string const& path);
 	PUBLIC info_ptr info (std::string path);
 	PUBLIC void wait_for_status (info_ptr info);
