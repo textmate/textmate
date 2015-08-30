975fd707797e2dc77d8cb0e715066c4aeb5b671a
Allan Odgaard <git@abetarda.com>
2015-08-30 10:53:24 +0200

    Re-encode redundantly encoded multi-byte sequences
    If our buffer contained such sequences then an exception could be thrown when creating an NSString representation, for example copying to the system clipboard or displaying search results in the UI.

--- Frameworks/OakFoundation/src/NSString Additions.mm
+++ Frameworks/OakFoundation/src/NSString Additions.mm
@@ -1,16 +1,26 @@
 #import "NSString Additions.h"
 #import <text/utf8.h>
+#import <oak/oak.h>
 
 @implementation NSString (Additions)
 + (NSString*)stringWithUTF8String:(char const*)aString length:(unsigned)aLength
 {
 	ASSERT(utf8::is_valid(aString, aString + aLength));
-	return [[NSString alloc] initWithBytes:aString length:aLength encoding:NSUTF8StringEncoding];
+
+	NSString* res = [[NSString alloc] initWithBytes:aString length:aLength encoding:NSUTF8StringEncoding];
+	if(!res && aLength && utf8::is_valid(aString, aString + aLength))
+	{
+		// Re-encode every code point to remove redundant encodings, which NSString does not like.
+		std::string tmp;
+		foreach(ch, utf8::make(aString), utf8::make(aString + aLength))
+			tmp += utf8::to_s(*ch);
+		res = [[NSString alloc] initWithBytes:tmp.data() length:tmp.size() encoding:NSUTF8StringEncoding];
+	}
+	return res;
 }
 
 + (NSString*)stringWithCxxString:(std::string const&)aString
 {
-	ASSERT(utf8::is_valid(aString.begin(), aString.end()));
-	return aString == NULL_STR ? nil : [[NSString alloc] initWithBytes:aString.data() length:aString.size() encoding:NSUTF8StringEncoding];
+	return aString == NULL_STR ? nil : [self stringWithUTF8String:aString.data() length:aString.size()];
 }
 @end
