b1fcb9dea436aa6544b7ba078a7af0c7c5eea102
Allan Odgaard <git@abetarda.com>
2016-06-29 11:02:45 +0200

    Update bundles preferences to new BundlesManager API
    We now use an array controller and bindings for the table view.

--- Frameworks/Preferences/src/BundlesPreferences.h
+++ Frameworks/Preferences/src/BundlesPreferences.h
@@ -1,6 +1,5 @@
 #import <MASPreferences/MASPreferencesViewController.h>
 #import <MGScopeBar/MGScopeBarDelegateProtocol.h>
-#import <updater/updater.h>
 
 @class BundlesManager;
 
@@ -10,14 +9,7 @@
 	IBOutlet NSTableView* bundlesTableView;
 	IBOutlet NSTextField* activityTextField;
 	IBOutlet NSSearchField* searchField;
-
-	NSString* filterString;
-	NSString* sortColumnIdentifier;
-	BOOL sortAscending;
-
-	std::vector<std::string> categories;
-	std::set<std::string> enabledCategories;
-	std::vector<bundles_db::bundle_ptr> bundles;
+	IBOutlet NSArrayController* arrayController;
 }
 @property (nonatomic, readonly) NSString* identifier;
 @property (nonatomic, readonly) NSImage*  toolbarItemImage;
--- Frameworks/Preferences/src/BundlesPreferences.mm
+++ Frameworks/Preferences/src/BundlesPreferences.mm
@@ -10,15 +10,62 @@
 #import <text/ctype.h>
 #import <text/decode.h>
 
-static std::string textify (std::string str)
+static NSMutableSet* BundlesBeingInstalled = [NSMutableSet set];
+
+@interface Bundle (BundlesInstallPreferences)
+@property (nonatomic) NSCellStateValue installedCellState;
+@end
+
+@implementation Bundle (BundlesInstallPreferences)
++ (NSSet*)keyPathsForValuesAffectingInstalledCellState
 {
-	str = format_string::replace(str, "\\A\\s+|<[^>]*>|\\s+\\z", "");
-	str = format_string::replace(str, "\\s+", " ");
-	str = decode::entities(str);
-	return str;
+	return [NSSet setWithObjects:@"installed", nil];
 }
 
+- (NSCellStateValue)installedCellState
+{
+	auto res = self.isInstalled ? NSOnState : NSOffState;
+	return [BundlesBeingInstalled containsObject:self] ? NSMixedState : res;
+}
+
+- (void)setInstalledCellState:(NSCellStateValue)newValue
+{
+	BundlesManager* manager = [BundlesManager sharedInstance];
+	if(newValue == NSOffState)
+	{
+		[manager uninstallBundle:self];
+		manager.activityText = [NSString stringWithFormat:@"Uninstalled ‘%@’.", self.name];
+	}
+	else if(![BundlesBeingInstalled containsObject:self])
+	{
+		[BundlesBeingInstalled addObject:self];
+
+		manager.isBusy = YES;
+		manager.activityText = [NSString stringWithFormat:@"Installing ‘%@’…", self.name];
+
+		[manager installBundles:@[ self ] completionHandler:^(NSArray<Bundle*>* bundles){
+			[self willChangeValueForKey:@"installedCellState"];
+			[BundlesBeingInstalled removeObject:self];
+			[self didChangeValueForKey:@"installedCellState"];
+
+			if(!self.installed)
+				manager.activityText = [NSString stringWithFormat:@"Error installing ‘%@’.", self.name];
+			else if(bundles.count == 1)
+				manager.activityText = [NSString stringWithFormat:@"Installed ‘%@’.", self.name];
+			else if(bundles.count == 2)
+				manager.activityText = [NSString stringWithFormat:@"Installed ‘%@’ and one dependency.", self.name];
+			else
+				manager.activityText = [NSString stringWithFormat:@"Installed ‘%@’ and %ld dependencies.", self.name, bundles.count-1];
+			manager.isBusy = NO;
+		}];
+	}
+}
+@end
+
 @interface BundlesPreferences ()
+{
+	NSMutableSet* enabledCategories;
+}
 @property (nonatomic) BundlesManager* bundlesManager;
 @end
 
@@ -28,75 +75,14 @@ static std::string textify (std::string str)
 - (NSString*)toolbarItemLabel      { return @"Bundles"; }
 - (NSView*)initialKeyView          { return bundlesTableView; }
 
-- (void)sortBundlesByColumnWithIdentifier:(NSString*)anIdentifier ascending:(BOOL)ascendingFlag
-{
-	text::less_t lessThan;
-
-	if([anIdentifier isEqualToString:@"installed"])
-		std::stable_sort(bundles.begin(), bundles.end(), [](bundles_db::bundle_ptr lhs, bundles_db::bundle_ptr rhs){ return lhs.get()->installed() && !rhs.get()->installed(); });
-	else if([anIdentifier isEqualToString:@"name"])
-		std::sort(bundles.begin(), bundles.end(), [&lessThan](bundles_db::bundle_ptr lhs, bundles_db::bundle_ptr rhs){ return lessThan(lhs.get()->name(), rhs.get()->name()); });
-	else if([anIdentifier isEqualToString:@"date"])
-		std::sort(bundles.begin(), bundles.end(), [](bundles_db::bundle_ptr lhs, bundles_db::bundle_ptr rhs){ return (rhs.get()->installed() ? rhs.get()->path_updated() : rhs.get()->url_updated()) < (lhs.get()->installed() ? lhs.get()->path_updated() : lhs.get()->url_updated()); });
-	else if([anIdentifier isEqualToString:@"description"])
-		std::sort(bundles.begin(), bundles.end(), [&lessThan](bundles_db::bundle_ptr lhs, bundles_db::bundle_ptr rhs){ return lessThan(textify(lhs.get()->description()), textify(rhs.get()->description())); });
-
-	if(!ascendingFlag)
-		std::reverse(bundles.begin(), bundles.end());
-}
-
-- (void)bundlesDidChange:(id)sender
-{
-	std::set<std::string, text::less_t> set;
-	for(size_t i = 0; i < [_bundlesManager numberOfBundles]; ++i)
-	{
-		bundles_db::bundle_ptr bundle = [_bundlesManager bundleAtIndex:i];
-		if(bundle->category() != NULL_STR)
-				set.insert(bundle->category());
-		else	NSLog(@"%s No category for bundle: %s", sel_getName(_cmd), bundle->name().c_str());
-	}
-
-	if(categories != std::vector<std::string>(set.begin(), set.end()))
-	{
-		categories = std::vector<std::string>(set.begin(), set.end());
-		[categoriesScopeBar reloadData];
-	}
-
-	bundles.clear();
-	for(size_t i = 0; i < [_bundlesManager numberOfBundles]; ++i)
-	{
-		bundles_db::bundle_ptr bundle = [_bundlesManager bundleAtIndex:i];
-		if(enabledCategories.empty() || enabledCategories.find(bundle->category()) != enabledCategories.end())
-		{
-			if(OakIsEmptyString(filterString))
-			{
-				bundles.push_back(bundle);
-			}
-			else
-			{
-				std::string filter = text::lowercase(to_s(filterString));
-				if(text::lowercase(bundle->name()).find(filter) != std::string::npos)
-					bundles.push_back(bundle);
-			}
-		}
-	}
-
-	[self sortBundlesByColumnWithIdentifier:sortColumnIdentifier ascending:sortAscending];
-	[bundlesTableView reloadData];
-}
-
 - (id)init
 {
 	if(self = [super initWithNibName:@"BundlesPreferences" bundle:[NSBundle bundleForClass:[self class]]])
 	{
 		[MGScopeBar class]; // Ensure that we reference the class so that the linker doesn’t strip the framework
 
-		sortColumnIdentifier = @"name";
-		sortAscending        = YES;
-
-		self.bundlesManager = [BundlesManager sharedInstance];
-		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(bundlesDidChange:) name:BundlesManagerBundlesDidChangeNotification object:_bundlesManager];
-		[self bundlesDidChange:self];
+		_bundlesManager = [BundlesManager sharedInstance];
+		enabledCategories = [NSMutableSet set];
 	}
 	return self;
 }
@@ -104,11 +90,12 @@ static std::string textify (std::string str)
 - (void)awakeFromNib
 {
 	[bundlesTableView setIndicatorImage:[NSImage imageNamed:@"NSAscendingSortIndicator"] inTableColumn:[bundlesTableView tableColumnWithIdentifier:@"name"]];
-}
-
-- (void)dealloc
-{
-	[[NSNotificationCenter defaultCenter] removeObserver:self];
+	arrayController.sortDescriptors = @[
+		[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES selector:@selector(localizedCompare:)],
+		[NSSortDescriptor sortDescriptorWithKey:@"installed" ascending:YES],
+		[NSSortDescriptor sortDescriptorWithKey:@"downloadLastUpdated" ascending:YES],
+		[NSSortDescriptor sortDescriptorWithKey:@"textSummary" ascending:YES selector:@selector(localizedCompare:)]
+	];
 }
 
 // =======================
@@ -122,13 +109,16 @@ static std::string textify (std::string str)
 
 - (NSArray*)scopeBar:(MGScopeBar*)theScopeBar itemIdentifiersForGroup:(int)groupNumber
 {
-	NSMutableArray* res = [NSMutableArray array];
-	if(groupNumber == 0)
+	if(groupNumber != 0)
+		return @[ ];
+
+	NSMutableSet* set = [NSMutableSet set];
+	for(Bundle* bundle in _bundlesManager.bundles)
 	{
-		for(auto const& category : categories)
-			[res addObject:[NSString stringWithCxxString:category]];
+		if(NSString* category = bundle.category)
+			[set addObject:category];
 	}
-	return res;
+	return [[set allObjects] sortedArrayUsingSelector:@selector(localizedCompare:)];
 }
 
 - (NSString*)scopeBar:(MGScopeBar*)theScopeBar labelForGroup:(int)groupNumber
@@ -149,9 +139,9 @@ static std::string textify (std::string str)
 - (void)scopeBar:(MGScopeBar*)theScopeBar selectedStateChanged:(BOOL)selected forItem:(NSString*)identifier inGroup:(int)groupNumber
 {
 	if(selected)
-			enabledCategories.insert(to_s(identifier));
-	else	enabledCategories.erase(to_s(identifier));
-	[self bundlesDidChange:self];
+			[enabledCategories addObject:identifier];
+	else	[enabledCategories removeObject:identifier];
+	[self filterStringDidChange:self];
 }
 
 - (NSView*)accessoryViewForScopeBar:(MGScopeBar*)theScopeBar
@@ -161,8 +151,13 @@ static std::string textify (std::string str)
 
 - (IBAction)filterStringDidChange:(id)sender
 {
-	filterString = searchField.stringValue;
-	[self bundlesDidChange:self];
+	NSMutableArray* predicates = [NSMutableArray array];
+	if(OakNotEmptyString(searchField.stringValue))
+		[predicates addObject:[NSPredicate predicateWithFormat:@"name CONTAINS[cd] %@", searchField.stringValue]];
+	if(enabledCategories.count)
+		[predicates addObject:[NSPredicate predicateWithFormat:@"category IN %@", enabledCategories]];
+	arrayController.filterPredicate = [NSCompoundPredicate andPredicateWithSubpredicates:predicates];
+	[arrayController rearrangeObjects];
 }
 
 // ========================
@@ -171,32 +166,51 @@ static std::string textify (std::string str)
 
 - (void)tableView:(NSTableView*)aTableView didClickTableColumn:(NSTableColumn*)aTableColumn
 {
-	NSArray* const sortableColumns = @[ @"installed", @"name", @"date", @"description" ];
+	NSDictionary* map = @{
+		@"name"         : @"name",
+		@"installed"    : @"installed",
+		@"date"         : @"downloadLastUpdated",
+		@"description"  : @"textSummary"
+	};
 
-	NSString* columnIdentifier = [aTableColumn identifier];
-	if(![sortableColumns containsObject:columnIdentifier])
+	NSString* key = map[aTableColumn.identifier];
+	if(!key)
 		return;
 
-	BOOL ascending = [columnIdentifier isEqualToString:sortColumnIdentifier] ? !sortAscending : YES;
-	[self sortBundlesByColumnWithIdentifier:columnIdentifier ascending:ascending];
+	NSMutableArray* descriptors = [arrayController.sortDescriptors mutableCopy];
 
-	sortColumnIdentifier = columnIdentifier;
-	sortAscending        = ascending;
+	NSInteger i = 0;
+	while(i < descriptors.count && ![arrayController.sortDescriptors[i].key isEqualToString:key])
+		++i;
+
+	if(i == descriptors.count)
+		return;
+
+	NSSortDescriptor* descriptor = descriptors[i];
+	descriptor = i == 0 || !descriptor.ascending ? [descriptor reversedSortDescriptor] : descriptor;
+	[descriptors removeObjectAtIndex:i];
+	[descriptors insertObject:descriptor atIndex:0];
+
+	arrayController.sortDescriptors = descriptors;
 
 	for(NSTableColumn* tableColumn in [bundlesTableView tableColumns])
 		[aTableView setIndicatorImage:nil inTableColumn:tableColumn];
-	[aTableView setIndicatorImage:[NSImage imageNamed:(ascending ? @"NSAscendingSortIndicator" : @"NSDescendingSortIndicator")] inTableColumn:aTableColumn];
-
-	[aTableView reloadData];
+	[aTableView setIndicatorImage:[NSImage imageNamed:(descriptor.ascending ? @"NSAscendingSortIndicator" : @"NSDescendingSortIndicator")] inTableColumn:aTableColumn];
 }
 
-- (void)tableView:(NSTableView*)aTableView willDisplayCell:(id)aCell forTableColumn:(NSTableColumn*)aTableColumn row:(int)rowIndex
+- (void)tableView:(NSTableView*)aTableView willDisplayCell:(id)aCell forTableColumn:(NSTableColumn*)aTableColumn row:(NSInteger)rowIndex
 {
 	if([[aTableColumn identifier] isEqualToString:@"link"])
 	{
-		BOOL enabled = bundles[rowIndex]->html_url() != NULL_STR;
+		Bundle* bundle = arrayController.arrangedObjects[rowIndex];
+		BOOL enabled = bundle.htmlURL ? YES : NO;
 		[aCell setEnabled:enabled];
 		[aCell setImage:enabled ? [NSImage imageNamed:@"NSFollowLinkFreestandingTemplate"] : nil];
+	}
+	else if([[aTableColumn identifier] isEqualToString:@"installed"])
+	{
+		Bundle* bundle = arrayController.arrangedObjects[rowIndex];
+		[aCell setEnabled:!bundle.isMandatory || !bundle.isInstalled];
 	}
 }
 
@@ -204,8 +218,8 @@ static std::string textify (std::string str)
 {
 	if([[aTableColumn identifier] isEqualToString:@"installed"])
 	{
-		if([_bundlesManager installStateForBundle:bundles[rowIndex]] != NSMixedState)
-			return YES;
+		Bundle* bundle = arrayController.arrangedObjects[rowIndex];
+		return bundle.isInstalled != -1;
 	}
 	return NO;
 }
@@ -216,54 +230,11 @@ static std::string textify (std::string str)
 	return clickedColumn != [aTableView columnWithIdentifier:@"installed"] && clickedColumn != [aTableView columnWithIdentifier:@"link"];
 }
 
-// ==========================
-// = NSTableView DataSource =
-// ==========================
-
-- (NSInteger)numberOfRowsInTableView:(NSTableView*)aTableView
-{
-	return bundles.size();
-}
-
-- (id)tableView:(NSTableView*)aTableView objectValueForTableColumn:(NSTableColumn*)aTableColumn row:(NSInteger)rowIndex
-{
-	bundles_db::bundle_ptr bundle = bundles[rowIndex];
-	if([[aTableColumn identifier] isEqualToString:@"installed"])
-	{
-		return @([_bundlesManager installStateForBundle:bundle]);
-	}
-	else if([[aTableColumn identifier] isEqualToString:@"name"])
-	{
-		return [NSString stringWithCxxString:bundle->name()];
-	}
-	else if([[aTableColumn identifier] isEqualToString:@"date"])
-	{
-		oak::date_t updated = bundle->installed() ? bundle->path_updated() : bundle->url_updated();
-		return [[NSDate dateWithTimeIntervalSinceReferenceDate:updated.value()] humanReadableTimeElapsed];
-	}
-	else if([[aTableColumn identifier] isEqualToString:@"description"])
-	{
-		return [NSString stringWithCxxString:textify(bundle->description())];
-	}
-	return nil;
-}
-
-- (void)tableView:(NSTableView*)aTableView setObjectValue:(id)anObject forTableColumn:(NSTableColumn*)aTableColumn row:(NSInteger)rowIndex
-{
-	if([[aTableColumn identifier] isEqualToString:@"installed"])
-	{
-		bundles_db::bundle_ptr bundle = bundles[rowIndex];
-		if([anObject boolValue])
-				[_bundlesManager installBundle:bundle completionHandler:nil];
-		else	[_bundlesManager uninstallBundle:bundle];
-	}
-}
-
 - (IBAction)didClickBundleLink:(NSTableView*)aTableView
 {
 	NSInteger rowIndex = [aTableView clickedRow];
-	bundles_db::bundle_ptr bundle = bundles[rowIndex];
-	if(bundle->html_url() != NULL_STR)
-		[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:[NSString stringWithCxxString:bundle->html_url()]]];
+	Bundle* bundle = arrayController.arrangedObjects[rowIndex];
+	if(bundle.htmlURL)
+		[[NSWorkspace sharedWorkspace] openURL:bundle.htmlURL];
 }
 @end
