textmate/textmate

commit 8c19532531b85539e998472ad3c502013e5351e4

Don’t change user defaults in background queue
The problem is that when we change user defaults a notification is posted, and this runs on the thread that did the change, so our own “user defaults did change” notification callbacks are executed on a background thread.

Applications/TextMate/src/AboutWindowController.mm
@@ -271,9 +271,9 @@ static NSTextField* OakCreateTextField ()
 			{
 				dispatch_async(dispatch_get_main_queue(), ^{
 					[[AboutWindowController sharedInstance] showChangesWindow:self];
+					[[NSUserDefaults standardUserDefaults] setObject:currentDigest forKey:kUserDefaultsReleaseNotesDigestKey];
 				});
 			}
-			[[NSUserDefaults standardUserDefaults] setObject:currentDigest forKey:kUserDefaultsReleaseNotesDigestKey];
 		}
 	});
 }

commit cc7ff6e6315f3593c2a6c4f55b023a1f558b54f5

Always store release notes digest in user defaults
Commit 8c19532531b85539e998472ad3c502013e5351e4 broke it so that a digest would only be stored if one was already there, meaning that new users (since November 2015) would not see release notes after update.

Applications/TextMate/src/AboutWindowController.mm
@@ -267,13 +267,11 @@ static NSTextField* OakCreateTextField ()
 		{
 			NSData* lastDigest    = [[NSUserDefaults standardUserDefaults] dataForKey:kUserDefaultsReleaseNotesDigestKey];
 			NSData* currentDigest = Digest(releaseNotes);
-			if(lastDigest && ![lastDigest isEqualToData:currentDigest])
-			{
-				dispatch_async(dispatch_get_main_queue(), ^{
+			dispatch_async(dispatch_get_main_queue(), ^{
+				if(lastDigest && ![lastDigest isEqualToData:currentDigest])
 					[[AboutWindowController sharedInstance] showChangesWindow:self];
-					[[NSUserDefaults standardUserDefaults] setObject:currentDigest forKey:kUserDefaultsReleaseNotesDigestKey];
-				});
-			}
+				[[NSUserDefaults standardUserDefaults] setObject:currentDigest forKey:kUserDefaultsReleaseNotesDigestKey];
+			});
 		}
 	});
 }