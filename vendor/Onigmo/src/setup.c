d2d56aaee7686194c9c55a175bd55c49fc54bde4
Allan Odgaard <git@abetarda.com>
2016-07-10 11:49:52 +0200

    Explicitly call onig_init()
    This is called the first time we create a regular expression, but first use of the library could be concurrent (at least for some of the tests).

--- vendor/Onigmo/src/setup.c
+++ vendor/Onigmo/src/setup.c
@@ -2,6 +2,8 @@
 
 __attribute__((constructor)) static void setup ()
 {
+        onig_init();
+
 	static OnigSyntaxType syntax;
 	onig_copy_syntax(&syntax, ONIG_SYNTAX_RUBY);
 	onig_set_syntax_options(&syntax, ONIG_OPTION_OFF(syntax.options, ONIG_OPTION_ASCII_RANGE));
