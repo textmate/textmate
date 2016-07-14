4854139426db132c39e764660a0e9dc30b0c2b1f
Allan Odgaard <git@abetarda.com>
2016-07-14 19:25:36 +0200

    Remove use of document::run from tests

--- Frameworks/editor/tests/t_command.cc
+++ Frameworks/editor/tests/t_command.cc
@@ -1,8 +1,27 @@
 #include <editor/editor.h>
-#include <document/collection.h>
+#include <command/runner.h>
 
 void test_replace_selection_command ()
 {
+	struct delegate_t : command::delegate_t
+	{
+		delegate_t (ng::editor_t& editor) : _editor(editor) { }
+
+		ng::ranges_t write_unit_to_fd (int fd, input::type unit, input::type fallbackUnit, input_format::type format, scope::selector_t const& scopeSelector, std::map<std::string, std::string>& variables, bool* inputWasSelection) { close(fd); return { }; }
+
+		void show_document (std::string const& str) { }
+		void show_tool_tip (std::string const& str) { }
+		void show_error (bundle_command_t const& command, int rc, std::string const& out, std::string const& err) { }
+
+		bool accept_result (std::string const& out, output::type placement, output_format::type format, output_caret::type outputCaret, ng::ranges_t const& inputRanges, std::map<std::string, std::string> const& environment)
+		{
+			return _editor.handle_result(out, placement, format, outputCaret, inputRanges, environment);
+		}
+
+	private:
+		ng::editor_t& _editor;
+	};
+
 	static std::string const plistSrc =
 		"{ command = '#!/bin/sh\necho Hello';"
 		"  input = 'document';"
@@ -13,8 +32,10 @@ void test_replace_selection_command ()
 	ng::editor_t editor(buf);
 	editor.insert("to be replaced");
 	editor.perform(ng::kSelectAll);
-	editor.execute_dispatch(boost::get<plist::dictionary_t>(plist::parse(plistSrc)), std::map<std::string, std::string>(), [](bundle_command_t const& cmd, ng::buffer_api_t const& buf, ng::ranges_t const& sel, std::map<std::string, std::string> const& env){
-		document::run(cmd, buf, sel, document::document_ptr(), env);
+	editor.execute_dispatch(boost::get<plist::dictionary_t>(plist::parse(plistSrc)), std::map<std::string, std::string>(), [&editor](bundle_command_t const& cmd, ng::buffer_api_t const& buf, ng::ranges_t const& sel, std::map<std::string, std::string> const& env){
+		command::runner_ptr runner = command::runner(cmd, buf, sel, env, std::make_shared<delegate_t>(editor));
+		runner->launch();
+		runner->wait_for_command();
 	});
-	// OAK_ASSERT_EQ(editor.as_string(), "Hello\n");
+	OAK_ASSERT_EQ(editor.as_string(), "Hello\n");
 }
--- Frameworks/editor/tests/t_macro.cc
+++ Frameworks/editor/tests/t_macro.cc
@@ -12,7 +12,7 @@ void test_insert ()
 	ng::buffer_t buf;
 	ng::editor_t editor(buf);
 	editor.macro_dispatch(boost::get<plist::dictionary_t>(plist::parse(plistSrc)), std::map<std::string, std::string>(), [](bundle_command_t const& cmd, ng::buffer_api_t const& buf, ng::ranges_t const& sel, std::map<std::string, std::string> const& env){
-		document::run(cmd, buf, sel, document::document_ptr(), env);
+		OAK_ASSERT(false); // Macro does not contain any commands
 	});
 	OAK_ASSERT_EQ(editor.as_string(), "Test");
 }
@@ -37,13 +37,32 @@ void test_snippet ()
 	ng::buffer_t buf;
 	ng::editor_t editor(buf);
 	editor.macro_dispatch(boost::get<plist::dictionary_t>(plist::parse(plistSrc)), std::map<std::string, std::string>(), [](bundle_command_t const& cmd, ng::buffer_api_t const& buf, ng::ranges_t const& sel, std::map<std::string, std::string> const& env){
-		document::run(cmd, buf, sel, document::document_ptr(), env);
+		OAK_ASSERT(false); // Macro does not contain any commands
 	});
 	OAK_ASSERT_EQ(editor.as_string(), "2010-01-25");
 }
 
 void test_command ()
 {
+	struct delegate_t : command::delegate_t
+	{
+		delegate_t (ng::editor_t& editor) : _editor(editor) { }
+
+		ng::ranges_t write_unit_to_fd (int fd, input::type unit, input::type fallbackUnit, input_format::type format, scope::selector_t const& scopeSelector, std::map<std::string, std::string>& variables, bool* inputWasSelection) { close(fd); return { }; }
+
+		void show_document (std::string const& str) { }
+		void show_tool_tip (std::string const& str) { }
+		void show_error (bundle_command_t const& command, int rc, std::string const& out, std::string const& err) { }
+
+		bool accept_result (std::string const& out, output::type placement, output_format::type format, output_caret::type outputCaret, ng::ranges_t const& inputRanges, std::map<std::string, std::string> const& environment)
+		{
+			return _editor.handle_result(out, placement, format, outputCaret, inputRanges, environment);
+		}
+
+	private:
+		ng::editor_t& _editor;
+	};
+
 	static std::string const plistSrc = "{ commands = ("
 		"{ command = 'selectAll:'; },"
 		"{ command = 'executeCommandWithOptions:';"
@@ -58,8 +77,10 @@ void test_command ()
 	ng::buffer_t buf;
 	ng::editor_t editor(buf);
 	editor.insert("to be replaced");
-	editor.macro_dispatch(boost::get<plist::dictionary_t>(plist::parse(plistSrc)), std::map<std::string, std::string>(), [](bundle_command_t const& cmd, ng::buffer_api_t const& buf, ng::ranges_t const& sel, std::map<std::string, std::string> const& env){
-		document::run(cmd, buf, sel, document::document_ptr(), env);
+	editor.macro_dispatch(boost::get<plist::dictionary_t>(plist::parse(plistSrc)), std::map<std::string, std::string>(), [&editor](bundle_command_t const& cmd, ng::buffer_api_t const& buf, ng::ranges_t const& sel, std::map<std::string, std::string> const& env){
+		command::runner_ptr runner = command::runner(cmd, buf, sel, env, std::make_shared<delegate_t>(editor));
+		runner->launch();
+		runner->wait_for_command();
 	});
-	// OAK_ASSERT_EQ(editor.as_string(), "Hello\n");
+	OAK_ASSERT_EQ(editor.as_string(), "Hello\n");
 }
