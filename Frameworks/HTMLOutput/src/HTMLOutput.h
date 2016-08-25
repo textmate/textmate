14782c0aea64a2569cc386e4f023fcf68d31f25d
Allan Odgaard <git@abetarda.com>
2016-08-25 16:08:29 +0200

    Remove old custom URL protocol

--- Frameworks/HTMLOutput/src/HTMLOutput.h
+++ Frameworks/HTMLOutput/src/HTMLOutput.h
@@ -1,7 +1,5 @@
 #include <oak/misc.h>
 
-extern NSString* const kCommandRunnerURLScheme;
-
 @interface OakHTMLOutputView : NSView
 - (void)loadRequest:(NSURLRequest*)aRequest environment:(std::map<std::string, std::string> const&)anEnvironment autoScrolls:(BOOL)flag;
 - (void)stopLoadingWithUserInteraction:(BOOL)askUserFlag completionHandler:(void(^)(BOOL didStop))handler;
@@ -14,6 +12,3 @@ extern NSString* const kCommandRunnerURLScheme;
 @property (nonatomic, readonly) WebView* webView;
 @property (nonatomic, readonly) BOOL needsNewWebView;
 @end
-
-namespace command { struct runner_t; typedef std::shared_ptr<runner_t> runner_ptr; }
-PUBLIC NSURLRequest* URLRequestForCommandRunner (command::runner_ptr aRunner);
