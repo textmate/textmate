a4b8abe73c786ff443a76f2eb1e707d061c467e3
Allan Odgaard <git@abetarda.com>
2016-08-28 13:25:51 +0200

    Disable “tightening” of truncated match results
    When we tighten the text then it doesn’t line up properly (especially line numbers) which looks off.

--- Frameworks/Find/src/FFResultNode.mm
+++ Frameworks/Find/src/FFResultNode.mm
@@ -15,6 +15,8 @@ namespace
 		{
 			NSMutableParagraphStyle* paragraph = [NSMutableParagraphStyle new];
 			[paragraph setLineBreakMode:mode];
+			if([paragraph respondsToSelector:@selector(setAllowsDefaultTighteningForTruncation:)]) // MAC_OS_X_VERSION_10_11
+				[paragraph setAllowsDefaultTighteningForTruncation:NO];
 
 			_string = [[NSMutableAttributedString alloc] init];
 			_attributes.push_back(@{ NSParagraphStyleAttributeName : paragraph });
