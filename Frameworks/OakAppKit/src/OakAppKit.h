22f3a4badf32cbb7a4ec6ac217dac96b2dd723ab
Allan Odgaard <git@abetarda.com>
2016-09-07 23:30:54 +0200

    Remove code required for building with the 10.8 SDK
    We reference several 10.9 symbols so while TextMate still runs on 10.8, it does not build without a more recent SDK.

--- Frameworks/OakAppKit/src/OakAppKit.h
+++ Frameworks/OakAppKit/src/OakAppKit.h
@@ -15,12 +15,6 @@ PUBLIC extern NSUInteger const OakMoveNoActionReturn;
 PUBLIC NSUInteger OakPerformTableViewActionFromKeyEvent (NSTableView* tableView, NSEvent* event);
 PUBLIC NSUInteger OakPerformTableViewActionFromSelector (NSTableView* tableView, SEL selector, NSTextView* textView);
 
-#if !defined(MAC_OS_X_VERSION_10_9) || (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_9)
-APPKIT_EXTERN void NSAccessibilityPostNotificationWithUserInfo(id element, NSString *notification, NSDictionary *userInfo) NS_AVAILABLE_MAC(10_7);
-APPKIT_EXTERN NSString *const NSAccessibilityAnnouncementRequestedNotification NS_AVAILABLE_MAC(10_7);
-APPKIT_EXTERN NSString *const NSAccessibilityAnnouncementKey            NS_AVAILABLE_MAC(10_7);
-#endif
-
 #if !defined(MAC_OS_X_VERSION_10_10) || (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_10)
 PUBLIC extern NSString *const *const pNSAccessibilitySharedFocusElementsAttribute;
 #define NSAccessibilitySharedFocusElementsAttribute (*pNSAccessibilitySharedFocusElementsAttribute)
