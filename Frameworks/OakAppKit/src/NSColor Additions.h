e6e31c211bbe9f71e67773a30b7a413d2a4ec360
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2016-09-10 22:35:40 -0700

    Update visual style of OakChooser matches

--- Frameworks/OakAppKit/src/NSColor Additions.h
+++ Frameworks/OakAppKit/src/NSColor Additions.h
@@ -1,3 +1,7 @@
 @interface NSColor (TMColorAdditions)
 + (NSColor*)colorWithString:(NSString*)aString;
++ (NSColor*)tmMatchedTextBackgroundColor;
++ (NSColor*)tmMatchedTextUnderlineColor;
++ (NSColor*)tmMatchedTextSelectedBackgroundColor;
++ (NSColor*)tmMatchedTextSelectedUnderlineColor;
 @end
