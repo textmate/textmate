textmate/textmate

commit 50dabac4678d0ed8c9b43d663efaa022e06ab0de

Let the symbol chooser use OakDocument instead of document_t

Frameworks/OakTextView/src/OakDocumentView.mm
@@ -369,7 +369,7 @@ private:
 
 	if(_symbolChooser)
 	{
-		_symbolChooser.document        = document;
+		_symbolChooser.document        = document->document();
 		_symbolChooser.selectionString = _textView.selectionString;
 	}
 
@@ -554,7 +554,7 @@ private:
 		[[NSNotificationCenter defaultCenter] removeObserver:self name:NSWindowWillCloseNotification object:_symbolChooser.window];
 
 		_symbolChooser.target   = nil;
-		_symbolChooser.document = document::document_ptr();
+		_symbolChooser.document = nil;
 	}
 
 	if(_symbolChooser = aSymbolChooser)
@@ -562,7 +562,7 @@ private:
 		_symbolChooser.target          = self;
 		_symbolChooser.action          = @selector(symbolChooserDidSelectItems:);
 		_symbolChooser.filterString    = @"";
-		_symbolChooser.document        = document;
+		_symbolChooser.document        = document->document();
 		_symbolChooser.selectionString = _textView.selectionString;
 
 		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(symbolChooserWillClose:) name:NSWindowWillCloseNotification object:_symbolChooser.window];

Frameworks/OakFilterList/src/SymbolChooser.h
@@ -1,8 +1,8 @@
 #import "OakChooser.h"
-#import <document/document.h>
+#import <document/OakDocument.h>
 
 PUBLIC @interface SymbolChooser : OakChooser
-@property (nonatomic) document::document_ptr document;
+@property (nonatomic) OakDocument* document;
 @property (nonatomic) NSString* selectionString;
 + (instancetype)sharedInstance;
 @end

Frameworks/OakFilterList/src/SymbolChooser.mm
@@ -17,14 +17,14 @@
 - (id)objectForKey:(id)key { return [self valueForKey:key]; }
 @end
 
-static SymbolChooserItem* CreateItem (document::document_ptr const& document, text::pos_t const& pos, std::string const& candidate, std::vector< std::pair<size_t, size_t> > const& ranges)
+static SymbolChooserItem* CreateItem (OakDocument* document, text::pos_t const& pos, NSString* symbol, std::vector< std::pair<size_t, size_t> > const& ranges)
 {
 	SymbolChooserItem* res = [SymbolChooserItem new];
-	res.path            = [NSString stringWithCxxString:document->path()];
-	res.identifier      = [NSString stringWithCxxString:document->identifier()];
+	res.path            = document.path;
+	res.identifier      = document.identifier.UUIDString;
 	res.selectionString = [NSString stringWithCxxString:pos];
-	res.name            = CreateAttributedStringWithMarkedUpRanges(candidate, ranges, NSLineBreakByTruncatingTail);
-	res.infoString      = [NSString stringWithCxxString:document->display_name() + ":" + (std::string)pos];
+	res.name            = CreateAttributedStringWithMarkedUpRanges(to_s(symbol), ranges, NSLineBreakByTruncatingTail);
+	res.infoString      = [document.displayName stringByAppendingFormat:@":%@", res.selectionString];
 	return res;
 }
 
@@ -63,35 +63,15 @@ static SymbolChooserItem* CreateItem (document::document_ptr const& document, te
 
 - (void)windowWillClose:(NSNotification*)aNotification
 {
-	[self setDocument:document::document_ptr()];
+	[self setDocument:nil];
 }
 
-- (void)setDocument:(document::document_ptr)aDocument
+- (void)setDocument:(OakDocument*)aDocument
 {
-	struct callback_t : document::open_callback_t
-	{
-		callback_t (SymbolChooser* self) : _self(self) { }
-
-		void show_error (std::string const& path, document::document_ptr document, std::string const& message, oak::uuid_t const& filter)
-		{
-			fprintf(stderr, "%s: %s\n", path.c_str(), message.c_str());
-		}
-
-		void show_document (std::string const& path, document::document_ptr document)
-		{
-			[_self updateItems:_self];
-		}
-
-	private:
-		SymbolChooser* _self;
-	};
-
-	if(_document)
-		_document->close();
-	if((_document = aDocument) && _document->try_open(std::make_shared<callback_t>(self)))
+	if(_document = aDocument)
 		[self updateItems:self];
-
-	self.window.title = _document ? [NSString stringWithFormat:@"Jump to Symbol — %@", [NSString stringWithCxxString:_document->display_name()]] : @"Jump to Symbol";
+	NSString* title = @"Jump to Symbol";
+	self.window.title = _document ? [title stringByAppendingFormat:@" — %@", _document.displayName] : title;
 }
 
 - (void)setSelectionString:(NSString*)aString
@@ -129,31 +109,30 @@ static SymbolChooserItem* CreateItem (document::document_ptr const& document, te
 	{
 		if(OakIsEmptyString(self.filterString))
 		{
-			for(auto const& pair : _document->symbols())
-			{
-				if(pair.second != "-")
-					[res addObject:CreateItem(_document, pair.first, pair.second, std::vector< std::pair<size_t, size_t> >())];
-			}
+			[_document enumerateSymbolsUsingBlock:^(text::pos_t const& pos, NSString* symbol){
+				if(![symbol isEqualToString:@"-"])
+					[res addObject:CreateItem(_document, pos, symbol, std::vector< std::pair<size_t, size_t> >())];
+			}];
 		}
 		else
 		{
 			std::string const filter = oak::normalize_filter(to_s(self.filterString));
 
-			std::string sectionName = NULL_STR;
-			std::multimap<double, SymbolChooserItem*> rankedItems;
-			for(auto const& pair : _document->symbols())
-			{
-				if(pair.second == "-")
-					continue;
+			__block NSString* sectionName = nil;
+			__block std::multimap<double, SymbolChooserItem*> rankedItems;
 
-				bool indented = pair.second.find("\u2003") == 0;
+			[_document enumerateSymbolsUsingBlock:^(text::pos_t const& pos, NSString* symbol){
+				if([symbol isEqualToString:@"-"])
+					return;
+
+				BOOL indented = [symbol hasPrefix:@"\u2003"];
 				if(!indented)
-					sectionName = pair.second;
+					sectionName = symbol;
 
 				std::vector< std::pair<size_t, size_t> > ranges;
-				if(double rank = oak::rank(filter, pair.second, &ranges))
-					rankedItems.emplace(1 - rank, CreateItem(_document, pair.first, indented && sectionName != NULL_STR ? (pair.second + " — " + sectionName) : pair.second, ranges));
-			}
+				if(double rank = oak::rank(filter, to_s(symbol), &ranges))
+					rankedItems.emplace(1 - rank, CreateItem(_document, pos, indented && sectionName ? [NSString stringWithFormat:@"%@ — %@", symbol, sectionName] : symbol, ranges));
+			}];
 
 			for(auto const& pair : rankedItems)
 				[res addObject:pair.second];

commit dae362060dea94d395b07d48f1732c65d7b2a6e5

Fix crash if symbol chooser was open when closing window
This was introduced in 50dabac4678d0ed8c9b43d663efaa022e06ab0de.

Frameworks/OakTextView/src/OakDocumentView.mm
@@ -369,7 +369,7 @@ private:
 
 	if(_symbolChooser)
 	{
-		_symbolChooser.document        = cppDocument->document();
+		_symbolChooser.document        = cppDocument ? cppDocument->document() : nil;
 		_symbolChooser.selectionString = _textView.selectionString;
 	}
 