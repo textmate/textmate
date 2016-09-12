textmate/textmate

commit c270b0c5afd3378b1c30cbc4cbab0b3ef57563b7

Allow setting font for OakChoiceMenu to match our text view

Frameworks/OakTextView/src/OakChoiceMenu.mm
@@ -26,6 +26,7 @@ enum action_t { kActionNop, kActionTab, kActionReturn, kActionCancel, kActionMov
 	{
 		_choices = [NSArray array];
 		_choiceIndex = NSNotFound;
+		_font = [NSFont controlContentFontOfSize:0];
 	}
 	return self;
 }
@@ -41,7 +42,7 @@ enum action_t { kActionNop, kActionTab, kActionReturn, kActionCancel, kActionMov
 	CGFloat const kTableViewPadding = 4;
 	CGFloat const kScrollBarWidth   = 15;
 
-	NSTextField* textField = OakCreateLabel(@"", [NSFont controlContentFontOfSize:0]);
+	NSTextField* textField = OakCreateLabel(@"", self.font);
 	CGFloat width = 60;
 	for(NSInteger i = 0; i < MIN(_choices.count, 256); ++i)
 	{
@@ -125,7 +126,9 @@ enum action_t { kActionNop, kActionTab, kActionReturn, kActionCancel, kActionMov
 	NSTextField* res = [aTableView makeViewWithIdentifier:identifier owner:self];
 	if(!res)
 	{
-		res = OakCreateLabel(@"", [NSFont controlContentFontOfSize:0]);
+		res = OakCreateLabel(@"", self.font);
+		[res sizeToFit];
+		aTableView.rowHeight = NSHeight(res.frame);
 		res.identifier = identifier;
 	}
 	return res;

Frameworks/OakTextView/src/OakChoiceMenu.h
@@ -8,6 +8,7 @@ extern NSUInteger const OakChoiceMenuKeyMovement;
 @property (nonatomic) NSArray* choices;
 @property (nonatomic) NSUInteger choiceIndex;
 @property (nonatomic, readonly) NSString* selectedChoice;
+@property (nonatomic) NSFont* font;
 - (void)showAtTopLeftPoint:(NSPoint)aPoint forView:(NSView*)aView;
 - (BOOL)isVisible;
 - (NSUInteger)didHandleKeyEvent:(NSEvent*)anEvent;

Frameworks/OakTextView/src/OakTextView.mm
@@ -1047,6 +1047,7 @@ doScroll:
 	if(!choiceVector.empty())
 	{
 		_choiceMenu = [OakChoiceMenu new];
+		_choiceMenu.font = [NSFont fontWithName:self.font.fontName size:self.font.pointSize * documentView->font_scale_factor() / 100];
 		_choiceMenu.choices = (__bridge NSArray*)((CFArrayRef)cf::wrap(choiceVector));
 
 		std::string const& currentChoice = documentView->placeholder_content();

commit 7b291185fee97bcac5492303a7ecf8f9c6d1409c

Only set table view’s row height during sizeToFit
The initial dimension of the OakChoiceMenu could be incorrect because the row height had not been updated.

This broke in c270b0c5afd3378b1c30cbc4cbab0b3ef57563b7

Frameworks/OakTextView/src/OakChoiceMenu.mm
@@ -43,6 +43,8 @@ enum action_t { kActionNop, kActionTab, kActionReturn, kActionCancel, kActionMov
 	CGFloat const kScrollBarWidth   = 15;
 
 	NSTextField* textField = OakCreateLabel(@"", self.font, NSLeftTextAlignment, NSLineBreakByTruncatingTail);
+	tableView.rowHeight = NSHeight(textField.frame);
+
 	CGFloat width = 60;
 	for(NSInteger i = 0; i < MIN(_choices.count, 256); ++i)
 	{
@@ -127,8 +129,6 @@ enum action_t { kActionNop, kActionTab, kActionReturn, kActionCancel, kActionMov
 	if(!res)
 	{
 		res = OakCreateLabel(@"", self.font, NSLeftTextAlignment, NSLineBreakByTruncatingTail);
-		[res sizeToFit];
-		aTableView.rowHeight = NSHeight(res.frame);
 		res.identifier = identifier;
 	}
 	return res;