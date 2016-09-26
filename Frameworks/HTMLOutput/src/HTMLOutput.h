529ecf82183ee7a177aec4acac3643738039f208
Allan Odgaard <git@abetarda.com>
2016-09-26 13:23:42 +0200

    Rename loadHTMLString: method to setContent:
    The previous name was inspired by WebView’s default API but it sounds like it is just the default implementation with a default baseURL: parameter, hence why I prefer to name it something different.

--- Frameworks/HTMLOutput/src/HTMLOutput.h
+++ Frameworks/HTMLOutput/src/HTMLOutput.h
@@ -3,7 +3,7 @@
 @interface OakHTMLOutputView : NSView
 - (void)loadRequest:(NSURLRequest*)aRequest environment:(std::map<std::string, std::string> const&)anEnvironment autoScrolls:(BOOL)flag;
 - (void)stopLoadingWithUserInteraction:(BOOL)askUserFlag completionHandler:(void(^)(BOOL didStop))handler;
-- (void)loadHTMLString:(NSString*)someHTML;
+- (void)setContent:(NSString*)someHTML;
 
 @property (nonatomic) NSUUID* commandIdentifier; // UUID from initial load request
 @property (nonatomic, getter = isRunningCommand, readonly) BOOL runningCommand;
