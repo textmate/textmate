9467f3f92b5ef81cfb1bd3495171d2a5cf34ccba
Allan Odgaard <git@abetarda.com>
2016-11-11 15:46:04 +0700

    Fix race condition
    We could call pop_callback before having pushed one because the latter is done after delay.
    
    The way we obtained a weak reference to ourself was also scoped incorrectly so our block effectively had a strong reference to self.
    
    Lastly when disabling observeSCMStatus we should destroy the SCM handle rather than only remove our callback (since we do not re-use the handle).

--- Frameworks/document/src/OakDocument.mm
+++ Frameworks/document/src/OakDocument.mm
@@ -1569,18 +1569,22 @@ NSString* OakDocumentBookmarkIdentifier           = @"bookmark";
 
 			// We must postpone potential self.scmStatus = «status» when our callstack
 			// is bind:toObject:withKeyPath:options: → scmStatus → setObserveSCMStatus:
+			__weak OakDocument* weakSelf = self;
 			dispatch_async(dispatch_get_main_queue(), ^{
-				__weak OakDocument* weakSelf = self;
-				_scmInfo->push_callback(^(scm::info_t const& info){
-					weakSelf.scmStatus    = info.status(to_s(weakSelf.path));
-					weakSelf.scmVariables = info.scm_variables();
-				});
+				OakDocument* doc = weakSelf;
+				if(doc && doc.scmInfo)
+				{
+					doc.scmInfo->push_callback(^(scm::info_t const& info){
+						doc.scmStatus    = info.status(to_s(doc.path));
+						doc.scmVariables = info.scm_variables();
+					});
+				}
 			});
 		}
 	}
-	else if(_scmInfo)
+	else
 	{
-		_scmInfo->pop_callback();
+		_scmInfo.reset();
 	}
 }
 
