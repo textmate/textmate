db7c63ec12497164631daa0afcdd72828e27160c
Ronald Wampler <rdwampler@gmail.com>
2016-11-22 15:16:11 -0500

    Introduce OakMoveToTrash and make it accessible only to C++
    This is just a C++ wrapper around the Cocoa API for moving a file to trash.

--- Frameworks/OakFoundation/src/OakFoundation.h
+++ Frameworks/OakFoundation/src/OakFoundation.h
@@ -1,4 +1,10 @@
 #import <oak/misc.h>
 
+#ifdef __cplusplus
+PUBLIC std::string OakMoveToTrash (std::string const& path);
+#endif
+
+#ifdef __OBJC__
 PUBLIC BOOL OakIsEmptyString (NSString* str);
 PUBLIC BOOL OakNotEmptyString (NSString* str);
+#endif
