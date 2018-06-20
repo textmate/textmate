44256787158a53b1937435ca6f0735fc97816c78
Allan Odgaard <git@abetarda.com>
2018-06-20 14:37:17 +0200

    Only launch Genie if not already running

--- Applications/GenieLauncher/src/main.mm
+++ Applications/GenieLauncher/src/main.mm
@@ -2,13 +2,31 @@
 
 int main (int argc, char const* argv[])
 {
-	for(NSUInteger i = 0; i < argc; ++i)
-		os_log_info(OS_LOG_DEFAULT, "Genie Launcher: argv[%lu] = %{public}s", i, argv[i]);
-
-	NSError* error;
-	NSString* geniePath = [[NSBundle.mainBundle.bundlePath stringByAppendingPathComponent:@"../../../../../Genie.app"] stringByStandardizingPath];
-	if(![NSWorkspace.sharedWorkspace launchApplicationAtURL:[NSURL fileURLWithPath:geniePath] options:NSWorkspaceLaunchWithoutActivation|NSWorkspaceLaunchWithoutAddingToRecents configuration:@{ } error:&error])
-		os_log_error(OS_LOG_DEFAULT, "Failed to launch Genie.app: %{public}@", error.localizedDescription);
-
+	NSString* geniePath = [[NSBundle.mainBundle.bundlePath stringByAppendingPathComponent:@"../../../.."] stringByStandardizingPath];
+	if(NSBundle* genieBundle = [NSBundle bundleWithPath:geniePath])
+	{
+		if(NSString* identifier = genieBundle.bundleIdentifier)
+		{
+			NSArray* runningApps = [NSRunningApplication runningApplicationsWithBundleIdentifier:identifier];
+			if(!runningApps.count)
+			{
+				NSError* error;
+				if(![NSWorkspace.sharedWorkspace launchApplicationAtURL:genieBundle.bundleURL options:NSWorkspaceLaunchWithoutActivation|NSWorkspaceLaunchWithoutAddingToRecents configuration:@{ } error:&error])
+					os_log_error(OS_LOG_DEFAULT, "Failed to launch Genie.app: %{public}@", error.localizedDescription);
+			}
+			else
+			{
+				os_log_info(OS_LOG_DEFAULT, "Genie is already running");
+			}
+		}
+		else
+		{
+			os_log_error(OS_LOG_DEFAULT, "No bundle identifier in bundle: %{public}@", genieBundle);
+		}
+	}
+	else
+	{
+		os_log_error(OS_LOG_DEFAULT, "Unable to load bundle at path: %{public}@", geniePath);
+	}
 	return EX_OK;
 }
