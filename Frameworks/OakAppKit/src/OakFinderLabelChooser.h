d6d47febfe292e2cc9ccfa51773b40c7040de215
Allan Odgaard <git@abetarda.com>
2018-07-08 17:17:32 +0200

    Remove OakFinderLabelChooser

--- Frameworks/OakAppKit/src/OakFinderLabelChooser.h
+++
@@ -1,9 +0,0 @@
-#import <oak/debug.h>
-
-PUBLIC @interface OakFinderLabelChooser : NSView
-@property (nonatomic) BOOL enabled;
-@property (nonatomic) NSInteger selectedIndex;
-@property (nonatomic, weak) id target;
-@property (nonatomic) SEL action;
-@property (nonatomic) NSFont* font;
-@end
