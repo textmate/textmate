43fc65805d298902ddd03d782eb17992f56ad670
Allan Odgaard <git@abetarda.com>
2018-08-21 11:30:26 +0200

    Add NSAppearanceName stuff from the macOS 10.14 SDK

--- Shared/include/oak/sdk-compat.h
+++ Shared/include/oak/sdk-compat.h
@@ -10,3 +10,19 @@
 #define NSAlertStyleInformational NSAlertStyleInformational
 #define NSAlertStyleCritical      NSAlertStyleCritical
 #endif
+
+#if !defined(MAC_OS_X_VERSION_10_13) || (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_13)
+typedef NSString *NSAppearanceName;
+#endif
+
+#if !defined(MAC_OS_X_VERSION_10_14) || (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_14)
+extern NSAppearanceName const NSAppearanceNameDarkAqua __attribute__((weak_import));
+
+@interface NSAppearance (Mojave)
+- (NSAppearanceName)bestMatchFromAppearancesWithNames:(NSArray<NSAppearanceName> *)appearances;
+@end
+
+@interface NSApplication (Mojave)
+@property(readonly, strong) NSAppearance *effectiveAppearance;
+@end
+#endif
