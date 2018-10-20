ec1b8e7691b34d41915e8c0502dd06c4c5730949
Ronald Wampler <rdwampler@gmail.com>
2018-10-20 11:26:10 -0400

    Use a dynamic color for the dark divider style (10.14)

--- Frameworks/OakAppKit/src/NSColor Additions.mm
+++ Frameworks/OakAppKit/src/NSColor Additions.mm
@@ -22,4 +22,8 @@
 + (NSColor*)tmMatchedTextUnderlineColor          { return [NSColor colorWithCalibratedRed:0.89 green:0.72 blue:0.0 alpha:1.0]; }
 + (NSColor*)tmMatchedTextSelectedBackgroundColor { return [NSColor colorWithCalibratedWhite:1.0 alpha:0.30]; }
 + (NSColor*)tmMatchedTextSelectedUnderlineColor  { return [NSColor whiteColor]; }
+
+#define COLOR_ASSET(name, color) + (NSColor*)tm##name { if(OAK_AVAILABLE(10, 14)) return [NSColor colorNamed:@#name]; return color; }
+
+COLOR_ASSET(DarkDividerColor, [NSColor controlShadowColor])
 @end
