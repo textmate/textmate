f4f517761362163735d1c053c370fa2393d90cf9
Allan Odgaard <git@abetarda.com>
2018-11-08 09:33:13 +0700

    Use a CALayer for our zooming icon animation
    This should ensure smooth animation even when the CPU load is high (which it often is when opening files).

--- Frameworks/OakAppKit/src/OakZoomingIcon.mm
+++ Frameworks/OakAppKit/src/OakZoomingIcon.mm
@@ -9,7 +9,7 @@
 @implementation OakZoomingIcon
 - (id)initWithIcon:(NSImage*)icon rect:(NSRect)aRect
 {
-	if(self = [super initWithContentRect:aRect styleMask:NSWindowStyleMaskBorderless backing:NSBackingStoreBuffered defer:NO])
+	if(self = [super initWithContentRect:NSInsetRect(aRect, -56, -56) styleMask:NSWindowStyleMaskBorderless backing:NSBackingStoreBuffered defer:NO])
 	{
 		self.releasedWhenClosed = NO;
 		self.ignoresMouseEvents = YES;
@@ -17,29 +17,43 @@
 		self.opaque             = NO;
 		self.level              = NSPopUpMenuWindowLevel;
 
-		icon = [icon copy];
-		[icon setSize:NSMakeSize(128, 128)];
+		NSView* view = self.contentView;
+		view.wantsLayer = YES;
 
-		NSImageView* imageView = [[NSImageView alloc] initWithFrame:aRect];
-		[imageView setImage:icon];
-		[imageView setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
-		self.contentView = imageView;
+		CALayer* layer = [CALayer layer];
+		[view.layer addSublayer:layer];
+
+		NSImage* image = [icon copy];
+		image.size = view.bounds.size;
+
+		layer.bounds   = { NSZeroPoint, aRect.size };
+		layer.position = NSMakePoint(NSMidX(view.bounds), NSMidY(view.bounds));
+		layer.contents = image;
 
 		[self orderFront:self];
 
-		[NSAnimationContext beginGrouping];
-
-		[NSAnimationContext currentContext].duration = ([[NSApp currentEvent] modifierFlags] & NSEventModifierFlagShift) ? 2.5 : 0.25;
-		[NSAnimationContext currentContext].completionHandler = ^{
-			[self orderOut:self];
-		};
-
-		[self.animator setAlphaValue:0];
-		[self.animator setFrame:NSInsetRect(aRect, -56, -56) display:YES animate:YES];
-
-		[NSAnimationContext endGrouping];
+		// Layer properties changed in this run loop cycle will not be animated
+		[self performSelector:@selector(runAnimation:) withObject:layer afterDelay:0];
 	}
 	return self;
+}
+
+- (void)runAnimation:(CALayer*)layer
+{
+	CGFloat duration = 0.25 * ((NSApp.currentEvent.modifierFlags & NSEventModifierFlagShift) ? 10 : 1);
+
+	[CATransaction begin];
+	[CATransaction setAnimationDuration:duration];
+	[CATransaction setAnimationTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn]];
+
+	[CATransaction setCompletionBlock:^{
+		[self close];
+	}];
+
+	layer.bounds  = self.contentView.bounds;
+	layer.opacity = 0;
+
+	[CATransaction commit];
 }
 
 + (OakZoomingIcon*)zoomIcon:(NSImage*)icon fromRect:(NSRect)aRect
