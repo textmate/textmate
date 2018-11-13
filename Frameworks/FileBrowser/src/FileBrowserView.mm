444c17f7bf46bd9d73ab6e081463cff9556ef9bc
Ronald Wampler <rdwampler@gmail.com>
2018-11-13 16:41:49 -0500

    Check the correct return value (-1) when item is not found

--- Frameworks/FileBrowser/src/FileBrowserView.mm
+++ Frameworks/FileBrowser/src/FileBrowserView.mm
@@ -1039,7 +1039,7 @@ static NSMutableIndexSet* MutableLongestCommonSubsequence (NSArray* lhs, NSArray
 - (NSRect)imageRectOfItem:(FileItem*)item
 {
 	NSInteger row = [_outlineView rowForItem:item];
-	if(row != NSNotFound)
+	if(row != -1)
 	{
 		FileItemTableCellView* view = [_outlineView viewAtColumn:0 row:row makeIfNecessary:YES];
 		if([view isKindOfClass:[FileItemTableCellView class]])
