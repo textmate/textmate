9fe62089efe90ad4ea046b8128b89a6b5a92b1a5
Ronald Wampler <rdwampler@gmail.com>
2018-11-14 13:13:23 -0500

    Remove forward declarations for macOS 10.12 SDK
    Although our minimum target is 10.10, we currently (dd41b992f70e66aac122e8a8479c65365d1375ab) only support compiling with Xcode 9, which has the macOS 10.13 SDK.

--- Shared/include/oak/sdk-compat.h
+++ Shared/include/oak/sdk-compat.h
@@ -1,16 +1,3 @@
-#if !defined(MAC_OS_X_VERSION_10_12) || (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_12)
-@interface NSWindow (Sierra)
-+ (void)setAllowsAutomaticWindowTabbing:(BOOL)flag;
-@end
-
-@protocol CAAnimationDelegate <NSObject>
-@end
-
-#define NSAlertStyleWarning       NSAlertStyleWarning
-#define NSAlertStyleInformational NSAlertStyleInformational
-#define NSAlertStyleCritical      NSAlertStyleCritical
-#endif
-
 #if !defined(MAC_OS_X_VERSION_10_14) || (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_14)
 #define NSVisualEffectMaterialHeaderView (NSVisualEffectMaterial)10
 #define NSVisualEffectMaterialToolTip (NSVisualEffectMaterial)17
