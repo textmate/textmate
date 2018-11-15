a0ac9c9a921c85910110fe795b95342e0e06f0df
Allan Odgaard <git@abetarda.com>
2018-11-15 12:17:33 +0700

    Let NSScrollView draw border above/below search results
    This requires clipping the left/right border of the scroll view (using a narrow superview), which is something I have avoided since Interface Builder has a warning about “view is clipping its content” and I seem to recall there being some issues with drawing focus ring around clipped views in the past, but as we are not using a xib nor drawing a focus ring around the NSScrollView being clipped, this approach should be OK.
    
    The advantage is of course that we ensure system colors for the borders.

--- Frameworks/Find/src/FFResultsViewController.mm
+++ Frameworks/Find/src/FFResultsViewController.mm
@@ -22,9 +22,7 @@ static FFResultNode* PreviousNode (FFResultNode* node)
 
 @interface FFResultsViewController () <NSOutlineViewDataSource, NSOutlineViewDelegate>
 {
-	NSView*        _topDivider;
 	NSScrollView*  _scrollView;
-	NSView*        _bottomDivider;
 	NSFont*        _searchResultsFont;
 
 	__weak id      _eventMonitor;
@@ -311,9 +309,6 @@ static FFResultNode* PreviousNode (FFResultNode* node)
 		[label sizeToFit];
 		CGFloat lineHeight = std::max(NSHeight(label.frame), ceil(_searchResultsFont.ascender) + ceil(fabs(_searchResultsFont.descender)) + ceil(_searchResultsFont.leading));
 
-		_topDivider    = OakCreateHorizontalLine(OakBackgroundFillViewStyleDivider);
-		_bottomDivider = OakCreateHorizontalLine(OakBackgroundFillViewStyleDivider);
-
 		_outlineView = [[NSOutlineView alloc] initWithFrame:NSZeroRect];
 		_outlineView.accessibilityLabel                 = @"Results";
 		_outlineView.focusRingType                      = NSFocusRingTypeNone;
@@ -337,20 +332,18 @@ static FFResultNode* PreviousNode (FFResultNode* node)
 		_scrollView.hasVerticalScroller   = YES;
 		_scrollView.hasHorizontalScroller = YES;
 		_scrollView.autohidesScrollers    = YES;
-		_scrollView.borderType            = NSNoBorder;
+		_scrollView.borderType            = NSLineBorder;
 		_scrollView.documentView          = _outlineView;
 
 		NSDictionary* views = @{
-			@"topDivider":    _topDivider,
-			@"scrollView":    _scrollView,
-			@"bottomDivider": _bottomDivider,
+			@"scrollView": _scrollView,
 		};
 
 		NSView* containerView = [[NSView alloc] initWithFrame:NSZeroRect];
 		OakAddAutoLayoutViewsToSuperview([views allValues], containerView);
 
-		[containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[topDivider][scrollView][bottomDivider]|" options:NSLayoutFormatAlignAllLeading|NSLayoutFormatAlignAllTrailing metrics:nil views:views]];
-		[containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[scrollView]|" options:0 metrics:nil views:views]];
+		[containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[scrollView]|" options:0 metrics:nil views:views]];
+		[containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(-1)-[scrollView]-(-1)-|" options:0 metrics:nil views:views]];
 
 		self.view = containerView;
 
