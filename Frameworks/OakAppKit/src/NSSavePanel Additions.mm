af3e3fb0c3b6a405bf5f22cc743e5c4b2fda283e
Allan Odgaard <git@abetarda.com>
2018-11-15 11:34:40 +0700

    Add margin around the “Show Hidden Files” checkbox in file open panel

--- Frameworks/OakAppKit/src/NSSavePanel Additions.mm
+++ Frameworks/OakAppKit/src/NSSavePanel Additions.mm
@@ -14,10 +14,21 @@
 		if([self respondsToSelector:@selector(showsHiddenFiles)] && [self respondsToSelector:@selector(setShowsHiddenFiles:)])
 		{
 			NSButton* checkbox = OakCreateCheckBox(@"Show Hidden Files");
-			[checkbox sizeToFit];
 			[checkbox bind:NSValueBinding toObject:[NSUserDefaultsController sharedUserDefaultsController] withKeyPath:@"values.NSOpenPanelShowHiddenFiles" options:nil];
 
-			[self setAccessoryView:checkbox];
+			NSDictionary* views = @{
+				@"checkbox": checkbox,
+			};
+
+			NSView* contentView = [[NSView alloc] initWithFrame:NSZeroRect];
+			OakAddAutoLayoutViewsToSuperview(views.allValues, contentView);
+
+			[contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[checkbox]-|" options:0 metrics:nil views:views]];
+			[contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[checkbox]-|" options:0 metrics:nil views:views]];
+
+			contentView.autoresizingMask = NSViewWidthSizable|NSViewHeightSizable;
+			[self setAccessoryView:contentView];
+
 			[self bind:@"showsHiddenFiles" toObject:[NSUserDefaultsController sharedUserDefaultsController] withKeyPath:@"values.NSOpenPanelShowHiddenFiles" options:nil];
 		}
 	}
