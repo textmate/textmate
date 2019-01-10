textmate/textmate

commit f850dbdbc772814372ad84f8740ea8234950cc5d

Support font style changes in OakSyntaxFormatter

Frameworks/OakAppKit/src/OakSyntaxFormatter.mm
@@ -11,6 +11,7 @@ static size_t kParseSizeLimit = 1024;
 @interface OakSyntaxFormatter ()
 {
 	NSString* _grammarName;
+	NSFont* _font;
 
 	BOOL _didLoadGrammarAndTheme;
 	parse::grammar_ptr _grammar;
@@ -72,13 +73,19 @@ static size_t kParseSizeLimit = 1024;
 	if(!styled || !_grammarName)
 		return;
 
+	if(!_font)
+	{
+		NSDictionary* attributes = [styled attributesAtIndex:0 effectiveRange:nil];
+		_font = attributes[NSFontAttributeName] ?: [NSFont systemFontOfSize:0];
+	}
+
 	NSString* plain = styled.string;
 	for(NSString* attr in @[ NSForegroundColorAttributeName, NSBackgroundColorAttributeName, NSUnderlineStyleAttributeName, NSStrikethroughStyleAttributeName ])
 		[styled removeAttribute:attr range:NSMakeRange(0, plain.length)];
 
 	if(_enabled == NO || ![self tryLoadGrammarAndTheme])
 	{
-		[styled addAttributes:@{ NSForegroundColorAttributeName: [NSColor controlTextColor] } range:NSMakeRange(0, plain.length)];
+		[styled addAttributes:@{ NSFontAttributeName: _font, NSForegroundColorAttributeName: [NSColor controlTextColor] } range:NSMakeRange(0, plain.length)];
 		return;
 	}
 
@@ -94,6 +101,7 @@ static size_t kParseSizeLimit = 1024;
 		size_t to = ++pair != scopes.end() ? pair->first : str.size();
 		size_t len = utf16::distance(str.data() + from, str.data() + to);
 		NSMutableDictionary* attributes = [@{
+			NSFontAttributeName:               _font,
 			NSForegroundColorAttributeName:    styles.foregroundColor,
 			NSUnderlineStyleAttributeName:     @(styles.underlined ? NSUnderlineStyleSingle : NSUnderlineStyleNone),
 			NSStrikethroughStyleAttributeName: @(styles.strikethrough ? NSUnderlineStyleSingle : NSUnderlineStyleNone),
@@ -101,6 +109,14 @@ static size_t kParseSizeLimit = 1024;
 
 		if(![styles.backgroundColor isEqual:_theme.backgroundColor])
 			attributes[NSBackgroundColorAttributeName] = styles.backgroundColor;
+
+		if(styles.fontTraits)
+		{
+			attributes[NSFontAttributeName] = [NSFont fontWithDescriptor:[NSFontDescriptor fontDescriptorWithFontAttributes:@{
+				NSFontFamilyAttribute: _font.familyName,
+				NSFontTraitsAttribute: @{ NSFontSymbolicTrait: @(styles.fontTraits) },
+			}] size:_font.pointSize];
+		}
 
 		[styled addAttributes:attributes range:NSMakeRange(pos, len)];
 

commit d8cead9538b44696dd6d1a180fefdc757acead86

OakSyntaxFormatter: Clean up font attributes
See https://lists.macromates.com/textmate/2019-January/040883.html.

The issue is caused by commit f850dbdbc772814372ad84f8740ea8234950cc5d, which added support for font style changes. It did so by applying a single font across the entire string. But, in some cases not all characters (e.g., Chinese or Japanese characters) may be available for that font so these characters would not be rendered correctly while editing.

Frameworks/OakAppKit/src/OakSyntaxFormatter.mm
@@ -75,6 +75,9 @@ static size_t kParseSizeLimit = 1024;
 	if(OakIsEmptyString(plain) || !_grammarName)
 		return;
 
+	if(_enabled == NO || ![self tryLoadGrammarAndTheme])
+		return;
+
 	if(!_font)
 	{
 		NSDictionary* attributes = [styled fontAttributesInRange:NSMakeRange(0, plain.length)];
@@ -83,12 +86,6 @@ static size_t kParseSizeLimit = 1024;
 
 	for(NSString* attr in @[ NSForegroundColorAttributeName, NSBackgroundColorAttributeName, NSUnderlineStyleAttributeName, NSStrikethroughStyleAttributeName ])
 		[styled removeAttribute:attr range:NSMakeRange(0, plain.length)];
-
-	if(_enabled == NO || ![self tryLoadGrammarAndTheme])
-	{
-		[styled addAttributes:@{ NSFontAttributeName: _font, NSForegroundColorAttributeName: [NSColor controlTextColor] } range:NSMakeRange(0, plain.length)];
-		return;
-	}
 
 	std::string str = to_s(plain);
 	std::map<size_t, scope::scope_t> scopes;
@@ -124,5 +121,6 @@ static size_t kParseSizeLimit = 1024;
 		pos += len;
 		from = to;
 	}
+	[styled fixFontAttributeInRange:NSMakeRange(0, plain.length)];
 }
 @end