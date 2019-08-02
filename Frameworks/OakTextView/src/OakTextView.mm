textmate/textmate

commit 1ef78b3af7d2aa189695badaf7c3c9bbb2b4bc03

Rename symbols to what’s expected by the 10.14 SDK

Frameworks/OakTextView/src/OakTextView.mm
@@ -2038,7 +2038,7 @@ static void update_menu_key_equivalents (NSMenu* menu, std::multimap<std::string
 		}];
 	});
 
-	[NSApp registerServicesMenuSendTypes:@[ NSStringPboardType ] returnTypes:@[ NSStringPboardType ]];
+	[NSApp registerServicesMenuSendTypes:@[ NSPasteboardTypeString ] returnTypes:@[ NSPasteboardTypeString ]];
 }
 
 // ======================
@@ -2047,9 +2047,9 @@ static void update_menu_key_equivalents (NSMenu* menu, std::multimap<std::string
 
 - (id)validRequestorForSendType:(NSString*)sendType returnType:(NSString*)returnType
 {
-	if([sendType isEqualToString:NSStringPboardType] && [self hasSelection] && !macroRecordingArray)
+	if([sendType isEqualToString:NSPasteboardTypeString] && [self hasSelection] && !macroRecordingArray)
 		return self;
-	if(!sendType && [returnType isEqualToString:NSStringPboardType] && !macroRecordingArray)
+	if(!sendType && [returnType isEqualToString:NSPasteboardTypeString] && !macroRecordingArray)
 		return self;
 	return [super validRequestorForSendType:sendType returnType:returnType];
 }
@@ -2057,22 +2057,22 @@ static void update_menu_key_equivalents (NSMenu* menu, std::multimap<std::string
 - (BOOL)writeSelectionToPasteboard:(NSPasteboard*)pboard types:(NSArray*)types
 {
 	BOOL res = NO;
-	if([self hasSelection] && [types containsObject:NSStringPboardType])
+	if([self hasSelection] && [types containsObject:NSPasteboardTypeString])
 	{
 		std::vector<std::string> v;
 		ng::ranges_t const ranges = ng::dissect_columnar(*documentView, documentView->ranges());
 		for(auto const& range : ranges)
 			v.push_back(documentView->substr(range.min().index, range.max().index));
 
-		[pboard declareTypes:@[ NSStringPboardType ] owner:nil];
-		res = [pboard setString:[NSString stringWithCxxString:text::join(v, "\n")] forType:NSStringPboardType];
+		[pboard declareTypes:@[ NSPasteboardTypeString ] owner:nil];
+		res = [pboard setString:[NSString stringWithCxxString:text::join(v, "\n")] forType:NSPasteboardTypeString];
 	}
 	return res;
 }
 
 - (BOOL)readSelectionFromPasteboard:(NSPasteboard*)pboard
 {
-	if(NSString* str = [pboard stringForType:[pboard availableTypeFromArray:@[ NSStringPboardType ]]])
+	if(NSString* str = [pboard stringForType:[pboard availableTypeFromArray:@[ NSPasteboardTypeString ]]])
 	{
 		AUTO_REFRESH;
 		documentView->insert(to_s(str));
@@ -2869,8 +2869,8 @@ static void update_menu_key_equivalents (NSMenu* menu, std::multimap<std::string
 
 - (find::options_t)incrementalSearchOptions
 {
-	BOOL ignoreCase = self.liveSearchView.ignoreCaseCheckBox.state == NSOnState;
-	BOOL wrapAround = self.liveSearchView.wrapAroundCheckBox.state == NSOnState;
+	BOOL ignoreCase = self.liveSearchView.ignoreCaseCheckBox.state == NSControlStateValueOn;
+	BOOL wrapAround = self.liveSearchView.wrapAroundCheckBox.state == NSControlStateValueOn;
 	return (ignoreCase ? find::ignore_case : find::none) | (wrapAround ? find::wrap_around : find::none) | find::ignore_whitespace;
 }
 
@@ -3044,11 +3044,11 @@ static char const* kOakMenuItemTitle = "OakMenuItemTitle";
 	else if([aMenuItem action] == @selector(toggleShowIndentGuides:))
 		[aMenuItem setTitle:(documentView && documentView->draw_indent_guides()) ? @"Hide Indent Guides" : @"Show Indent Guides"];
 	else if([aMenuItem action] == @selector(toggleContinuousSpellChecking:))
-		[aMenuItem setState:documentView->live_spelling() ? NSOnState : NSOffState];
+		[aMenuItem setState:documentView->live_spelling() ? NSControlStateValueOn : NSControlStateValueOff];
 	else if([aMenuItem action] == @selector(takeSpellingLanguageFrom:))
-		[aMenuItem setState:[[NSString stringWithCxxString:documentView->spelling_language()] isEqualToString:[aMenuItem representedObject]] ? NSOnState : NSOffState];
+		[aMenuItem setState:[[NSString stringWithCxxString:documentView->spelling_language()] isEqualToString:[aMenuItem representedObject]] ? NSControlStateValueOn : NSControlStateValueOff];
 	else if([aMenuItem action] == @selector(takeWrapColumnFrom:))
-		[aMenuItem setState:(documentView && documentView->wrap_column() == [aMenuItem tag]) ? NSOnState : NSOffState];
+		[aMenuItem setState:(documentView && documentView->wrap_column() == [aMenuItem tag]) ? NSControlStateValueOn : NSControlStateValueOff];
 	else if([aMenuItem action] == @selector(undo:))
 	{
 		[aMenuItem setTitle:@"Undo"];
@@ -3292,7 +3292,7 @@ static char const* kOakMenuItemTitle = "OakMenuItemTitle";
 			[alertWindow recalculateKeyViewLoop];
 		}
 
-		[alert beginSheetModalForWindow:self.window completionHandler:^(NSInteger returnCode){
+		[alert beginSheetModalForWindow:self.window completionHandler:^(NSModalResponse returnCode){
 			if(returnCode == NSAlertFirstButtonReturn)
 				[self setWrapColumn:std::max<NSInteger>([textField integerValue], 10)];
 		}];
@@ -3608,7 +3608,7 @@ static char const* kOakMenuItemTitle = "OakMenuItemTitle";
 {
 	return @[ NSColorPboardType, NSFilenamesPboardType,
 		@"WebURLsWithTitlesPboardType", (NSString*)kUTTypeURL, @"public.url-name", NSURLPboardType,
-		NSStringPboardType ];
+		NSPasteboardTypeString ];
 }
 
 - (void)setDropMarkAtPoint:(NSPoint)aPoint
@@ -3808,7 +3808,7 @@ static char const* kOakMenuItemTitle = "OakMenuItemTitle";
 		documentView->set_ranges(ng::range_t(pos));
 		documentView->insert(text::join(paths, "\n"));
 	}
-	else if(NSString* text = [pboard stringForType:[pboard availableTypeFromArray:@[ NSStringPboardType ]]])
+	else if(NSString* text = [pboard stringForType:[pboard availableTypeFromArray:@[ NSPasteboardTypeString ]]])
 	{
 		D(DBF_OakTextView_DragNDrop, bug("plain text: %s\n", [text UTF8String]););
 		if(shouldMove && documentView->has_selection())

Applications/TextMate/src/AboutWindowController.mm
@@ -296,7 +296,7 @@ static NSData* Digest (NSString* someString)
 		NSMenuItem* item = [aMenu addItemWithTitle:label action:@selector(didClickToolbarItem:) keyEquivalent:key < 9 ? [NSString stringWithFormat:@"%c", '0' + (++key % 10)] : @""];
 		[item setRepresentedObject:label];
 		[item setTarget:self];
-		[item setState:[label isEqualToString:[self.toolbar selectedItemIdentifier]] ? NSOnState : NSOffState];
+		[item setState:[label isEqualToString:[self.toolbar selectedItemIdentifier]] ? NSControlStateValueOn : NSControlStateValueOff];
 	}
 }
 

Applications/TextMate/src/AppController.mm
@@ -799,7 +799,7 @@ BOOL HasDocumentWindow (NSArray* windows)
 				case find::ignore_whitespace:  active = [entry ignoreWhitespace];  enabled = ![entry regularExpression]; break;
 				case find::wrap_around:        active = [[NSUserDefaults standardUserDefaults] boolForKey:kUserDefaultsFindWrapAround]; break;
 			}
-			[item setState:(active ? NSOnState : NSOffState)];
+			[item setState:(active ? NSControlStateValueOn : NSControlStateValueOff)];
 		}
 		else
 		{

Applications/TextMate/src/Favorites.mm
@@ -120,8 +120,8 @@ static NSUInteger const kOakSourceIndexFavorites      = 1;
 		NSButton* removeButton = [NSButton new];
 		removeButton.controlSize = NSControlSizeSmall;
 		removeButton.refusesFirstResponder = YES;
-		removeButton.bezelStyle = NSRoundRectBezelStyle;
-		removeButton.buttonType = NSMomentaryPushInButton;
+		removeButton.bezelStyle = NSBezelStyleRoundRect;
+		removeButton.buttonType = NSButtonTypeMomentaryPushIn;
 		removeButton.image      = removeTemplateImage;
 		removeButton.target     = self;
 		removeButton.action     = @selector(takeItemToRemoveFrom:);
@@ -358,7 +358,7 @@ static NSUInteger const kOakSourceIndexFavorites      = 1;
 {
 	BOOL activate = YES;
 	if([item action] == @selector(takeSourceIndexFrom:))
-		[item setState:[item tag] == self.sourceIndex ? NSOnState : NSOffState];
+		[item setState:[item tag] == self.sourceIndex ? NSControlStateValueOn : NSControlStateValueOff];
 	return activate;
 }
 

Frameworks/BundleEditor/src/BundleEditor.mm
@@ -286,7 +286,7 @@ static be::entry_ptr parent_for_column (NSBrowser* aBrowser, NSInteger aColumn, 
 	[typeChooser setMenu:menu];
 	[typeChooser sizeToFit];
 	[alert setAccessoryView:typeChooser];
-	[alert beginSheetModalForWindow:self.window completionHandler:^(NSInteger returnCode){
+	[alert beginSheetModalForWindow:self.window completionHandler:^(NSModalResponse returnCode){
 		if(returnCode == NSAlertFirstButtonReturn)
 			[self createItemOfType:(bundles::kind_t)[[(NSPopUpButton*)[alert accessoryView] selectedItem] tag]];
 	}];
@@ -399,7 +399,7 @@ static be::entry_ptr parent_for_column (NSBrowser* aBrowser, NSInteger aColumn, 
 		if(!success)
 		{
 			NSAlert* alert = [NSAlert tmAlertWithMessageText:@"Error Parsing Property List" informativeText:@"The property list is not valid.\n\nUnfortunately I am presently unable to point to where the parser failed." buttons:@"OK", nil];
-			[alert beginSheetModalForWindow:self.window completionHandler:^(NSInteger returnCode){ }];
+			[alert beginSheetModalForWindow:self.window completionHandler:^(NSModalResponse returnCode){ }];
 			return NO;
 		}
 	}
@@ -474,7 +474,7 @@ static be::entry_ptr parent_for_column (NSBrowser* aBrowser, NSInteger aColumn, 
 	if(!changes.empty())
 	{
 		NSAlert* alert = [NSAlert tmAlertWithMessageText:@"Error Saving Bundle Item" informativeText:@"Sorry, but something went wrong while trying to save your changes. More info may be available via the console." buttons:@"OK", nil];
-		[alert beginSheetModalForWindow:self.window completionHandler:^(NSInteger returnCode){
+		[alert beginSheetModalForWindow:self.window completionHandler:^(NSModalResponse returnCode){
 			if(returnCode == NSAlertSecondButtonReturn) // Discard Changes
 			{
 				changes.clear();
@@ -580,8 +580,8 @@ static be::entry_ptr parent_for_column (NSBrowser* aBrowser, NSInteger aColumn, 
 
 - (void)copyUUID:(NSMenuItem*)sender
 {
-	[[NSPasteboard generalPasteboard] declareTypes:@[ NSStringPboardType ] owner:nil];
-	[[NSPasteboard generalPasteboard] setString:[sender representedObject] forType:NSStringPboardType];
+	[[NSPasteboard generalPasteboard] declareTypes:@[ NSPasteboardTypeString ] owner:nil];
+	[[NSPasteboard generalPasteboard] setString:[sender representedObject] forType:NSPasteboardTypeString];
 }
 
 - (void)exportBundle:(id)sender
@@ -595,8 +595,8 @@ static be::entry_ptr parent_for_column (NSBrowser* aBrowser, NSInteger aColumn, 
 
 		NSSavePanel* savePanel = [NSSavePanel savePanel];
 		[savePanel setNameFieldStringValue:[NSString stringWithCxxString:name + ".tmbundle"]];
-		[savePanel beginSheetModalForWindow:self.window completionHandler:^(NSInteger result) {
-			if(result == NSFileHandlingPanelOKButton)
+		[savePanel beginSheetModalForWindow:self.window completionHandler:^(NSModalResponse result) {
+			if(result == NSModalResponseOK)
 			{
 				NSString* path = [[savePanel.URL filePathURL] path];
 				if([[NSFileManager defaultManager] fileExistsAtPath:path])
@@ -763,7 +763,7 @@ static NSMutableDictionary* DictionaryForPropertyList (plist::dictionary_t const
 	{
 		NSMenuItem* item = [self createMenuItemForCxxPath:path];
 		item.title = [[NSString stringWithCxxString:path] stringByAbbreviatingWithTildeInPath];
-		item.state = NSOffState;
+		item.state = NSControlStateValueOff;
 		[menu addItem:item];
 	}
 	return YES;
@@ -912,7 +912,7 @@ static NSString* DescriptionForChanges (std::map<bundles::item_ptr, plist::dicti
 	[alert setMessageText:DescriptionForChanges(changes)];
 	[alert setInformativeText:@"Your changes will be lost if you don’t save them."];
 	[alert addButtons:@"Save", @"Cancel", @"Don’t Save", nil];
-	[alert beginSheetModalForWindow:self.window completionHandler:^(NSInteger returnCode){
+	[alert beginSheetModalForWindow:self.window completionHandler:^(NSModalResponse returnCode){
 		if(returnCode != NSAlertSecondButtonReturn) // Not "Cancel"
 		{
 			if(returnCode == NSAlertFirstButtonReturn) // "Save"

Frameworks/CommitWindow/src/CommitWindow.mm
@@ -164,15 +164,15 @@ static void* kOakCommitWindowIncludeItemBinding = &kOakCommitWindowIncludeItemBi
 
 		_showTableButton = [[NSButton alloc] initWithFrame:NSZeroRect];
 		_showTableButton.buttonType = NSOnOffButton;
-		_showTableButton.bezelStyle = NSRoundedDisclosureBezelStyle;
+		_showTableButton.bezelStyle = NSBezelStyleRoundedDisclosure;
 		_showTableButton.title = @"";
 		_showTableButton.action = @selector(toggleTableView:);
-		_showTableButton.state = _showsTableView ? NSOnState : NSOffState;
+		_showTableButton.state = _showsTableView ? NSControlStateValueOn : NSControlStateValueOff;
 
 		_previousCommitMessagesPopUpButton = [NSPopUpButton new];
 		_previousCommitMessagesPopUpButton.bordered   = YES;
 		_previousCommitMessagesPopUpButton.pullsDown  = YES;
-		_previousCommitMessagesPopUpButton.bezelStyle = NSTexturedRoundedBezelStyle;
+		_previousCommitMessagesPopUpButton.bezelStyle = NSBezelStyleTexturedRounded;
 		[self setupPreviousCommitMessagesMenu];
 
 		_topDocumentViewDivider    = OakCreateHorizontalLine(OakBackgroundFillViewStyleDivider);
@@ -359,7 +359,7 @@ static void* kOakCommitWindowIncludeItemBinding = &kOakCommitWindowIncludeItemBi
 	_bottomScrollViewDivider = OakCreateHorizontalLine(OakBackgroundFillViewStyleDivider);
 
 	_actionPopUpButton = OakCreateActionPopUpButton(YES);
-	_actionPopUpButton.bezelStyle = NSTexturedRoundedBezelStyle;
+	_actionPopUpButton.bezelStyle = NSBezelStyleTexturedRounded;
 	_actionPopUpButton.menu.delegate = self;
 
 	OakAddAutoLayoutViewsToSuperview(@[ _topScrollViewDivider, _scrollView, _bottomScrollViewDivider, _actionPopUpButton ], contentView);
@@ -710,7 +710,7 @@ static void* kOakCommitWindowIncludeItemBinding = &kOakCommitWindowIncludeItemBi
 			{
 				dispatch_async(dispatch_get_main_queue(), ^{
 					NSAlert* alert = [NSAlert tmAlertWithMessageText:@"Failed running diff command." informativeText:[NSString stringWithCxxString:cmdString] buttons:@"OK", nil];
-					[alert beginSheetModalForWindow:self.window completionHandler:^(NSInteger returnCode){}];
+					[alert beginSheetModalForWindow:self.window completionHandler:^(NSModalResponse returnCode){}];
 				});
 			}
 		});
@@ -770,7 +770,7 @@ static void* kOakCommitWindowIncludeItemBinding = &kOakCommitWindowIncludeItemBi
 		if(rangeOfStatus.location == NSNotFound)
 		{
 			NSAlert* alert = [NSAlert tmAlertWithMessageText:@"Cannot understand output from command" informativeText:[NSString stringWithCxxString:cmdString] buttons:@"OK", nil];
-			[alert beginSheetModalForWindow:self.window completionHandler:^(NSInteger returnCode){}];
+			[alert beginSheetModalForWindow:self.window completionHandler:^(NSModalResponse returnCode){}];
 		}
 		NSString* newStatus = [outputStatus substringToIndex:rangeOfStatus.location];
 		CWItem* item = [[_arrayController arrangedObjects] objectAtIndex:row];
@@ -780,7 +780,7 @@ static void* kOakCommitWindowIncludeItemBinding = &kOakCommitWindowIncludeItemBi
 	else
 	{
 		NSAlert* alert = [NSAlert tmAlertWithMessageText:@"Failed running command" informativeText:[NSString stringWithCxxString:cmdString] buttons:@"OK", nil];
-		[alert beginSheetModalForWindow:self.window completionHandler:^(NSInteger returnCode){}];
+		[alert beginSheetModalForWindow:self.window completionHandler:^(NSModalResponse returnCode){}];
 	}
 }
 
@@ -920,6 +920,6 @@ static void* kOakCommitWindowIncludeItemBinding = &kOakCommitWindowIncludeItemBi
 	}
 
 	OakCommitWindow* commitWindow = [[OakCommitWindow alloc] initWithOptions:someOptions];
-	[commitWindow beginSheetModalForWindow:projectWindow completionHandler:^(NSInteger returnCode){ }];
+	[commitWindow beginSheetModalForWindow:projectWindow completionHandler:^(NSModalResponse returnCode){ }];
 }
 @end

Frameworks/DocumentWindow/src/DocumentWindowController.mm
@@ -57,7 +57,7 @@ static void show_command_error (std::string const& message, oak::uuid_t const& u
 	if(bundleItem)
 		[alert addButtonWithTitle:@"Edit Command"];
 
-	[alert beginSheetModalForWindow:window completionHandler:^(NSInteger button){
+	[alert beginSheetModalForWindow:window completionHandler:^(NSModalResponse button){
 		if(button == NSAlertSecondButtonReturn)
 			[[BundleEditor sharedInstance] revealBundleItem:bundleItem];
 	}];
@@ -459,7 +459,7 @@ static NSArray* const kObservedKeyPaths = @[ @"arrayController.arrangedObjects.p
 	}
 
 	NSAlert* alert = [DocumentWindowController saveAlertForDocuments:someDocuments];
-	[alert beginSheetModalForWindow:self.window completionHandler:^(NSInteger returnCode){
+	[alert beginSheetModalForWindow:self.window completionHandler:^(NSModalResponse returnCode){
 		switch(returnCode)
 		{
 			case NSAlertFirstButtonReturn: /* "Save" */
@@ -2209,7 +2209,7 @@ static NSArray* const kObservedKeyPaths = @[ @"arrayController.arrangedObjects.p
 		if(aMenu.propertiesToUpdate & NSMenuPropertyItemImage)
 			item.image = document.icon;
 		if(i == _selectedTabIndex)
-			[item setState:NSOnState];
+			[item setState:NSControlStateValueOn];
 		else if(document.isDocumentEdited)
 			[item setModifiedState:YES];
 		++i;
@@ -2271,7 +2271,7 @@ static NSArray* const kObservedKeyPaths = @[ @"arrayController.arrangedObjects.p
 	else if([menuItem action] == @selector(moveFocus:))
 		[menuItem setTitle:self.window.firstResponder == self.textView ? @"Move Focus to File Browser" : @"Move Focus to Document"];
 	else if([menuItem action] == @selector(takeProjectPathFrom:))
-		[menuItem setState:[self.defaultProjectPath isEqualToString:[menuItem representedObject]] ? NSOnState : NSOffState];
+		[menuItem setState:[self.defaultProjectPath isEqualToString:[menuItem representedObject]] ? NSControlStateValueOn : NSControlStateValueOff];
 	else if([menuItem action] == @selector(performCloseOtherTabsXYZ:))
 		active = _documents.count > 1;
 	else if([menuItem action] == @selector(performCloseTabsToTheRight:))
@@ -2286,7 +2286,7 @@ static NSArray* const kObservedKeyPaths = @[ @"arrayController.arrangedObjects.p
 		{
 			active = [indexSet count] != 0;
 			if(active && [menuItem action] == @selector(toggleSticky:))
-				[menuItem setState:[self isDocumentSticky:_documents[indexSet.firstIndex]] ? NSOnState : NSOffState];
+				[menuItem setState:[self isDocumentSticky:_documents[indexSet.firstIndex]] ? NSControlStateValueOn : NSControlStateValueOff];
 		}
 	}
 

Frameworks/FileBrowser/src/FileBrowserDiskOperations.mm
@@ -54,7 +54,7 @@
 				switch([alert runModal])
 				{
 					case NSAlertFirstButtonReturn: // Replace
-						forceFlag = alert.suppressionButton.state == NSOnState;
+						forceFlag = alert.suppressionButton.state == NSControlStateValueOn;
 						res = [self performOperation:op sourceURL:srcURL destinationURL:&destURL force:YES error:&error];
 					break;
 
@@ -81,7 +81,7 @@
 				switch([alert runModal])
 				{
 					case NSAlertFirstButtonReturn: // Delete
-						forceFlag = alert.suppressionButton.state == NSOnState;
+						forceFlag = alert.suppressionButton.state == NSControlStateValueOn;
 						res = [self performOperation:op sourceURL:srcURL destinationURL:&destURL force:YES error:&error];
 					break;
 

commit 774e66c009a671b1ebf62519c0b68a899d63d59e

Ignore ‘types’ in implementation of writeSelectionToPasteboard:types:
The issue is that the types requested may be ‘NSStringPboardType’ but that symbol is deprecated (starting with 10.14), yet the replacement (‘NSPasteboardTypeString’) is not string-equivalent, therefore our simple string comparison would fail.

This effectively broke services starting with 1ef78b3af7d2aa189695badaf7c3c9bbb2b4bc03 (where we adopted the new symbols).

Implementation of validRequestorForSendType:returnType: does not appear to be a problem, as that method is called with both ‘NSStringPboardType’ and ‘NSPasteboardTypeString’.

Also switched to using NSPasteboard’s writeObjects: so we don’t have to explicitly deal with the type of the data we write to the pasteboard (presumably NSString will declare it using both the old and new type).

Frameworks/OakTextView/src/OakTextView.mm
@@ -2056,18 +2056,16 @@ static void update_menu_key_equivalents (NSMenu* menu, std::multimap<std::string
 
 - (BOOL)writeSelectionToPasteboard:(NSPasteboard*)pboard types:(NSArray*)types
 {
-	BOOL res = NO;
-	if([self hasSelection] && [types containsObject:NSPasteboardTypeString])
-	{
-		std::vector<std::string> v;
-		ng::ranges_t const ranges = ng::dissect_columnar(*documentView, documentView->ranges());
-		for(auto const& range : ranges)
-			v.push_back(documentView->substr(range.min().index, range.max().index));
+	if(![self hasSelection])
+		return NO;
 
-		[pboard declareTypes:@[ NSPasteboardTypeString ] owner:nil];
-		res = [pboard setString:[NSString stringWithCxxString:text::join(v, "\n")] forType:NSPasteboardTypeString];
-	}
-	return res;
+	std::vector<std::string> v;
+	ng::ranges_t const ranges = ng::dissect_columnar(*documentView, documentView->ranges());
+	for(auto const& range : ranges)
+		v.push_back(documentView->substr(range.min().index, range.max().index));
+
+	[pboard clearContents];
+	return [pboard writeObjects:@[ to_ns(text::join(v, "\n")) ]];
 }
 
 - (BOOL)readSelectionFromPasteboard:(NSPasteboard*)pboard