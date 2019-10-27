5a6cf83e2d27bd36b63dc4fe8dcf83581b5c4b39
Allan Odgaard <git@abetarda.com>
2019-10-27 15:17:03 +0100

    Fix issue with FSEvents wrapper when dealing with multiple volumes
    The specific issue being addressed is macOS 10.15 where the mount point for ~/Library/Application Support is under /System/Volumes/Data so our assertion that the relative path received in our callback can be appended to the mount point, to match the path being observed, is false.
    
    As a workaround we call realpath() on the path we wish to observe, observe that path, and in our callback we calculate the relative path from the real observed path, and append that to the requested path.
    
    In theory the caller could be made responsible for expanding paths to their real path, but the current abstraction allows observing non-existing paths, which is why it needs to handle this path rewriting.

--- Frameworks/io/src/events.cc
+++ Frameworks/io/src/events.cc
@@ -7,29 +7,6 @@ OAK_DEBUG_VAR(FS_Events);
 
 namespace
 {
-	struct device_info_t
-	{
-		dev_t device;
-		std::string mount_point;
-
-		device_info_t (std::string path)
-		{
-			while(true)
-			{
-				struct statfs buf;
-				if(statfs(path.c_str(), &buf) == 0)
-				{
-					device      = buf.f_fsid.val[0];
-					mount_point = buf.f_mntonname;
-					break;
-				}
-
-				ASSERT_EQ(errno, ENOENT);
-				path = path::parent(path);
-			}
-		}
-	};
-
 	bool operator== (timespec const& lhs, timespec const& rhs)
 	{
 		return lhs.tv_sec == rhs.tv_sec && lhs.tv_nsec == rhs.tv_nsec;
@@ -67,22 +44,33 @@ namespace
 		timespec _ctimespec;
 	};
 
+	static std::string real_path (std::string const& path)
+	{
+		if(char* tmp = realpath(path.c_str(), nullptr))
+		{
+			std::string str = tmp;
+			free(tmp);
+			return str;
+		}
+		return path;
+	}
+
 	struct events_t
 	{
 		struct stream_t
 		{
-			stream_t (std::string const& path, fs::event_callback_t* calback, uint64_t eventId, CFTimeInterval latency) : _requested(path), _observed(_requested), _callback(calback), _event_id(eventId), _replay(false)
+			stream_t (std::string const& path, fs::event_callback_t* calback, uint64_t eventId, CFTimeInterval latency) : _requested(path), _observed(real_path(path)), _callback(calback), _event_id(eventId), _replay(false)
 			{
-				while(!_observed.is_directory() && _observed.path() != "/")
-					_observed = file_info_t(path::parent(_observed.path()));
+				_requestedExists = true;
 
-				device_info_t devInfo(_observed.path());
-				_mount_point = devInfo.mount_point;
-				std::string devPath = path::relative_to(_observed.path(), devInfo.mount_point);
-				D(DBF_FS_Events, bug("watch ‘%s’ / ‘%s’ (device %x, event 0x%llx)\n", devInfo.mount_point.c_str(), devPath.c_str(), devInfo.device, _event_id););
+				while(!_observed.is_directory() && _observed.path() != "/")
+				{
+					_observed = file_info_t(path::parent(_observed.path()));
+					_requestedExists = false;
+				}
 
 				FSEventStreamContext contextInfo = { 0, this, nullptr, nullptr, nullptr };
-				if(!(_stream = FSEventStreamCreateRelativeToDevice(kCFAllocatorDefault, &events_t::callback, &contextInfo, devInfo.device, cf::wrap(std::vector<std::string>(1, devPath)), eventId, latency, kFSEventStreamCreateFlagNone)))
+				if(!(_stream = FSEventStreamCreate(kCFAllocatorDefault, &events_t::callback, &contextInfo, cf::wrap(std::vector<std::string>(1, _observed.path())), eventId, latency, kFSEventStreamCreateFlagNone)))
 					fprintf(stderr, "can’t observe ‘%s’\n", path.c_str());
 
 				_event_id = FSEventsGetCurrentEventId();
@@ -113,13 +101,13 @@ namespace
 			explicit operator bool () const    { return _stream; }
 			operator FSEventStreamRef () const { return _stream; }
 
-			std::string _mount_point;
 			FSEventStreamRef _stream;
 			file_info_t _requested;
 			file_info_t _observed;
 			fs::event_callback_t* _callback;
 			uint64_t _event_id;
 			bool _replay;
+			bool _requestedExists;
 		};
 
 		typedef std::shared_ptr<stream_t> stream_ptr;
@@ -163,8 +151,12 @@ namespace
 			uint64_t lastEventId = 0;
 			for(size_t i = 0; i < numEvents; ++i)
 			{
-				std::string path = path::join(stream._mount_point, ((char const* const*)eventPaths)[i]);
+				std::string path = ((char const* const*)eventPaths)[i];
 				std::string const& requestedPath = stream._requested.path();
+
+				if(stream._requestedExists && requestedPath != stream._observed.path())
+					path = path::join(requestedPath, path::relative_to(path, stream._observed.path()));
+
 				D(DBF_FS_Events, bug("%zu/%zu) 0x%llx: %s%s%s%s\n", i+1, numEvents, eventIds[i], path.c_str(), (eventFlags[i] & kFSEventStreamEventFlagMustScanSubDirs) ? ", recursive" : "", (eventFlags[i] & kFSEventStreamEventFlagHistoryDone) ? ", history done" : "", stream._replay ? " (replay history)" : ""););
 				if(eventFlags[i] & kFSEventStreamEventFlagHistoryDone)
 				{
