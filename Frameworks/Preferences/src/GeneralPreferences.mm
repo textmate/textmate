40379d8ab15a2552b229c43a17bc3cb72eaa3068
Allan Odgaard <git@abetarda.com>
2020-04-18 22:22:52 +0700

    Remove (unused) general preferences pane
    This was just a placeholder/template.

--- Frameworks/Preferences/src/GeneralPreferences.mm
+++
@@ -1,8 +0,0 @@
-#import "GeneralPreferences.h"
-
-@implementation GeneralPreferences
-- (id)init                         { return [super initWithNibName:@"GeneralPreferences" bundle:[NSBundle bundleForClass:[self class]]]; }
-- (NSString*)viewIdentifier        { return @"General"; }
-- (NSImage*)toolbarItemImage       { return [NSImage imageNamed:NSImageNamePreferencesGeneral]; }
-- (NSString*)toolbarItemLabel      { return @"General"; }
-@end
