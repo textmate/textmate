91f7bae6d275581ca5be79fdf873917ab1532400
Allan Odgaard <git@abetarda.com>
2020-04-22 11:47:16 +0700

    Notify observers of OakTabBarViewController’s selectedIndex property

--- Frameworks/OakTabBarView/src/OakTabBarViewController.mm
+++ Frameworks/OakTabBarView/src/OakTabBarViewController.mm
@@ -69,11 +69,29 @@
 
 - (void)setSelectedIndex:(NSUInteger)newSelectedIndex
 {
+	BOOL notifyObservers = _selectedIndex != newSelectedIndex;
+
 	_selectedIndex = newSelectedIndex;
 	if(!_hasPendingReloadTabsFlag)
 		[_tabBarView setSelectedTabIndex:_selectedIndex];
 
 	[self invalidateRestorableState];
+
+	if(notifyObservers)
+	{
+		if(NSDictionary* info = [self infoForBinding:@"selectedIndex"])
+		{
+			id controller     = info[NSObservedObjectKey];
+			NSString* keyPath = info[NSObservedKeyPathKey];
+			if(controller && controller != [NSNull null] && keyPath && (id)keyPath != [NSNull null])
+			{
+				id newValue = [NSNumber numberWithUnsignedInteger:_selectedIndex];
+				id oldValue = [controller valueForKeyPath:keyPath];
+				if(!oldValue || ![oldValue isEqual:newValue])
+					[controller setValue:newValue forKeyPath:keyPath];
+			}
+		}
+	}
 }
 
 - (void)setIdentifiers:(NSArray<NSUUID*>*)newIdentifiers
