5d60fe2d62a8879d937525c13c6a1e2332e2c225
Allan Odgaard <git@abetarda.com>
2020-04-26 14:42:35 +0700

    Move FFTextFieldViewController to its own file

---
+++ Frameworks/Find/src/FFTextFieldViewController.h
@@ -0,0 +1,9 @@
+@class OakPasteboard;
+
+@interface FFTextFieldViewController : NSViewController
+@property (nonatomic) BOOL syntaxHighlightEnabled;
+@property (nonatomic) BOOL hasFocus;
+
+- (instancetype)initWithPasteboard:(OakPasteboard*)pasteboard grammarName:(NSString*)grammarName;
+- (void)showHistory:(id)sender;
+@end
--- Frameworks/Find/src/Find.mm
+++ Frameworks/Find/src/Find.mm
@@ -1,6 +1,7 @@
 #import "Find.h"
 #import "FFResultNode.h"
 #import "FFResultsViewController.h"
+#import "FFTextFieldViewController.h"
 #import "FFDocumentSearch.h"
 #import "CommonAncestor.h"
 #import "FFFolderMenu.h"
@@ -13,8 +14,6 @@
 #import <OakAppKit/NSMenuItem Additions.h>
 #import <OakAppKit/OakAppKit.h>
 #import <OakAppKit/OakPasteboard.h>
-#import <OakAppKit/OakPasteboardSelector.h>
-#import <OakAppKit/OakSyntaxFormatter.h>
 #import <OakAppKit/OakUIConstructionFunctions.h>
 #import <MenuBuilder/MenuBuilder.h>
 #import <Preferences/Keys.h>
@@ -61,168 +60,9 @@ enum FindActionTag
 }
 @end
 
-// ====================
-// = Various controls =
-// ====================
-
-@interface OakAutoSizingTextField : NSTextField
-@property (nonatomic) NSSize myIntrinsicContentSize;
-@end
-
-@implementation OakAutoSizingTextField
-- (NSSize)intrinsicContentSize
-{
-	return NSEqualSizes(self.myIntrinsicContentSize, NSZeroSize) ? [super intrinsicContentSize] : self.myIntrinsicContentSize;
-}
-
-- (void)updateIntrinsicContentSizeToEncompassString:(NSString*)aString
-{
-	NSTextFieldCell* cell = [self.cell copy];
-	cell.stringValue = aString;
-
-	self.myIntrinsicContentSize = NSMakeSize(NSViewNoIntrinsicMetric, MAX(22, MIN([cell cellSizeForBounds:NSMakeRect(0, 0, NSWidth([self bounds]), CGFLOAT_MAX)].height, 225)));
-	[self invalidateIntrinsicContentSize];
-}
-@end
-
-@interface FFTextFieldViewController : NSViewController <NSTextFieldDelegate, NSTextStorageDelegate>
-{
-	OakAutoSizingTextField* _textField;
-	OakSyntaxFormatter*     _syntaxFormatter;
-	OakPasteboard*          _pasteboard;
-	NSString*               _grammarName;
-}
-@property (nonatomic) BOOL syntaxHighlightEnabled;
-@property (nonatomic) BOOL hasFocus;
-
-- (void)showHistory:(id)sender;
-@end
-
-static void* kFirstResponderContext = &kFirstResponderContext;
-
-@implementation FFTextFieldViewController
-- (instancetype)initWithPasteboard:(OakPasteboard*)pasteboard grammarName:(NSString*)grammarName
-{
-	if(self = [self initWithNibName:nil bundle:nil])
-	{
-		_pasteboard  = pasteboard;
-		_grammarName = grammarName;
-	}
-	return self;
-}
-
-- (void)showHistory:(id)sender
-{
-	if(!OakPasteboardSelector.sharedInstance.window.isVisible)
-		[_pasteboard selectItemForControl:_textField];
-}
-
-- (void)setSyntaxHighlightEnabled:(BOOL)flag
-{
-	if(_syntaxHighlightEnabled == flag)
-		return;
-
-	_syntaxHighlightEnabled  = flag;
-	_syntaxFormatter.enabled = flag;
-
-	// Re-format current value
-	if(!_textField.currentEditor)
-	{
-		NSString* currentString = [_textField.stringValue copy];
-		_textField.objectValue = nil;
-		_textField.objectValue = currentString;;
-	}
-
-	[self addStylesToFieldEditor];
-}
-
-- (OakSyntaxFormatter*)syntaxFormatter
-{
-	if(!_syntaxFormatter)
-		_syntaxFormatter = [[OakSyntaxFormatter alloc] initWithGrammarName:_grammarName];
-	return _syntaxFormatter;
-}
-
-- (OakAutoSizingTextField*)textField
-{
-	if(!_textField)
-	{
-		_textField = [[OakAutoSizingTextField alloc] initWithFrame:NSZeroRect];
-		_textField.font       = OakControlFont();
-		_textField.formatter  = self.syntaxFormatter;
-		_textField.delegate   = self;
-		_textField.cell.wraps = YES;
-	}
-	return _textField;
-}
-
-- (void)loadView
-{
-	self.view = self.textField;
-}
-
-- (void)viewDidAppear
-{
-	[self.view.window addObserver:self forKeyPath:@"firstResponder" options:0 context:kFirstResponderContext];
-}
-
-- (void)viewWillDisappear
-{
-	[self.view.window removeObserver:self forKeyPath:@"firstResponder" context:kFirstResponderContext];
-}
-
-- (void)observeValueForKeyPath:(NSString*)keyPath ofObject:(id)object change:(NSDictionary*)change context:(void*)context
-{
-	if(context == kFirstResponderContext)
-	{
-		id firstResponder = self.view.window.firstResponder;
-		self.hasFocus = firstResponder == _textField || firstResponder == _textField.currentEditor;
-	}
-}
-
-- (void)setHasFocus:(BOOL)flag
-{
-	_hasFocus = flag;
-	if(_hasFocus && [_textField.currentEditor isKindOfClass:[NSTextView class]])
-	{
-		NSTextView* textView = (NSTextView*)_textField.currentEditor;
-		textView.textStorage.delegate = self;
-		[self addStylesToFieldEditor];
-	}
-}
-
-- (BOOL)control:(NSControl*)control textView:(NSTextView*)textView doCommandBySelector:(SEL)command
-{
-	if(command == @selector(moveDown:))
-	{
-		NSRange lastNewline    = [textView.string rangeOfString:@"\n" options:NSBackwardsSearch];
-		NSRange insertionPoint = textView.selectedRanges.lastObject.rangeValue;
-
-		if(lastNewline.location == NSNotFound || lastNewline.location < NSMaxRange(insertionPoint))
-			return [self showHistory:self], YES;
-	}
-	return NO;
-}
-
-- (void)controlTextDidChange:(NSNotification*)aNotification
-{
-	OakAutoSizingTextField* textField = aNotification.object;
-	NSDictionary* userInfo = aNotification.userInfo;
-	NSTextView* textView = userInfo[@"NSFieldEditor"];
-	if(textView && textField)
-		[textField updateIntrinsicContentSizeToEncompassString:textView.string];
-}
-
-- (void)textStorageDidProcessEditing:(NSNotification*)aNotification
-{
-	[self addStylesToFieldEditor];
-}
-
-- (void)addStylesToFieldEditor
-{
-	[_syntaxFormatter addStylesToString:((NSTextView*)_textField.currentEditor).textStorage];
-}
-@end
+// ========================
+// = FindWindowController =
+// ========================
 
 static NSButton* OakCreateHistoryButton (NSString* toolTip)
 {
@@ -235,10 +75,6 @@ static NSButton* OakCreateHistoryButton (NSString* toolTip)
 	[res setContentCompressionResistancePriority:NSLayoutPriorityRequired forOrientation:NSLayoutConstraintOrientationHorizontal];
 	return res;
 }
-
-// ========================
-// = FindWindowController =
-// ========================
 
 @interface FindWindowController : NSWindowController <NSWindowDelegate, NSMenuDelegate, NSPopoverDelegate>
 {
