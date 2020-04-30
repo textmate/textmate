49bbb8296a72ce27df118f418e1a1285fd719920
Allan Odgaard <git@abetarda.com>
2020-04-30 10:19:21 +0700

    Turn OakHistortyList into a generic class

--- Frameworks/OakFoundation/src/OakHistoryList.h
+++ Frameworks/OakFoundation/src/OakHistoryList.h
@@ -7,15 +7,15 @@ If an object that is already in the list is added, it will be moved to the top o
 If the list grows beyond _stackSize_ objects, the last object will be removed before the new item is added.
 */
 
-PUBLIC @interface OakHistoryList : NSObject
+PUBLIC @interface OakHistoryList<ObjectType> : NSObject
 @property (nonatomic, readonly) NSUInteger stackSize;
-@property (nonatomic) id head;
+@property (nonatomic) ObjectType head;
 
 - (id)initWithName:(NSString*)defaultsName stackSize:(NSUInteger)size;
 - (id)initWithName:(NSString*)defaultsName stackSize:(NSUInteger)size fallbackUserDefaultsKey:(NSString*)fallbackDefaultsName;
 - (id)initWithName:(NSString*)defaultsName stackSize:(NSUInteger)size defaultItems:(id)firstItem, ...;
-- (void)addObject:(id)newItem;
-- (NSEnumerator*)objectEnumerator;
-- (id)objectAtIndex:(NSUInteger)index;
+- (void)addObject:(ObjectType)newItem;
+- (NSEnumerator<ObjectType>*)objectEnumerator;
+- (ObjectType)objectAtIndex:(NSUInteger)index;
 - (NSUInteger)count;
 @end
