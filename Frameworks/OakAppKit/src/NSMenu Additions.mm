94022fb4f779d193ba198b1a34d28e40ff599c01
Allan Odgaard <git@abetarda.com>
2020-05-15 12:17:39 +0700

    Simplify implementation of parentMenuItem

--- Frameworks/OakAppKit/src/NSMenu Additions.mm
+++ Frameworks/OakAppKit/src/NSMenu Additions.mm
@@ -1,17 +1,9 @@
 #import "NSMenu Additions.h"
 
 @implementation NSMenu (Additions)
-- (NSMenuItem*)parentMenuItem;
+- (NSMenuItem*)parentMenuItem
 {
-	NSMenuItem* superItem = nil;
-	for(NSMenuItem* item in [[self supermenu] itemArray])
-	{
-		if([item submenu] == self)
-		{
-			superItem = item;
-			break;
-		}
-	}
-	return superItem;
+	NSInteger i = [self.supermenu indexOfItemWithSubmenu:self];
+	return i != -1 ? self.supermenu.itemArray[i] : nil;
 }
 @end
