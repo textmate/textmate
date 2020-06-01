a69d4acace53dbf20c390f8884735fc95cbbf2cb
Allan Odgaard <git@abetarda.com>
2020-06-01 18:14:05 +0700

    Update find tests to use find_t::each_match
    These tests are not exactly exhaustive and should probably be amended with more edge cases, especially now that the API has been simplified (so easier to add more tests).

--- Frameworks/regexp/tests/t_find.cc
+++ Frameworks/regexp/tests/t_find.cc
@@ -1,26 +1,31 @@
 #include <regexp/find.h>
 
-typedef std::pair<ssize_t, ssize_t> range_t;
+typedef std::pair<size_t, size_t> range_t;
+
+static std::vector<range_t> all_matches (find::find_t matcher, std::string const& text)
+{
+	std::vector<range_t> res;
+	matcher.each_match(text.data(), text.size(), false /* no more data */, [&res](std::pair<size_t, size_t> const& m, std::map<std::string, std::string> const& captures){
+		res.push_back(m);
+	});
+	return res;
+}
 
 void test_regular_backward ()
 {
-	std::string const text = "var = 32 && var == 5;";
+	find::find_t matcher("var = ");
+	std::vector<range_t> ranges = all_matches(matcher, "var = 32 && var = 5;");
 
-	find::find_t matcher("var = ", find::backwards);
-	range_t m = matcher.match(text.data(), text.size());
-	OAK_ASSERT_EQ(m, range_t(0, 6));
+	OAK_ASSERT_EQ(ranges.size(), 2);
+	OAK_ASSERT_EQ(ranges[0], range_t(0, 6));
+	OAK_ASSERT_EQ(ranges[1], range_t(12, 18));
 }
 
 void test_regexp_backward ()
 {
-	std::string const text = "Lorem text::uppercase ipsum.";
+	find::find_t matcher("text::(lower|upper)", find::regular_expression);
+	std::vector<range_t> ranges = all_matches(matcher, "Lorem text::uppercase ipsum.");
 
-	find::find_t matcher("text::(lower|upper)", find::backwards|find::regular_expression);
-	range_t m = matcher.match(text.data() + 10, text.size() - 10);
-	OAK_ASSERT_LT(m.second, m.first);
-	m = matcher.match(text.data(), 10);
-	OAK_ASSERT_LT(m.second, m.first);
-	m = matcher.match(nullptr, 0);
-	ssize_t base = text.size();
-	OAK_ASSERT_EQ(range_t(base + m.first, base + m.second), range_t(6, 17));
+	OAK_ASSERT_EQ(ranges.size(), 1);
+	OAK_ASSERT_EQ(ranges[0], range_t(6, 17));
 }
