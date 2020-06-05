249f365e707d6ddb786e3cfd544ab360dbe3bcd7
Allan Odgaard <git@abetarda.com>
2020-06-05 18:18:38 +0700

    Remove majority of code from OakDebug framework

--- Frameworks/OakDebug/src/OakDebug.h
+++ Frameworks/OakDebug/src/OakDebug.h
@@ -2,15 +2,10 @@
 #define OAKDEBUG_H
 
 #include "OakAssert.h"
-#include "OakDebugLog.h"
-#include "OakWatchLeaks.h"
-#include "OakBenchmark.h"
 
 #ifndef NDEBUG
-#define DB(code) code
 #define DB_VAR
 #else
-#define DB(code)
 #define DB_VAR __attribute__ ((unused))
 #endif
 
