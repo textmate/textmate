88ca589cbc06ce8730182c82fb88d699a1e44ed8
Allan Odgaard <git@abetarda.com>
2021-03-29 10:00:56 +0200

    Increase padding for file browser’s top folder pop-up
    The spacing was so that the icon (in the folder pop-up) was aligned with the file item icons in the list below, but this no longer holds on Big Sur, and the spacing is too tight on Big Sur.
    
    Like with the status bar pop-ups, I am using the increased spacing for all builds, to keep things simple.

--- Frameworks/FileBrowser/src/OFB/OFBHeaderView.mm
+++ Frameworks/FileBrowser/src/OFB/OFBHeaderView.mm
@@ -58,7 +58,7 @@ static NSPopUpButton* OakCreateFolderPopUpButton ()
 		OakAddAutoLayoutViewsToSuperview([views allValues], self);
 		OakSetupKeyViewLoop(@[ self, _folderPopUpButton, _goBackButton, _goForwardButton ]);
 
-		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(-3)-[folder(>=75)]-(3)-[divider(==1)]-(2)-[back(==22)]-(2)-[forward(==back)]-(3)-|" options:NSLayoutFormatAlignAllCenterY metrics:nil views:views]];
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(3)-[folder(>=75)]-(3)-[divider(==1)]-(2)-[back(==22)]-(2)-[forward(==back)]-(3)-|" options:NSLayoutFormatAlignAllCenterY metrics:nil views:views]];
 		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[bottomDivider]|"                                                                     options:0 metrics:nil views:views]];
 		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(4)-[divider(==15)]-(4)-[bottomDivider(==1)]|"                                   options:0 metrics:nil views:views]];
 	}
